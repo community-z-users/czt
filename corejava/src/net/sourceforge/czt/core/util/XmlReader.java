0765452ad6ae05fd5213b7d69452f6fd5101fd0e
Petra Malik <petramalik@users.sourceforge.net>
2003-08-13 04:14:59 +0000

    INITIAL RELEASE   XML reader and writer interfaces.

---
+++ corejava/src/net/sourceforge/czt/core/util/XmlReader.java
@@ -0,0 +1,50 @@
+/*
+Copyright 2003 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.core.util;
+
+import java.io.File;
+import java.io.InputStream;
+import net.sourceforge.czt.core.ast.Term;
+
+/**
+ * Provides methods for reading XML data.
+ *
+ * @author Petra Malik
+ * @czt.todo Provide Exceptions that can be thrown
+ *           in case the unmarshalling was unseccessful.
+ */
+public interface XmlReader
+{
+  /**
+   * Unmarshalles XML data from the specified file and
+   * returns the root term.
+   * @param file  the file to be unmarshalled.
+   * @return the root element of the unmarshalled file.
+   */
+  public Term read(File file);
+
+  /**
+   * Unmarshalles XML data from the specified input stream and
+   * returns the root term.
+   * @param stream  the input stream used for unmarshalling.
+   * @return the root element of the unmarshalled file.
+   */
+  public Term read(InputStream stream);
+}
---
+++ corejava/src/net/sourceforge/czt/core/util/XmlWriter.java
@@ -0,0 +1,48 @@
+/*
+Copyright 2003 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.core.util;
+
+import java.io.OutputStream;
+import java.io.Writer;
+import net.sourceforge.czt.core.ast.Term;
+
+/**
+ * Provides methods for writing XML data.
+ *
+ * @author Petra Malik
+ * @czt.todo Provide Exceptions that can be thrown
+ *           in case the marshalling was unseccessful.
+ */
+public interface XmlWriter
+{
+  /**
+   * Marshalles a Term into the specified Writer.
+   * @param term    the Term to be marshalled.
+   * @param writer  the Writer used for marshalling.
+   */
+  public void write(Term term, Writer writer);
+
+  /**
+   * Marshalles a Term into the specified stream.
+   * @param term    the Term to be marshalled.
+   * @param stream  the OutputStream used for marshalling.
+   */
+  public void write(Term term, OutputStream stream);
+}
