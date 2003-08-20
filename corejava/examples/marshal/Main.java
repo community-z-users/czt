9a0da7bb08d9b4df5f4f4bdd2a338ac48500bf92
Petra Malik <petramalik@users.sourceforge.net>
2003-08-20 02:07:01 +0000

    INITIAL RELEASE   Example for marshalling.

---
+++ corejava/examples/marshal/Main.java
@@ -0,0 +1,42 @@
+/**
+Copyright 2003 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+import java.util.logging.*;
+
+import net.sourceforge.czt.core.util.*;
+import net.sourceforge.czt.core.jaxb.JaxbXmlReader;
+import net.sourceforge.czt.core.jaxb.JaxbXmlWriter;
+import net.sourceforge.czt.core.ast.Spec;
+
+public class Main {
+  public static void main( String[] args ) {
+    try {
+      // Unmarshal file eg1.xml ...
+      XmlReader reader = new JaxbXmlReader();
+      Spec spec =
+	(Spec) reader.read(new java.io.File("../../../zml/examples/eg1.xml"));
+
+      // ... and marshal it to stdout
+      XmlWriter writer = new JaxbXmlWriter();
+      writer.write(spec, System.out);
+    } catch( Exception e ) {
+      e.printStackTrace();
+    }
+  }
+}
