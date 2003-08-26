a8e0f213c74d2a36aecde1cbe97a3cbf9d3b97f6
Petra Malik <petramalik@users.sourceforge.net>
2003-08-26 01:32:07 +0000

    ADD   Some more test cases.

--- corejava/tests/eg1/Eg1.java
+++ corejava/tests/eg1/Eg1.java
@@ -19,6 +19,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package eg1;
 
+import java.util.*;
+
 import junit.framework.*;
 import net.sourceforge.czt.core.ast.*;
 import net.sourceforge.czt.core.util.XmlReader;
@@ -28,7 +30,9 @@ import net.sourceforge.czt.core.jaxb.JaxbXmlReader;
  * @author Petra Malik
  */
 public class Eg1 extends TestCase {
+  private static final String sFilename = "../../zml/examples/eg1.xml";
   private Spec mSpec;
+  private ZSect mZSect;
 
   public static Test suite() {
     return new TestSuite(Eg1.class);
@@ -37,8 +41,9 @@ public class Eg1 extends TestCase {
     try {
       XmlReader reader
 	= new JaxbXmlReader();
-      mSpec =
-	(Spec) reader.read(new java.io.File("../../zml/examples/eg1.xml"));
+      mSpec = (Spec) reader.read(new java.io.File(sFilename));
+      if (mSpec.getSect().size() > 0)
+	mZSect = (ZSect) mSpec.getSect().get(0);
     } catch(Exception e) {
       e.printStackTrace();
     }
@@ -47,7 +52,31 @@ public class Eg1 extends TestCase {
     Assert.assertTrue(mSpec.getSect().size()==1);
   }
   public void testZSectName() {
-    ZSect sect = (ZSect) mSpec.getSect().get(0);
-    Assert.assertTrue(sect.getName().equals("Specification"));
+    Assert.assertTrue(mZSect.getName().equals("Specification"));
+  }
+  public void testZSectParent() {
+    List parentList = mZSect.getParent();
+    Assert.assertTrue(parentList.size() == 2);
+    Parent parent1 = (Parent) parentList.get(0);
+    Parent parent2 = (Parent) parentList.get(1);
+    Assert.assertEquals(parent1.getWord(), "Sect1");
+    Assert.assertEquals(parent2.getChildren()[0], "Sect2");
+    Assert.assertFalse(parent1.equals(parent2));
+    Assert.assertFalse(parent2.equals(parent1));
+    parent1.setWord("Sect3");
+    Object[] args = { "Sect3" };
+    Term parent3 = parent2.create(args);
+    Assert.assertTrue(parent1.equals(parent3));
+    Assert.assertTrue(parent3.equals(parent1));
+    Assert.assertTrue(parent1.hashCode() == parent3.hashCode());
+    List anns1 = parent1.getAnns();
+    List anns2 = parent2.getAnns();
+    Assert.assertTrue(anns1.size() == 1);
+    Assert.assertTrue(anns2.size() == 0);
+  }
+  public void testZSectPara() {
+    List paraList = mZSect.getPara();
+    Assert.assertTrue(paraList.size() == 5);
+    Assert.assertFalse(paraList.get(0).equals(paraList.get(1)));
   }
 }
