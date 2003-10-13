b0564fac8c8328a71cf448b750c31ce71bf62f52
Petra Malik <petramalik@users.sourceforge.net>
2003-10-13 03:22:31 +0000

    MOVE   TermAImpl from package core to zed since core is for generated classes   and zed is for hand-written code and TermAImpl is written by hand.

--- corejava/src/net/sourceforge/czt/zed/impl/TermAImpl.java
+++ corejava/src/net/sourceforge/czt/zed/impl/TermAImpl.java
@@ -17,14 +17,14 @@ along with czt; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package net.sourceforge.czt.core.impl;
+package net.sourceforge.czt.zed.impl;
 
 import java.util.List;
 import java.util.Vector;
 
-import net.sourceforge.czt.core.ast.*;
+import net.sourceforge.czt.zed.ast.*;
 
-import net.sourceforge.czt.core.visitor.TermAVisitor;
+import net.sourceforge.czt.zed.visitor.TermAVisitor;
 import net.sourceforge.czt.util.Visitor;
 
 /**
