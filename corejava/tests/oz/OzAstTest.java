21a204cab5c720d62926b5bf59fabd1b394f33d5
Petra Malik <petramalik@users.sourceforge.net>
2003-12-18 03:09:19 +0000

    COSMETICS   clean up code.

--- corejava/tests/oz/OzAstTest.java
+++ corejava/tests/oz/OzAstTest.java
@@ -6,7 +6,6 @@ import java.io.*;
 import junit.framework.*;
 
 import net.sourceforge.czt.base.ast.Term;
-import net.sourceforge.czt.base.jaxb.JaxbXmlWriter;
 import net.sourceforge.czt.base.util.AstValidator;
 import net.sourceforge.czt.base.util.XmlReader;
 import net.sourceforge.czt.base.util.XmlWriter;
@@ -17,6 +16,7 @@ import net.sourceforge.czt.oz.ast.*;
 import net.sourceforge.czt.oz.jaxb.JaxbContext;
 import net.sourceforge.czt.oz.jaxb.JaxbValidator;
 import net.sourceforge.czt.oz.jaxb.JaxbXmlReader;
+import net.sourceforge.czt.oz.jaxb.JaxbXmlWriter;
 import net.sourceforge.czt.oz.jaxb.AstToJaxb;
 
 /**
@@ -25,163 +25,237 @@ import net.sourceforge.czt.oz.jaxb.AstToJaxb;
 public class OzAstTest extends TestCase {
   
   //the number of operations in the ClassPara
-  final protected static int NUM_OP_BOXES = 5;
-  final protected static int NUM_OPS = NUM_OP_BOXES + 2;
-  
-  protected OzFactory mOzFactory;
-  protected ZFactory mZFactory;
-  protected Spec mSpec;
-  
-  protected void setUp() {
-    mSpec = null;
-  }
-  
-  protected List list(Object o) {
-    List aList = new ArrayList();
-    aList.add(o);
-    
-    return aList;
-  }
+  final private static int NUM_OP_BOXES = 5;
+  final private static int NUM_OPS = NUM_OP_BOXES + 2;
 
-  protected void createAst() {
-    
-    mOzFactory = new net.sourceforge.czt.oz.impl.OzFactoryImpl();
-    mZFactory = new net.sourceforge.czt.z.impl.ZFactoryImpl();	
-    
-    mSpec = mZFactory.createSpec();
+  /**
+   * The factory for creating Z terms.
+   */
+  private ZFactory zFactory_ = new net.sourceforge.czt.z.impl.ZFactoryImpl();
+
+  /**
+   * The factory for creating Object Z terms.
+   */
+  private OzFactory ozFactory_ = new net.sourceforge.czt.oz.impl.OzFactoryImpl();
+
+  /**
+   * An Object Z validator which can be used
+   * for on-demand validations of an AST.
+   */
+  private AstValidator validator_ = new JaxbValidator();
+
+  /**
+   * A Z specification (the root of the AST).
+   */
+  private Spec spec_ = null;
+
+  /**
+   * Sets up a quite complex Object Z AST.
+   */  
+  protected void setUp() {
+    spec_ = zFactory_.createSpec();
     
     //the class name
-    DeclName className = mZFactory.createDeclName("MyClass",
-						  new ArrayList(),
-						  "MyClass");
+    DeclName className =
+      zFactory_.createDeclName("MyClass", null, "MyClass");
     
     //create formal parameters
-    RefName xTypeRefName = mZFactory.createRefName("X",
-						   new ArrayList(),
-						   null);
+    RefName xTypeRefName =
+      zFactory_.createRefName("X", null, null);
     
     FormalParameters fp = 
-      mOzFactory.createFormalParameters(list(xTypeRefName));
+      ozFactory_.createFormalParameters(list(xTypeRefName));
     
     //create a visibility list
-    DeclName xName = mZFactory.createDeclName("x", new ArrayList(), null);
-    DeclName pxName = mZFactory.createDeclName("px", new ArrayList(), null);
+    DeclName xName = zFactory_.createDeclName("x", null, null);
+    DeclName pxName = zFactory_.createDeclName("px", null, null);
     List vNameList = new ArrayList();	
     vNameList.add(xName);
     vNameList.add(pxName);
-    StringListType sList = mOzFactory.createStringListType(vNameList);
+    StringListType sList = ozFactory_.createStringListType(vNameList);
     
-    /*
-     * create a state schema with a variable declaration and
-     * secondary variable
-     */
-    RefName xRefName = mZFactory.createRefName("x", new ArrayList(), null);
-    RefName pxRefName = mZFactory.createRefName("px", new ArrayList(), null);
+    // create a state schema with a variable declaration and
+    // secondary variable
+    RefName xRefName = zFactory_.createRefName("x", null, null);
+    RefName pxRefName = zFactory_.createRefName("px", null, null);
     
-    RefExpr xTypeRefExpr = mZFactory.createRefExpr(xRefName,
-						   new ArrayList(),
-						   new Boolean(false));
-    PowerExpr powerX = mZFactory.createPowerExpr(xTypeRefExpr);
-    VarDecl xDecl = mZFactory.createVarDecl(list(xName), xTypeRefExpr);
-    VarDecl pxDecl = mZFactory.createVarDecl(list(pxName), powerX);
+    RefExpr xTypeRefExpr =
+      zFactory_.createRefExpr(xRefName, null, new Boolean(false));
+    PowerExpr powerX = zFactory_.createPowerExpr(xTypeRefExpr);
+    VarDecl xDecl = zFactory_.createVarDecl(list(xName), xTypeRefExpr);
+    VarDecl pxDecl = zFactory_.createVarDecl(list(pxName), powerX);
     
-    RefExpr xRefExpr = mZFactory.createRefExpr(xRefName,
-					       new ArrayList(),
-					       new Boolean(false));
-    RefExpr pxRefExpr = mZFactory.createRefExpr(pxRefName,
-						new ArrayList(),
-						new Boolean(false));
+    RefExpr xRefExpr =
+      zFactory_.createRefExpr(xRefName, null, new Boolean(false));
+    RefExpr pxRefExpr =
+      zFactory_.createRefExpr(pxRefName, null, new Boolean(false));
     
-    MemPred memPred = mZFactory.createMemPred(xRefExpr, 
-					      pxRefExpr,
-					      new Boolean(false));
-    
-    State state = mOzFactory.createState(list(xDecl),
-					 list(pxDecl),
-					 new ArrayList());
+    MemPred memPred =
+      zFactory_.createMemPred(xRefExpr, pxRefExpr, new Boolean(false));
+
+    // This call does create an invalid AST.
+    // The second argument of createState should be a list of SecondaryAttributes.
+    // But Jaxb doesn't find this error.
+    State state =
+      ozFactory_.createState(list(xDecl), list(pxDecl), null);
     
     //create the init operation
-    TruePred truePred = mZFactory.createTruePred();
-    InitialState init = mOzFactory.createInitialState(list(truePred));
+    TruePred truePred = zFactory_.createTruePred();
+    InitialState init = ozFactory_.createInitialState(list(truePred));
     
     //create some operations
-    DeclName xInName = mZFactory.createDeclName("x",
-						list(mZFactory.createInStroke()),
-						null);
-    VarDecl xInDecl = mZFactory.createVarDecl(list(xInName), xTypeRefExpr);
+    DeclName xInName =
+      zFactory_.createDeclName("x", list(zFactory_.createInStroke()), null);
+    VarDecl xInDecl = zFactory_.createVarDecl(list(xInName), xTypeRefExpr);
     
     List opList = new ArrayList();
     for (int i = 0; i < NUM_OP_BOXES; i++) {
-      OperationBox box1 = mOzFactory.createOperationBox(sList,
-							list(xInDecl),
-							list(truePred));
-      
-      Operation op = mOzFactory.createOperation(
-						mZFactory.createDeclName("op" + i, new ArrayList(), null),
-						box1);
-      
+      OperationBox box1 =
+	ozFactory_.createOperationBox(sList, list(xInDecl), list(truePred));
+      DeclName declName = zFactory_.createDeclName("op" + i, null, null);
+      Operation op = ozFactory_.createOperation( declName, box1);
       opList.add(op);
     }
     
     //create a parallel operation
-    RefName refNameOp1 = mZFactory.createRefName("op1",
-						 new ArrayList(),
-						 null);
-    RefName refNameOp2 = mZFactory.createRefName("op2",
-						 new ArrayList(),
-						 null);
+    RefName refNameOp1 = zFactory_.createRefName("op1", null, null);
+    RefName refNameOp2 = zFactory_.createRefName("op2", null, null);
     OperationExpr leftExpr =
-      mOzFactory.createOpPromotionExpr(null, refNameOp1);
+      ozFactory_.createOpPromotionExpr(null, refNameOp1);
     OperationExpr rightExpr =
-      mOzFactory.createOpPromotionExpr(null, refNameOp2);
+      ozFactory_.createOpPromotionExpr(null, refNameOp2);
     OperationExpr parallelOpExpr =
-      mOzFactory.createParallelOpExpr(leftExpr, rightExpr);
+      ozFactory_.createParallelOpExpr(leftExpr, rightExpr);
+    DeclName declName = zFactory_.createDeclName("paraOp", null, null);
     Operation parallelOp =
-      mOzFactory.createOperation(
-				 mZFactory.createDeclName("paraOp", new ArrayList(), null),
-				 parallelOpExpr);
+      ozFactory_.createOperation(declName, parallelOpExpr);
     opList.add(parallelOp);
     
     //create a distibuted choice operation
-    SchText schText = mZFactory.createSchText(list(xInDecl), truePred);
+    SchText schText = zFactory_.createSchText(list(xInDecl), truePred);
     OperationExpr promoteOpExpr =
-      mOzFactory.createOpPromotionExpr(null, refNameOp2);
-    Operation distOp = 
-      mOzFactory.createOperation(
-				 mZFactory.createDeclName("distOp", new ArrayList(), null),
-				 promoteOpExpr);
+      ozFactory_.createOpPromotionExpr(null, refNameOp2);
+    declName = zFactory_.createDeclName("distOp", new ArrayList(), null);
+    Operation distOp = ozFactory_.createOperation(declName, promoteOpExpr);
     opList.add(distOp);
     
     //create the class paragraph
-    ClassPara classPara = mOzFactory.createClassPara(className,
-						     fp,
-						     sList,
-						     new ArrayList(),
-						     null,
-						     state,
-						     init,
-						     opList);
+    ClassPara classPara =
+      ozFactory_.createClassPara(className, fp, sList, null,
+				 null, state, init, opList);
     
     ArrayList paras = new ArrayList();
     paras.add(classPara);
     ZSect section =
-      mZFactory.createZSect("Specification", null, paras);
+      zFactory_.createZSect("Specification", null, paras);
     
-    mSpec.getSect().add(section);
-    
+    spec_.getSect().add(section);
+  }
+
+  /**
+   * Creates a list with the given object as element.
+   *
+   * @param object the object to be inserted into the list.
+   * @return a list with exactly one element.  The element is
+   *         the one provided as argument.
+   */  
+  private List list(Object object) {
+    List list = new ArrayList();
+    list.add(object);
+    return list;
+  }
+
+  /**
+   * Tests the provided AST directly.
+   */
+  public void testAst()
+  {
+    Assert.assertTrue(validator_.validate(spec_));
+    numberOfSectTest();
+    classDetailsTest();
+  }
+
+  /**
+   * Test the AST that one gets
+   * after writing and reading it back
+   * into memory.
+   *
+   * @czt.todo Use a temporary file instead of a regular file.
+   */
+  public void testWriteReadAst()
+    throws Exception
+  {
+    Spec oldSpec = spec_;
+
+    // write ...
+    XmlWriter writer = new JaxbXmlWriter();
+    OutputStreamWriter outputStream
+      = new OutputStreamWriter(new FileOutputStream("MyClass.xml"), "utf8");
+    writer.write(spec_, outputStream);
+
+    // ... and read back
+    XmlReader reader = new JaxbXmlReader();
+    spec_ = (Spec) reader.read(new java.io.File("MyClass.xml"));
+
+    // perform checks
+
+    // TODO: check why this check does not work
+    //    Assert.assertTrue(oldSpec.equals(spec_));
+    Assert.assertTrue(validator_.validate(spec_));
+    numberOfSectTest();
+    classDetailsTest();
+  }
+
+  private void numberOfSectTest() {
+    Assert.assertEquals(1, spec_.getSect().size());
   }
   
-  public void openXml() {
+  private void classDetailsTest() {
+    List sects = spec_.getSect();
+    ZSect firstSect = (ZSect)sects.get(0);
+    List paras = firstSect.getPara();
+    Iterator it = paras.iterator();
+    ClassPara classPara = (ClassPara)it.next();
     
-    //createAst();
-    try {
-      XmlReader reader = new JaxbXmlReader();
-      mSpec = (Spec) reader.read(new java.io.File("MyClass.xml"));
-    } catch(Exception e) {
-      e.printStackTrace();
+    Assert.assertTrue(classPara.getName().getWord().equals("MyClass"));
+    
+    FormalParameters fp = classPara.getFormalParameters();
+    Assert.assertEquals(1, fp.getRefName().size());
+    
+    StringListType sList = classPara.getVisibilityList();
+    Assert.assertEquals(2, sList.getName().size());
+    
+    List inheritList = classPara.getInheritedClass();
+    Assert.assertEquals(0, inheritList.size());
+    
+    Assert.assertTrue(classPara.getLocalDef() == null);
+    
+    State state = classPara.getState();
+
+    // The following assertion is not satisfied
+    // since the VarDecl included in the SecondaryAttributes list
+    // is also written into the XML file and inserted as a Decl
+    // when read from the file.
+    // Actually, the provided AST should not be valid,
+    // but Jaxb does not catch this when validation is performed.
+    // For now uncommented ...
+
+    //    Assert.assertEquals(1, state.getDecl().size());
+
+    Assert.assertEquals(0, state.getPred().size());
+    
+    InitialState init = classPara.getInitialState();
+    Assert.assertEquals(1, init.getPred().size());
+    
+    List ops = classPara.getOperation();
+    Assert.assertEquals(NUM_OPS, ops.size());
+    
+    for (int i = 0; i < NUM_OP_BOXES; i++) {
+      Operation op = (Operation)ops.get(i);
+      Assert.assertEquals("op" + i, op.getName().getWord());
     }
   }
-  
+
   public void testJaxbAst()
     throws Exception
   {
@@ -236,106 +310,5 @@ public class OzAstTest extends TestCase {
       javax.xml.bind.JAXBContext.newInstance(jaxbContext);
     javax.xml.bind.Validator v = jc.createValidator();
     Assert.assertTrue(v.validate(state));
-  }
-  
-  public void testValid()
-  {
-    createAst();
-    AstValidator v = new JaxbValidator();
-    Assert.assertTrue(v.validate(mSpec));
-  }
-
-  public void testAstNumberOfSect() {
-    createAst();
-    numberOfSectTest();
-  }
-  
-  public void testXmlReadNumberOfSect() {
-    openXml();
-    numberOfSectTest();
-  }
-  
-  public void testAstClassDetails() {
-    createAst();
-    classDetailsTest();
-  }
-  
-  public void testXmlReadClassDetails() {
-    openXml();
-    classDetailsTest();
-  }
-  
-  public void testAstXmlWriter() {
-    createAst();
-    xmlWriterTest();
-  }
-  
-  public void testXmlReadXmlWriter() {
-    openXml();
-    xmlWriterTest();
-  }
-  
-  protected void numberOfSectTest() {
-    Assert.assertEquals(1, mSpec.getSect().size());
-  }
-  
-  protected void xmlWriterTest() {
-    XmlWriter writer = 
-      new JaxbXmlWriter(new AstToJaxb(), JaxbContext.PATH);
-    
-    try {
-      OutputStreamWriter outputStream
-	= new OutputStreamWriter(new FileOutputStream("MyClass.xml"), "utf8");
-      writer.write(mSpec, outputStream);
-    } catch (Throwable e) {
-      //    e.printStackTrace();
-      System.exit(-1);
-    }
-  }
-  
-  protected void classDetailsTest() {
-    List sects = mSpec.getSect();
-    ZSect firstSect = (ZSect)sects.get(0);
-    List paras = firstSect.getPara();
-    Iterator it = paras.iterator();
-    ClassPara classPara = (ClassPara)it.next();
-    
-    Assert.assertTrue(classPara.getName().getWord().equals("MyClass"));
-    
-    FormalParameters fp = classPara.getFormalParameters();
-    Assert.assertEquals(1, fp.getRefName().size());
-    
-    StringListType sList = classPara.getVisibilityList();
-    Assert.assertEquals(2, sList.getName().size());
-    
-    List inheritList = classPara.getInheritedClass();
-    Assert.assertEquals(0, inheritList.size());
-    
-    Assert.assertTrue(classPara.getLocalDef() == null);
-    
-    State state = classPara.getState();
-
-    // The following assertion is not satisfied
-    // since the VarDecl included in the SecondaryAttributes list
-    // is also written into the XML file and inserted as a Decl
-    // when read from the file.
-    // Actually, the provided AST should not be valid,
-    // but Jaxb does not catch this when validation is performed.
-    // For now uncommented ...
-
-    //    Assert.assertEquals(1, state.getDecl().size());
-
-    Assert.assertEquals(0, state.getPred().size());
-    
-    InitialState init = classPara.getInitialState();
-    Assert.assertEquals(1, init.getPred().size());
-    
-    List ops = classPara.getOperation();
-    Assert.assertEquals(NUM_OPS, ops.size());
-    
-    for (int i = 0; i < NUM_OP_BOXES; i++) {
-      Operation op = (Operation)ops.get(i);
-      Assert.assertEquals("op" + i, op.getName().getWord());
-    }
   }
 }
