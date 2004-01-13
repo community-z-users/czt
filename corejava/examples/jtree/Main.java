cf6343060582b8a65cbba8bfbb31a733e3d8b029
Petra Malik <petramalik@users.sourceforge.net>
2004-01-13 02:30:35 +0000

    INITIAL RELEASE   an example that shows how JTree can be used to represent an AST.

---
+++ corejava/examples/jtree/Main.java
@@ -0,0 +1,100 @@
+/**
+Copyright 2003 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import net.sourceforge.czt.z.jaxb.JaxbXmlReader;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.base.util.XmlReader;
+
+public class Main extends JPanel 
+  implements ActionListener
+{
+  AstTree tree;
+
+  public Main() {
+    super(new BorderLayout());
+
+    //Construct the tree.
+    tree = new AstTree(getAst());
+    JScrollPane scrollPane = new JScrollPane(tree);
+    scrollPane.setPreferredSize(new Dimension(200, 200));
+
+    //Add everything to this panel.
+    add(scrollPane, BorderLayout.CENTER);
+  }
+
+  /**
+   *  Constructs the genealogy graph used by the model.
+   */
+  public Term getAst()
+  {
+    String fileName = "../../../zml/examples/eg1.xml";
+    try {
+      XmlReader reader = new JaxbXmlReader();
+      Term term = reader.read(new java.io.File(fileName));
+      return term;
+    } catch( Exception e ) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  /**
+   * Create the GUI and show it.  For thread safety,
+   * this method should be invoked from the
+   * event-dispatching thread.
+   */
+  private static void createAndShowGUI() {
+    //Make sure we have nice window decorations.
+    JFrame.setDefaultLookAndFeelDecorated(true);
+
+    //Create and set up the window.
+    JFrame frame = new JFrame("Ast");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Create and set up the content pane.
+    Main newContentPane = new Main();
+    newContentPane.setOpaque(true); //content panes must be opaque
+    frame.setContentPane(newContentPane);
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent ae) {
+  }
+
+  public static void main(String[] args) {
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          createAndShowGUI();
+        }
+      });
+  }
+}
