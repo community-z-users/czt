996454812d0f98effb7f3540604c367aac15d3f0
cvs2svn <cvs2svn>
2004-02-08 05:02:32 +0000

    This commit was manufactured by cvs2svn to create tag 'OLD_PARSER'.

--- oz/parser/cup.sh
+++
@@ -1,9 +0,0 @@
-export OZ_JAVA_CUP=/usr/java/java_cup/
-export OZ_JFLEX=/usr/java/JFlex/lib/JFlex.jar
-
-export OZ_SRCDIR=src/net/sourceforge/czt/parser/oz
-
-cd $OZ_SRCDIR
-java -cp $OZ_JFLEX:$CLASSPATH JFlex.Main LatexScanner.flex
-java -cp $OZ_JAVA_CUP:$CLASSPATH java_cup.Main -parser LatexParser -symbols LatexSym < LatexParser.cup
-
