f9dc48147656c9f5a37e1e111ad5cf1c61a06d0c
Petra Malik <petramalik@users.sourceforge.net>
2004-02-23 22:01:58 +0000

    ADD   support for different encodings, namely utf16.

--- parser/src/net/sourceforge/czt/scanner/UnicodeToLatex.java
+++ parser/src/net/sourceforge/czt/scanner/UnicodeToLatex.java
@@ -6,15 +6,15 @@ public class UnicodeToLatex
 {
   static public void main(String[] args) {    
     String usage = "Usage: java net.sourceforge.czt.scanner.UnicodeToLatex"
-      + " [ -in <inputfile>] [ -out <outputfile>]";
+      + " [ -in <inputfile>] [ -out <outputfile>] [-enc <encoding>]";
     try {
-      InputStreamReader input = new InputStreamReader(System.in);
-      Writer writer = new PrintWriter(System.out);
+      InputStream instream = System.in;
+      Writer writer = new OutputStreamWriter(System.out);
+      String encoding = "utf8";
       for (int i = 0; i < args.length; i++) {
         if ("-in".equals(args[i])) {
           if (i < args.length) {
-            input =
-              new InputStreamReader(new FileInputStream(args[++i]), "utf8");
+            instream = new FileInputStream(args[++i]);
           } else {
             System.err.println(usage);
             return;
@@ -23,6 +23,10 @@ public class UnicodeToLatex
           if (i < args.length) {
             writer =
               new OutputStreamWriter(new FileOutputStream(args[++i]));
+          }
+        } else if ("-enc".equals(args[i])) {
+          if (i < args.length) {
+            encoding = args[++i];
           } else {
             System.err.println(usage);
             return;
@@ -32,7 +36,8 @@ public class UnicodeToLatex
           return;
         }
       }
-      Unicode2Latex parser = new Unicode2Latex(new UnicodeLexer(input));
+      InputStreamReader reader = new InputStreamReader(instream, encoding);
+      Unicode2Latex parser = new Unicode2Latex(new UnicodeLexer(reader));
       parser.setWriter(writer);
       Object result = parser.parse().value;      
       writer.close();
