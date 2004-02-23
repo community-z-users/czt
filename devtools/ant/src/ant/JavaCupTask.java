d01064f26d5021e877de9c930c56fd1452ded31f
Tim Miller <timmiller@users.sourceforge.net>
2004-02-23 23:28:03 +0000

    Updated java_cup ant task to support versions of java_cup < 1.0k

--- devtools/ant/src/ant/JavaCupTask.java
+++ devtools/ant/src/ant/JavaCupTask.java
@@ -12,7 +12,6 @@ import java_cup.*;
  */
 public class JavaCupTask
 {
-
   //the file from which to read the grammar
   private String inputFile_;
 
@@ -26,7 +25,7 @@ public class JavaCupTask
   private String srcDir_;
 
   //the destination directory
-  private String destDir_;
+  private String destDir_;  
 
   public void execute() throws Exception
   {
@@ -63,27 +62,63 @@ public class JavaCupTask
       File fInputFile = new File(inputFileFull);
       File fParserFile = new File(parserFileFull + ".java");
       File fSymbolFile = new File(symbolFileFull + ".java");
-
+      
       //only regenerate the parser is the CUP file has changed
       if (fInputFile.lastModified() > fParserFile.lastModified()) {
-        String [] args =
-	  new String [] {"-parser", parserFile_,
-                         "-symbols", symbolFile_,
-			 inputFileFull};
+	String [] cmdarray =
+	  new String [] {
+	    "java", "java_cup.Main",
+	    "-parser", parserFile_,
+	    "-symbols", symbolFile_,
+	  };
+	
+	//call CUP
+	Process p = Runtime.getRuntime().exec(cmdarray);
+	
+	InputStream is = new FileInputStream(inputFileFull);
+	
+	StreamWriter sw = new StreamWriter(p.getOutputStream(), is, this);
+	StreamReader sr = new StreamReader(p.getErrorStream(), System.err, this);
+	StreamReader so = new StreamReader(p.getInputStream(), System.out, this);
 
-        //call CUP. This is dodgy...
-	java_cup.Main.main(args);
+	//write to the process and read back the output
+	sw.write();
+	sr.read();
+	so.read();
 
-        //move the files
-        String intermediateParserFile = new String();
-        String intermediateSymbolFile = new String();
-        intermediateParserFile += parserFile_ + ".java";
-        intermediateSymbolFile += symbolFile_ + ".java";
+	//wait for the process to finish executing
+	p.waitFor();
 
-        boolean pMoved = 
+	//move the files
+	String intermediateParserFile = new String();
+	String intermediateSymbolFile = new String();
+	intermediateParserFile += parserFile_ + ".java";
+	intermediateSymbolFile += symbolFile_ + ".java";
+	
+	boolean pMoved = 
 	  new File(intermediateParserFile).renameTo(fParserFile);
-        boolean sMoved =
+	boolean sMoved =
 	  new File(intermediateSymbolFile).renameTo(fSymbolFile);
+	
+	if (!pMoved) {
+	  String error = "Parser could not be written to " +
+	    fParserFile.getAbsolutePath();
+	  throw new Exception(error);
+	}
+	else {
+	  System.out.println("Parser written to " +
+			     fParserFile.getAbsolutePath());
+	}
+
+	if (!sMoved) {
+	  String error = "Symbols could not be written to " +
+	    fSymbolFile.getAbsolutePath();
+	  throw new Exception(error);
+	}
+	else {
+	  System.out.println("Symbols written to " +
+			     fSymbolFile.getAbsolutePath());
+	}
       }
     }
     catch (Exception e) {
@@ -118,3 +153,80 @@ public class JavaCupTask
   }
 }
 
+class StreamReader
+{
+  //the output stream of the process
+  private InputStream pOut_;
+
+  //the stream to which to send any output
+  private OutputStream write_;
+
+  //the calling JavaCupTask object
+  private JavaCupTask caller_;
+
+  StreamReader(InputStream out, OutputStream write, JavaCupTask caller)
+  {
+    pOut_ = out;
+    write_ = write;
+    caller_ = caller;
+  }
+
+  public void read()
+  {
+    try
+    {
+      if (pOut_.available() > 0) {
+	int c = pOut_.read();
+	while (pOut_.available() > 0) {
+	  write_.write(c);
+	  c = pOut_.read();
+	}
+      }
+      write_.flush();
+      write_.close();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+class StreamWriter
+{
+  //the input stream of the process
+  //declared as an OutputStream because we write to it
+  private OutputStream pIn_;
+
+  //the stream from which to read data and send to the process's 
+  //input stream
+  private InputStream read_;
+
+  //the calling JavaCupTask object
+  private JavaCupTask caller_; 
+
+  StreamWriter(OutputStream in, InputStream read, JavaCupTask caller)
+  {
+    pIn_ = in;
+    read_ = read;
+    caller_ = caller;
+  }
+  
+  public synchronized void write()
+  {
+    try
+    {
+      //wait for the input stream to be assigned
+      int c = read_.read();
+      while (c >= 0) {
+        pIn_.write(c);
+	pIn_.flush();
+	c = read_.read();
+      }
+      pIn_.flush();
+      pIn_.close();
+    }
+    catch (Exception e) {
+      e.printStackTrace();  
+    }
+  }
+}
