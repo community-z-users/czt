26bf97b613f226ce20a74e0a7e6b22945ccf6194
Petra Malik <petramalik@users.sourceforge.net>
2004-02-24 01:57:10 +0000

    INITIAL RELEASE:   a scanner that forwards tokens but removes some of the NL tokens.

---
+++ parser/src/net/sourceforge/czt/scanner/NewlineScanner.java
@@ -0,0 +1,64 @@
+/**
+Copyright 2003 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.scanner;
+
+import java_cup.runtime.Scanner;
+import java_cup.runtime.Symbol;
+
+/**
+ * This scanner gets token from another scanner and provides them
+ * to the parser, but removes some of the NL tokens.
+ *
+ * @author Petra Malik
+ */
+public class NewlineScanner
+  implements Scanner
+{
+  Scanner scanner_;
+  Symbol lastToken_;
+  Symbol lastButOneToken_;
+
+  public NewlineScanner(Scanner scanner)
+  {
+    scanner_ = scanner;
+  }
+
+  public Symbol next_token()
+    throws Exception
+  {
+    Symbol symbol = scanner_.next_token();
+    if (symbol.sym == sym.NL) {
+      int last = lastToken_.sym;
+      int lastButOne = lastButOneToken_.sym;
+      boolean lastTokenIsBox = sym.ZED == last || sym.AX == last || sym.GENAX == last
+        || sym.GENSCH == last;
+      boolean lastIsWhere = sym.BAR == last;
+      boolean lastButOneIsSch = sym.SCH == lastButOne;
+      if (lastTokenIsBox || lastIsWhere || lastButOneIsSch) {
+        while (symbol.sym == sym.NL) {
+          symbol = scanner_.next_token();
+        }
+      }
+    }
+    lastButOneToken_ = lastToken_;
+    lastToken_ = symbol;
+    return symbol;
+  }
+}
