dff29ebbf9567c17ede9553b5692463334f76c2f
Petra Malik <petramalik@users.sourceforge.net>
2004-03-08 21:20:12 +0000

    MOVE   parser/src/net/sourceforge/czt/parser/oz/Parser.cup to   parser/templates/Parser.xml   to prepare the generation of several kinds of parser   (z, oz, tcoz, etc.).   Some tags (like add:z, add:oz) are added around the parser code,   which are used to identify for which parser the code is to be used.   The build script is updated to build the cup file automatically from   the new template file.

--- parser/cup.sh
+++ parser/cup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Make sure to run 'ant install-bin' in CZT_HOME
+# (this will generate ../bin/settings.sh) and
+# 'ant prepare-cup' in this directory before using this script.
+#
 # This works with JavaCup 0.10j, which is the version that
 # is included in J2SDK 1.4.2.  (It has some peculiar features,
 # that are fixed in JavaCup 0.10k, like only reading the .cup file 
@@ -7,11 +11,11 @@
 source ../bin/settings.sh
 BASEDIR=`pwd`
 
-cd ${BASEDIR}/src/net/sourceforge/czt/parser/z
+cd ${BASEDIR}/build/src/net/sourceforge/czt/parser/z
 
-java -cp ${JAVA_CUP} java_cup.Main -parser LTZparser -symbols LTZsym <LTZparser.cup
+java -cp ${JAVA_CUP} java_cup.Main -parser Parser -symbols Sym < Parser.cup
 
-cd ${BASEDIR}/src/net/sourceforge/czt/parser/oz
+cd ${BASEDIR}/build/src/net/sourceforge/czt/parser/oz
 
 java -cp ${JAVA_CUP} java_cup.Main -parser Parser -symbols Sym < Parser.cup
 
