87bc58b59dd61c56efd70ea53128433d80c9010a
Petra Malik <petramalik@users.sourceforge.net>
2004-03-17 21:21:52 +0000

    BUGFIX:   Use the Latex2Unicode instead of LatexToUnicode (which is not generated   any more) and update the classpath.

--- bin/latex2unicode.bat
+++ bin/latex2unicode.bat
@@ -1,2 +1,2 @@
 call settings.bat
-java -classpath "%PARSER%;%COREJAVA_BASE%" net.sourceforge.czt.scanner.LatexToUnicode %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%PARSER%;%JAVA_CUP%;%COREJAVA%" net.sourceforge.czt.scanner.Latex2Unicode %1 %2 %3 %4 %5 %6 %7 %8 %9
