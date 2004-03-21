8e0bbed8cf07b20e4766800c1a48ad1222d3fb23
Petra Malik <petramalik@users.sourceforge.net>
2004-03-21 21:54:52 +0000

    MOVE   parser/src/net/sourceforge/czt/scanner/LatexCommand.java and   parser/src/net/sourceforge/czt/scanner/LatexMarkupUtils.java to   src/net/sourceforge/czt/parser/util/ since it is a more appropriate   location for those classes.

--- parser/src/net/sourceforge/czt/parser/util/LatexCommand.java
+++ parser/src/net/sourceforge/czt/parser/util/LatexCommand.java
@@ -17,7 +17,7 @@ along with czt; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package net.sourceforge.czt.scanner;
+package net.sourceforge.czt.parser.util;
 
 /**
  * A latex command for Zed markup together with its rendering information.
