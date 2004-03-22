3291d85d10cf2531b1aaa9e866687c4171f03c35
Petra Malik <petramalik@users.sourceforge.net>
2004-03-22 00:54:50 +0000

    UPDATE:   shell scripts to the new package structure in parser.

--- bin/latex2unicode.bat
+++ bin/latex2unicode.bat
@@ -1,2 +1,2 @@
 call settings.bat
-java -classpath "%PARSER%;%JAVA_CUP%;%COREJAVA%" net.sourceforge.czt.scanner.Latex2Unicode %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%PARSER%;%JAVA_CUP%;%COREJAVA%" net.sourceforge.czt.parser.z.Latex2Unicode %1 %2 %3 %4 %5 %6 %7 %8 %9
--- bin/unicode2latex.bat
+++ bin/unicode2latex.bat
@@ -1,2 +1,2 @@
 call settings.bat
-java -classpath "%PARSER%;%COREJAVA_BASE%;%JAVA_CUP%" net.sourceforge.czt.scanner.UnicodeToLatex %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%PARSER%;%COREJAVA%;%JAVA_CUP%" net.sourceforge.czt.print.z.UnicodeToLatex %1 %2 %3 %4 %5 %6 %7 %8 %9
