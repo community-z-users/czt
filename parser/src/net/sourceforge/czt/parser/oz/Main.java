3078f189ca5b1ccd9ee3af3690a1215812a3bd51
Tim Miller <timmiller@users.sourceforge.net>
2004-03-30 01:01:34 +0000

    Improved the parser. It now parses a library file only if that library file is specified as a parent. The parser tries to find a parent specification in a file of the same name as the parent, firstly in the same directory as the section, then at the path specified by the environment variable CZT_LIB, which is passed to the program using the -D option (see run.sh).

--- parser/src/net/sourceforge/czt/parser/oz/Main.java
+++ parser/src/net/sourceforge/czt/parser/oz/Main.java
@@ -60,7 +60,7 @@ public class Main extends JPanel implements ActionListener
     super(new BorderLayout());
 
     List libFiles = new ArrayList();
-
+    /*
     libFiles.add("lib/prelude.tex");
     libFiles.add("lib/set_toolkit.tex");
     libFiles.add("lib/relation_toolkit.tex");
@@ -68,7 +68,8 @@ public class Main extends JPanel implements ActionListener
     libFiles.add("lib/number_toolkit.tex");
     libFiles.add("lib/sequence_toolkit.tex");
     libFiles.add("lib/standard_toolkit.tex");
-    libFiles.add("lib/oz_toolkit.tex");    
+    libFiles.add("lib/oz_toolkit.tex");
+    */
 
     for (Iterator iter = libFiles.iterator(); iter.hasNext(); ) {
       String file = (String) iter.next();
