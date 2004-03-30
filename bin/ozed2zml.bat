3783bd796e796c9cc8b958ab637460a40a13f53f
Petra Malik <petramalik@users.sourceforge.net>
2004-03-30 22:16:46 +0000

    ADD:   call java with -Dczt.lib=... to set the path to the toolkit library.

--- bin/ozed2zml.bat
+++ bin/ozed2zml.bat
@@ -1,2 +1,2 @@
 call settings.bat
-java -classpath "%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" net.sourceforge.czt.parser.oz.ParseUtils %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib net.sourceforge.czt.parser.oz.ParseUtils %1 %2 %3 %4 %5 %6 %7 %8 %9
