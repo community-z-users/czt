28292c20d694a3922f0d8a2ec366567622719c5c
cvs2svn <cvs2svn>
2004-04-07 23:21:59 +0000

    This commit was manufactured by cvs2svn to create tag 'corejava_1_2_0'.

--- bin/ozed2zml.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %~f0\..\settings.bat
-java -classpath "%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib net.sourceforge.czt.parser.oz.ParseUtils %1 %2 %3 %4 %5 %6 %7 %8 %9
--- parser/run.sh
+++
@@ -1,3 +0,0 @@
-source ../bin/settings.sh
-
-java -cp $COREJAVA:$JWSDP:$PARSER:$JAVA_CUP -Dczt.lib=$CZT_LIB net.sourceforge.czt.parser.oz.Main $*
