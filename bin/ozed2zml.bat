2b8d01ce47af8c3a385c771ca3608f2d790412ba
Petra Malik <petramalik@users.sourceforge.net>
2004-04-21 05:18:14 +0000

    MODIFY:   use the new class LatexParser instead of ParseUtils.

--- bin/ozed2zml.bat
+++ bin/ozed2zml.bat
@@ -1,3 +1,3 @@
 @echo off
 call %~f0\..\settings.bat
-java -classpath "%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib net.sourceforge.czt.parser.oz.ParseUtils %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib net.sourceforge.czt.parser.oz.LatexParser %1 %2 %3 %4 %5 %6 %7 %8 %9
