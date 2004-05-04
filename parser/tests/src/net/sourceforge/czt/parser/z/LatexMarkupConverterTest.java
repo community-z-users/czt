16d9207ae9071140ea77f39c163ca1d79fe0f116
Petra Malik <petramalik@users.sourceforge.net>
2004-05-04 00:26:12 +0000

    INITIAL RELEASE:   latex markup converter test.

---
+++ parser/tests/src/net/sourceforge/czt/parser/z/LatexMarkupConverterTest.java
@@ -0,0 +1,78 @@
+/**
+Copyright (C) 2004 Petra Malik
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.parser.z;
+
+import java.io.*;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.parser.util.AbstractParserTest;
+import net.sourceforge.czt.print.z.UnicodeToLatex;
+import net.sourceforge.czt.session.SectionManager;
+import net.sourceforge.czt.util.ParseException;
+
+/**
+ * A (JUnit) test class for testing the latex markup converter.
+ *
+ * @author Petra Malik
+ */
+public class LatexMarkupConverterTest
+  extends AbstractParserTest
+{
+  public static Test suite()
+  {
+    return new TestSuite(LatexMarkupConverterTest.class);
+  }
+
+  public Term parse(String filename, SectionManager manager)
+    throws ParseException, FileNotFoundException
+  {
+    try {
+      String test = new File(filename).getName();
+      File tmpUnicodeFile = File.createTempFile("cztLatexMarkup", test + ".utf8");
+      tmpUnicodeFile.deleteOnExit();
+      File tmpLatexFile = File.createTempFile("cztLatexMarkup", test + ".tex");
+      tmpLatexFile.deleteOnExit();
+      String uniFile = tmpUnicodeFile.getAbsolutePath();
+      String latexFile = tmpLatexFile.getAbsolutePath();
+      if (filename.endsWith(".tex") || filename.endsWith(".TEX")) {
+        String[] args1 = { "-in", filename, "-out", uniFile };
+        LatexToUnicode.main(args1);
+        String[] args2 = { "-in", uniFile, "-out", latexFile };
+        UnicodeToLatex.main(args2);
+        return ParseUtils.parse(tmpLatexFile.getAbsolutePath(), manager_);
+      }
+      else if(filename.endsWith(".utf8") || filename.endsWith(".UTF8")) {
+        String[] args1 = { "-in", filename, "-out", latexFile };
+        UnicodeToLatex.main(args1);
+        String[] args2 = { "-in", latexFile, "-out", uniFile };
+        LatexToUnicode.main(args2);
+        return ParseUtils.parse(tmpLatexFile.getAbsolutePath(), manager_);
+      }
+      return ParseUtils.parse(filename, manager_);
+    }
+    catch (IOException e) {
+      fail("Should not throw IOException");
+      return null;
+    }
+  }
+}
