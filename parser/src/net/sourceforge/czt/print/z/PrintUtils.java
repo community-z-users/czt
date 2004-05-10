62e8f230f4f868701788bf59337a891d175f086a
Petra Malik <petramalik@users.sourceforge.net>
2004-05-10 00:13:50 +0000

    INITIAL RELEASE:   utilities for printing Z specifications.

---
+++ parser/src/net/sourceforge/czt/print/z/PrintUtils.java
@@ -0,0 +1,64 @@
+/**
+Copyright (C) 2004 Petra Malik
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.print.z;
+
+import java.io.Writer;
+
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.util.CztException;
+
+public final class PrintUtils
+{
+  /**
+   * Do not create instances of this class.
+   */
+  private PrintUtils()
+  {
+  }
+
+  public static void printLatex(Term term, Writer out)
+  {
+    EqualPredVisitor visitor = new EqualPredVisitor();
+    Term t = (Term) term.accept(visitor);
+    ZmlScanner scanner = new ZmlScanner(t);
+    Unicode2Latex parser = new Unicode2Latex(scanner);
+    UnicodePrinter printer = new UnicodePrinter(out);
+    parser.setWriter(printer);
+    try {
+      parser.parse();
+    }
+    catch(Exception e) {
+      printer.close();
+      throw new CztException(e);
+    }
+    printer.close();
+  }
+
+  public static void printUnicode(Term term, Writer out)
+    throws Exception
+  {
+    EqualPredVisitor visitor = new EqualPredVisitor();
+    Term t = (Term) term.accept(visitor);
+    ZmlScanner scanner = new ZmlScanner(t);
+    UnicodePrinter printer = new UnicodePrinter(out);
+    printer.printZed(scanner);
+    printer.close();
+  }
+}
