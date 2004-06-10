8e23b44ca7b312690fdad294fa2ad9e9e4b8b9b2
Rohit Bansal <rohitbansal@users.sourceforge.net>
2004-06-10 02:45:34 +0000

    MODIFY : Implemented the Tests for FlatConst.java and FlatNegate.java

---
+++ zlive/tests/src/net/sourceforge/czt/animation/eval/flatpred/FlatNegateTest.java
@@ -0,0 +1,127 @@
+/**
+Copyright (C) 2004 Mark Utting
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.animation.eval.flatpred;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.math.*;
+
+import junit.framework.*;
+
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.z.ast.*;
+import net.sourceforge.czt.parser.z.ParseUtils;
+import net.sourceforge.czt.session.SectionManager;
+import net.sourceforge.czt.util.CztException;
+import net.sourceforge.czt.util.ParseException;
+import net.sourceforge.czt.animation.eval.*;
+import net.sourceforge.czt.animation.eval.flatpred.*;
+
+
+/**
+ * A (JUnit) test class for testing the Animator
+ *
+ * @author Mark Utting
+ */
+public class FlatNegateTest
+  extends TestCase
+{
+  private ZFactory factory_ = new net.sourceforge.czt.z.impl.ZFactoryImpl();
+
+  private final double ACCURACY = 0.01;
+  private List emptyList = new ArrayList();
+  private Envir empty = new Envir();
+  private BigInteger a = new BigInteger("10");
+  private BigInteger b = new BigInteger("20");
+  private BigInteger c = a.negate();
+  private RefName x = factory_.createRefName("x",emptyList,null);
+  private RefName y = factory_.createRefName("y",emptyList,null);
+  private Expr i10 = factory_.createNumExpr(a);
+  private Expr i20 = factory_.createNumExpr(b);
+  private Expr in10 = factory_.createNumExpr(c);
+  private FlatPred pred = new FlatNegate(x,y);
+
+  public void testEmpty()
+  {
+    Assert.assertNull("should not return a mode", pred.chooseMode(empty));
+  }
+
+  public void testII()
+  {
+    Envir envX = empty.add(x,i10);
+    Envir envXY = envX.add(y,in10);
+    Mode m = pred.chooseMode(envXY);
+    Assert.assertTrue(m != null);
+    Assert.assertEquals(2, m.getNumArgs());
+    Assert.assertEquals(true, m.isInput(0));
+    Assert.assertEquals(true, m.isInput(1));
+    Assert.assertEquals(0.5, m.getSolutions(), ACCURACY);
+    pred.setMode(m);
+    // Start a evaluation which succeeds:  negate(10) = -10
+    pred.startEvaluation();
+    Assert.assertTrue(pred.nextEvaluation());
+    Assert.assertEquals("result value", in10, m.getEnvir().lookup(y));
+    Assert.assertFalse(pred.nextEvaluation());
+    // Start a evaluation which fails:  negate(10) = -20
+    pred.getMode().getEnvir().setValue(y, i20);  // updates the environment
+    pred.startEvaluation();
+    Assert.assertFalse(pred.nextEvaluation());
+  }
+
+  public void testIO()
+  {
+    Envir envX = empty.add(x,i10);
+    Assert.assertTrue(envX.lookup(y) == null);
+    Mode m = pred.chooseMode(envX);
+    Assert.assertTrue(m != null);
+    Assert.assertEquals(2, m.getNumArgs());
+    Assert.assertEquals(true, m.isInput(0));
+    Assert.assertEquals(false, m.isInput(1));
+    Assert.assertTrue(m.getEnvir().lookup(y) != null);
+    Assert.assertEquals(1.0, m.getSolutions(), ACCURACY);
+    pred.setMode(m);
+    pred.startEvaluation();
+    Assert.assertTrue(pred.nextEvaluation());
+    Assert.assertEquals("result value", in10, m.getEnvir().lookup(y));
+    Assert.assertFalse(pred.nextEvaluation());
+  }
+
+  public void testOI()
+  {
+    Envir envY = empty.add(y,in10);
+    Mode m = pred.chooseMode(envY);
+    Assert.assertTrue(m != null);
+    Assert.assertEquals(2, m.getNumArgs());
+    Assert.assertEquals(false, m.isInput(0));
+    Assert.assertEquals(true, m.isInput(1));
+    Assert.assertTrue(m.getEnvir().lookup(x) != null);
+    Assert.assertEquals(1.0, m.getSolutions(), ACCURACY);
+    pred.setMode(m);
+    pred.startEvaluation();
+    Assert.assertTrue(pred.nextEvaluation());
+    Assert.assertEquals("result value", i10, m.getEnvir().lookup(x));
+    Assert.assertFalse(pred.nextEvaluation());
+  }
+}
+
+
+
+
