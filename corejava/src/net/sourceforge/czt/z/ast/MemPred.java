d1191b9aa1658265e64539f97fae28d08583cee7
Petra Malik <petramalik@users.sourceforge.net>
2004-06-17 03:42:34 +0000

    REMOVE:   method MemPred.isEquality() since it is not clear what it is supposed   to do.

--- corejava/src/net/sourceforge/czt/z/ast/MemPred.java
+++ corejava/src/net/sourceforge/czt/z/ast/MemPred.java
@@ -90,12 +90,4 @@ public interface MemPred extends Pred
    * @see #getMixfix
    */
   void setMixfix(Boolean mixfix);
-
-  /**
-   * Returns whether this MemPred represents an equality.
-   * This is the case iff mixfix is true and the
-   * right expression is an instance of SetExpr containing
-   * just one expression.
-   */
-  boolean isEquality();
 }
--- gnast/src/vm/MemPred.java
+++
@@ -1,8 +0,0 @@
-
-  /**
-   * Returns whether this MemPred represents an equality.
-   * This is the case iff mixfix is true and the
-   * right expression is an instance of SetExpr containing
-   * just one expression.
-   */
-  boolean isEquality();
