722c55b0e1726d20c89efb462afe54743aaebe73
Petra Malik <petramalik@users.sourceforge.net>
2004-06-21 04:22:00 +0000

    MODIFY:   call the OperatorName constructor with this instead of only the word.

--- gnast/src/vm/NameImpl.java
+++ gnast/src/vm/NameImpl.java
@@ -13,7 +13,7 @@
   public net.sourceforge.czt.z.util.OperatorName getOperatorName()
   {
     try {
-      return new net.sourceforge.czt.z.util.OperatorName(getWord());
+      return new net.sourceforge.czt.z.util.OperatorName(this);
     }
     catch(net.sourceforge.czt.z.util.OperatorName.OperatorNameException e) {
       return null;
