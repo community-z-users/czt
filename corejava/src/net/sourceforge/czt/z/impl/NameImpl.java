d5a2026b684f1da5f9d82ae73d5979cf31e35d98
Petra Malik <petramalik@users.sourceforge.net>
2004-06-21 04:24:05 +0000

    MODIFY:   use the new constructor of OperatorName that takes a Name.

--- corejava/src/net/sourceforge/czt/z/impl/NameImpl.java
+++ corejava/src/net/sourceforge/czt/z/impl/NameImpl.java
@@ -149,7 +149,7 @@ public abstract class NameImpl
   public net.sourceforge.czt.z.util.OperatorName getOperatorName()
   {
     try {
-      return new net.sourceforge.czt.z.util.OperatorName(getWord());
+      return new net.sourceforge.czt.z.util.OperatorName(this);
     }
     catch(net.sourceforge.czt.z.util.OperatorName.OperatorNameException e) {
       return null;
