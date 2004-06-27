c92a48f5ccb648d61bed7ad2109e02034629ef22
Petra Malik <petramalik@users.sourceforge.net>
2004-06-27 21:18:23 +0000

    MODIFY:   add ZLive to the classpath.

--- bin/beanshell.bat
+++ bin/beanshell.bat
@@ -1,3 +1,3 @@
 @echo off
 call %~f0\..\settings.bat
-java -classpath "%~f0\..;%BSH%;%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib bsh.Console %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~f0\..;%BSH%;%ZLIVE%;%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" -Dczt.lib=%~f0\..\..\parser\lib bsh.Console %1 %2 %3 %4 %5 %6 %7 %8 %9
