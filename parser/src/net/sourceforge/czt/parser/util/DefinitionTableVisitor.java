25a8cd1d9189db0fc59caf402a4d6f56141c768f
Petra Malik <petramalik@users.sourceforge.net>
2004-06-29 23:08:37 +0000

    INITIAL RELEASE:   initial support for definition tables.

---
+++ parser/src/net/sourceforge/czt/parser/util/DefinitionTableVisitor.java
@@ -0,0 +1,122 @@
+/**
+Copyright (C) 2004 Petra Malik
+This file is part of the czt project.
+
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.parser.util;
+
+import java.util.*;
+
+import net.sourceforge.czt.base.ast.*;
+import net.sourceforge.czt.base.visitor.*;
+import net.sourceforge.czt.session.*;
+import net.sourceforge.czt.util.*;
+import net.sourceforge.czt.z.ast.*;
+import net.sourceforge.czt.z.visitor.*;
+
+public class DefinitionTableVisitor
+  implements TermVisitor,
+             ListTermVisitor,
+             AxParaVisitor,
+             ParaVisitor,
+             ZSectVisitor
+{
+  DefinitionTable table_;
+  SectionManager manager_;
+
+  public DefinitionTableVisitor(SectionManager manager)
+  {
+    manager_ = manager;
+  }
+
+  public DefinitionTable getDefinitionTable()
+  {
+    return table_;
+  }
+
+  public Object visitTerm(Term term)
+  {
+    final String message = "DefinitionTables can only be build for ZSects; " +
+      "was tried for " + term.getClass();
+    throw new UnsupportedOperationException(message);
+  }
+
+  public Object visitListTerm(ListTerm listTerm)
+  {
+    for (Iterator iter = listTerm.iterator(); iter.hasNext(); ) {
+      Object o = iter.next();
+      if (o instanceof Term) {
+        Term t = (Term) o;
+        visit(t);
+      }
+    }
+    return null;
+  }
+
+  public Object visitAxPara(AxPara axPara)
+  {
+    SchText schText = axPara.getSchText();
+    List decls = schText.getDecl();
+    for (Iterator iter = decls.iterator(); iter.hasNext(); ) {
+      Decl decl = (Decl) iter.next();
+      if (decl instanceof ConstDecl) {
+        ConstDecl constDecl = (ConstDecl) decl;
+        String name = constDecl.getDeclName().toString();
+        try {
+          table_.add(name, constDecl.getExpr());
+        }
+        catch (Exception e) {
+          throw new CztException(e);
+        }
+      }
+    }
+    return null;
+  }
+
+  public Object visitPara(Para para)
+  {
+    return null;
+  }
+
+  public Object visitZSect(ZSect zSect)
+  {
+    final String name = zSect.getName();
+    List parentTables = new ArrayList();
+    for (Iterator iter = zSect.getParent().iterator(); iter.hasNext(); ) {
+      Parent parent = (Parent) iter.next();
+      DefinitionTable parentTable =
+        manager_.getDefinitionTable(parent.getWord());
+      if (parentTable != null) {
+        parentTables.add(parentTable);
+      }
+    }
+    try {
+      table_ = new DefinitionTable(name, parentTables);
+    }
+    catch (Exception e)
+    {
+      throw new CztException(e);
+    }
+    visit(zSect.getPara());
+    return null;
+  }
+
+  protected void visit(Term term)
+  {
+    term.accept(this);
+  }
+}
