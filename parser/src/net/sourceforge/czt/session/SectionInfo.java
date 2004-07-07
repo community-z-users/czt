8497adb8ab01a9708063ca0d38a712cec13c99f4
Petra Malik <petramalik@users.sourceforge.net>
2004-07-07 22:44:22 +0000

    ADD:   javadoc documenation.

--- parser/src/net/sourceforge/czt/session/SectionInfo.java
+++ parser/src/net/sourceforge/czt/session/SectionInfo.java
@@ -19,7 +19,27 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package net.sourceforge.czt.session;
 
+/**
+ * <p>Provides information about sections.</p>
+ *
+ * <p>This interface provides a generic and extensible way
+ * for applications to ask for all kinds of informations about
+ * sections.  Sections are identified by its name, which should
+ * be unique.  A SectionInfo object can provide a fixed set
+ * of information, or can be generic in a way that classes
+ * providing information about sections register there service.</p>
+ *
+ * <p>A SectionInfo object can cache the information provided,
+ * but care should be taken when implementing such caches when
+ * the information provided is mutable.</p>
+ */
 public interface SectionInfo
 {
+  /**
+   * Returns either an object of type <code>infoType</code>,
+   * if that particular information is available for the section
+   * with name <code>sectionName</code>, or <code>null</code>
+   * if this kind of information is not available.
+   */
   Object getInfo(String sectionName, Class infoType);
 }
