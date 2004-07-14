969a4ff508b0cf984cfb10153031787a34d28366
Rohit Bansal <rohitbansal@users.sourceforge.net>
2004-07-14 03:07:29 +0000

    ADD: Added the equals() method to compare equality of two EvalSets

--- zlive/src/net/sourceforge/czt/animation/eval/EvalSet.java
+++ zlive/src/net/sourceforge/czt/animation/eval/EvalSet.java
@@ -28,26 +28,33 @@ import net.sourceforge.czt.z.ast.Expr;
  * This defines the interface to all different kinds of set objects.
  */
 public interface EvalSet extends Expr {
-  
+
   /** A list of all the free variables that this set depends upon.
    * @return The free variables.
    */
   public List freeVars();
-  
+
   /** Estimate the size of the set. */
   //@ requires getEnvir() != null;
   public double estSize();
-  
+
   /** Iterate through all members of the set.
    *  It guarantees that there will be no duplicates.
-   * 
+   *
    * @return an Iterator object.
    */
   public Iterator members();
-  
+
   /** Tests for membership of the set.
    * @param e  The fully evaluated expression.
    * @return   true iff e is a member of the set.
    */
   public boolean isMember(Expr e);
+
+  /**Tests for the equality of any two sets.
+     Here, the equality is based upon both the sets
+     having the same elements, not taking into consideration
+     the duplication of elements.
+   */
+  public boolean equals(Object o);
 }
