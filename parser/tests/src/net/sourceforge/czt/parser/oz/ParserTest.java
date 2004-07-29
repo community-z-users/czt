a6d28de861f1e40edc1fa1bc0043aaa745a5c08d
Petra Malik <petramalik@users.sourceforge.net>
2004-07-29 21:52:25 +0000

    ADD:   first OZ test case simple1.

--- parser/tests/src/net/sourceforge/czt/parser/oz/ParserTest.java
+++ parser/tests/src/net/sourceforge/czt/parser/oz/ParserTest.java
@@ -1,35 +1,36 @@
-/**
-Copyright (C) 2004 Petra Malik
-This file is part of the czt project.
+/*
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.parser.oz;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import junit.framework.*;
 
 import net.sourceforge.czt.base.ast.Term;
-import net.sourceforge.czt.parser.util.AbstractParserFailTest;
-import net.sourceforge.czt.parser.util.AbstractParserTest;
+import net.sourceforge.czt.oz.jaxb.*;
+import net.sourceforge.czt.parser.util.*;
 import net.sourceforge.czt.session.SectionManager;
-import net.sourceforge.czt.util.ParseException;
+import net.sourceforge.czt.util.*;
+import net.sourceforge.czt.z.ast.*;
+import net.sourceforge.czt.z.util.*;
 
 /**
  * A (JUnit) test class for testing the Z parser.
@@ -65,10 +66,53 @@ public class ParserTest
     }
   }
 
+  public void testOzSimple1()
+  {
+    compareOz(getOzTestExample("simple1.tex"),
+              getOzTestExample("simple1.xml"));
+  }
+
   public Term parse(URL url, SectionManager manager)
     throws ParseException, IOException
   {
     return ParseUtils.parse(url, manager);
+  }
+
+  public void compareOz(URL url, URL zmlURL)
+  {
+    try {
+      JaxbXmlReader reader = new JaxbXmlReader();
+      Visitor visitor = new DeleteNarrVisitor();
+      Spec zmlSpec = (Spec) reader.read(zmlURL.openStream()).accept(visitor);
+      Spec parsedSpec =
+        (Spec) parse(url, manager_).accept(visitor);
+      visitor = new DeleteMarkupParaVisitor();
+      parsedSpec = (Spec) parsedSpec.accept(visitor);
+      zmlSpec = (Spec) zmlSpec.accept(visitor);
+      JaxbValidator validator = new JaxbValidator();
+      Assert.assertTrue(validator.validate(parsedSpec));
+      Assert.assertTrue(validator.validate(zmlSpec));
+      if (! zmlSpec.equals(parsedSpec)) {
+        String message = "For " + url.toString();
+        JaxbXmlWriter xmlWriter = new JaxbXmlWriter();
+        File expected = File.createTempFile("cztParser", "test.zml");
+        Writer out =
+          new OutputStreamWriter(new FileOutputStream(expected), "UTF-8");
+        xmlWriter.write(zmlSpec, out);
+        out.close();
+        message += "\nexpected: " + expected.getAbsolutePath();
+        File got = File.createTempFile("cztParser", "test.zml");
+        out = new OutputStreamWriter(new FileOutputStream(got), "UTF-8");
+        xmlWriter.write(parsedSpec, out);
+        out.close();
+        message += "\nbut was:" + got.getAbsolutePath();
+        fail(message);
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      fail("Should not throw exception " + e);
+    }
   }
 
   /**
