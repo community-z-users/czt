a5a0457e06eaf17f1baa7007d922b05a9f20aca4
Petra Malik <petramalik@users.sourceforge.net>
2004-08-18 04:02:56 +0000

    REFACTORING:   Rename the inner class LookaheadScanner of SectHeadScanner to TokenStack   and move it into package net.sourceforge.czt.parser.util so that other   scanners can use it as well.

---
+++ parser/src/net/sourceforge/czt/parser/util/TokenStack.java
@@ -0,0 +1,64 @@
+/*
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
+
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.parser.util;
+
+import java.util.Stack;
+
+import java_cup.runtime.Scanner;
+import java_cup.runtime.Symbol;
+
+/**
+ * A stack of tokens that is initialised with the tokens provided by a scanner.
+ *
+ * A scanner that returns a stream of tokens can be seen as an
+ * iterator, or a stack without #push method.  This class provides a
+ * stack view of a scanner that allows pushing tokens back into the
+ * token stream.  When initialised with a scanner, the stack contains
+ * all the tokens provided by the scanner.  They are returned via the
+ * #pop method.  In addition, it is possible to push tokens back into
+ * the stream.  This allows, for instance, lookahead and eases the
+ * manipulation of token streams.
+ *
+ * @author Petra Malik
+ */
+public class TokenStack
+{
+  private Scanner scanner_;
+  private Stack stack_ = new Stack();
+
+  public TokenStack(Scanner scanner)
+  {
+    scanner_ = scanner;
+  }
+
+  public Symbol pop()
+    throws Exception
+  {
+    if (stack_.empty()) {
+      return scanner_.next_token();
+    }
+    return (Symbol) stack_.pop();
+  }
+
+  public void push(Symbol symbol)
+  {
+    stack_.push(symbol);
+  }
+}
