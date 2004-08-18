5ac71da7fa47050822b482f6a7935513e95a4403
Petra Malik <petramalik@users.sourceforge.net>
2004-08-18 01:41:57 +0000

    ADD:   two new test cases that test whether specifications containing just one   latex markup directive that do not end with a newline are handled   correctly. COSMETICS:   indend copyright notice since this looks nicer.

--- parser/tests/src/net/sourceforge/czt/parser/util/AbstractParserTest.java
+++ parser/tests/src/net/sourceforge/czt/parser/util/AbstractParserTest.java
@@ -1,20 +1,20 @@
-/**
-Copyright (C) 2004 Petra Malik
-This file is part of the czt project.
+/*
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.parser.util;
@@ -249,6 +249,18 @@ public abstract class AbstractParserTest
   {
     compare(getTestExample("animate_sequences.tex"),
             getTestExample("animate_sequences.xml"));
+  }
+
+  public void testLatexMarkupWithoutNl1()
+  {
+    compare(getTestExample("latexMarkupWithoutNl1.tex"),
+            getTestExample("latexMarkupWithoutNl1.xml"));
+  }
+
+  public void testLatexMarkupWithoutNl2()
+  {
+    compare(getTestExample("latexMarkupWithoutNl2.tex"),
+            getTestExample("latexMarkupWithoutNl2.xml"));
   }
 
   public abstract Term parse(URL url, SectionManager manager)
