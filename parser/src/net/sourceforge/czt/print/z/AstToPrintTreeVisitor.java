d85b737c3332bd9ae0c1b6759c011fd509a770ed
Petra Malik <petramalik@users.sourceforge.net>
2004-08-23 00:54:45 +0000

    COSMETICS:   improve error messages and indent copyright notice.

--- parser/src/net/sourceforge/czt/print/z/AstToPrintTreeVisitor.java
+++ parser/src/net/sourceforge/czt/print/z/AstToPrintTreeVisitor.java
@@ -1,20 +1,20 @@
 /*
-Copyright (C) 2004 Petra Malik
-This file is part of the czt project.
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.print.z;
@@ -256,7 +256,7 @@ public class AstToPrintTreeVisitor
         if (opInfo != null) {
           if (opInfo.getPrec() == null) {
             String message =
-              "Cannot find precedence of infix operator " + opName;
+              "Cannot find precedence of infix operator '" + opName + "'.";
             throw new CannotPrintAstException(message);
           }
           prec = new Precedence(180, opInfo.getPrec().intValue());
@@ -264,7 +264,7 @@ public class AstToPrintTreeVisitor
         }
         else {
           String message =
-            "Cannot find precedence and associativity for '" + opName + "'";
+            "Cannot find precedence and associativity for '" + opName + "'.";
           throw new CannotPrintAstException(message);
         }
       }
