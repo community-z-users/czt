79e4e71191a719ea995ebeb0ddb937961f9e66d3
Petra Malik <petramalik@users.sourceforge.net>
2004-09-01 21:53:09 +0000

    COSMETICS:   remove some unused import statements,   indent copyright file,   improve documentation.

--- corejava/src/net/sourceforge/czt/base/util/XmlWriter.java
+++ corejava/src/net/sourceforge/czt/base/util/XmlWriter.java
@@ -1,26 +1,25 @@
 /*
-Copyright 2003, 2004 Mark Utting
-This file is part of the czt project.
+  Copyright (C) 2003, 2004 Mark Utting
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.base.util;
 
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 
 import net.sourceforge.czt.base.ast.Term;
@@ -29,8 +28,6 @@ import net.sourceforge.czt.base.ast.Term;
  * Provides methods for writing XML data.
  *
  * @author Petra Malik
- * @czt.todo Provide Exceptions that can be thrown
- *           in case the marshalling was unseccessful.
  */
 public interface XmlWriter
 {
