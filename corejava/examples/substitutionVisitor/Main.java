d1334a090d822d9a5726216f24ce19114ddf7b76
Petra Malik <petramalik@users.sourceforge.net>
2004-09-27 00:34:42 +0000

    BUGFIX:   make it at least compile again. COSMETICS:   make the code look a bit nicer.

--- corejava/examples/substitutionVisitor/Main.java
+++ corejava/examples/substitutionVisitor/Main.java
@@ -1,20 +1,20 @@
-/**
-Copyright 2003 Mark Utting
-This file is part of the czt project.
+/*
+  Copyright (C) 2003, 2004 Mark Utting
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 import java.io.*;
@@ -23,13 +23,13 @@ import java.util.logging.*;
 import net.sourceforge.czt.base.util.*;
 import net.sourceforge.czt.base.visitor.VisitorUtils;
 import net.sourceforge.czt.z.jaxb.JaxbXmlReader;
-import net.sourceforge.czt.z.dom.DomXmlWriter;
+import net.sourceforge.czt.z.jaxb.JaxbXmlWriter;
 import net.sourceforge.czt.base.ast.Term;
 
 public class Main {
   public static void main(String[] args) {
     // Use a default file name ...
-    String fileName = "../../../zml/examples/eg1.xml";
+    String fileName = "../../../zml/examples/z/birthdaybook.xml";
     // ... or the file provided as an argument.
     if (args.length > 0) fileName = args[0];
 
@@ -62,7 +62,7 @@ public class Main {
       Term result = (Term) spec.accept(visitor);
 
       // ... and marshal the result to stdout.
-      XmlWriter writer = new DomXmlWriter();
+      XmlWriter writer = new JaxbXmlWriter();
       writer.write(result, System.out);
     } catch( Exception e ) {
       e.printStackTrace();
