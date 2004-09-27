2a2cb059cebd01998aefb3a2fa08703e1817205a
Petra Malik <petramalik@users.sourceforge.net>
2004-09-27 00:29:29 +0000

    MODIFY:   use a different example file since the old one does not exist any more. COSMETICS:   make code look nicer.

--- corejava/examples/debug/Main.java
+++ corejava/examples/debug/Main.java
@@ -1,20 +1,20 @@
-/**
-Copyright 2003 Mark Utting
-This file is part of the czt project.
+/*
+  Copyright (C) 2003, 2004 Mark Utting
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 import java.util.logging.*;
@@ -27,7 +27,7 @@ public class Main {
   public static void main( String[] args ) {
     String logFileName = "debug.log";
     // Use a default file name ...
-    String fileName = "../../../zml/examples/eg1.xml";
+    String fileName = "../../../zml/examples/z/birthdaybook.xml";
     // ... or the file provided as an argument.
     if (args.length > 0) fileName = args[0];
 
@@ -56,7 +56,8 @@ public class Main {
 	(Spec) reader.read(new java.io.File(fileName));
 
       System.out.println("\nYou should find logging information ");
-      System.out.println("for all method calls to DeclName in file " + logFileName + ".");
+      System.out.println("for all method calls to DeclName in file "
+                         + logFileName + ".");
     } catch( Exception e ) {
       e.printStackTrace();
     }
