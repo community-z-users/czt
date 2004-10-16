ee0c835fd3e688dd895e0819103ed7f0fdf2c7c4
Tim Miller <timmiller@users.sourceforge.net>
2004-10-16 16:30:17 +0000

    ADDED: An undeclared annotation to label a name as undeclared

---
+++ typechecker/src/net/sourceforge/czt/typecheck/util/typingenv/UndeclaredAnn.java
@@ -0,0 +1,11 @@
+package net.sourceforge.czt.typecheck.util.typingenv;
+
+import java.util.List;
+
+/**
+ * An annotation for recording undeclared reference names
+ */
+public class UndeclaredAnn
+{
+  public UndeclaredAnn() {}
+}
