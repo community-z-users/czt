6ea6bcdae7ca9fc89eb90e1120a4836f5fb60840
Petra Malik <petramalik@users.sourceforge.net>
2004-11-03 23:39:50 +0000

    REFACTORING:   provide new private method check to avoid duplicated code. MODIFY:   accept also that no exception is thrown and null is returned instead.

--- parser/tests/src/net/sourceforge/czt/parser/util/AbstractParserFailTest.java
+++ parser/tests/src/net/sourceforge/czt/parser/util/AbstractParserFailTest.java
@@ -1,20 +1,20 @@
-/**
-Copyright (C) 2004 Petra Malik
-This file is part of the czt project.
+/*
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.parser.util;
@@ -50,10 +50,12 @@ public abstract class AbstractParserFailTest
     return Examples.getTestExample(name);
   }
 
-  public void testInvalidOperatorWord()
+  private void check(URL url)
   {
     try {
-      parse(getTestExample("invalidOperatorWord.tex"), manager_);
+      Term term = parse(url, manager_);
+      // A return value of null is also acceptable
+      if (term == null) return;
       fail("Should throw parse exception!");
     }
     catch (ParseException e) {
@@ -62,103 +64,45 @@ public abstract class AbstractParserFailTest
     catch (IOException e) {
       fail("Should not throw IOException!");
     }
+  }
+
+  public void testInvalidOperatorWord()
+  {
+    check(getTestExample("invalidOperatorWord.tex"));
   }
 
   public void testInvalidOperatorWord2()
   {
-    try {
-      parse(getTestExample("invalidOperatorWord2.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperatorWord2.tex"));
   }
 
   public void testInvalidOperatorWord3()
   {
-    try {
-      parse(getTestExample("invalidOperatorWord3.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperatorWord3.tex"));
   }
 
   public void testInvalidOperatorWord4()
   {
-    try {
-      parse(getTestExample("invalidOperatorWord4.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperatorWord4.tex"));
   }
 
   public void testInvalidOperator1()
   {
-    try {
-      parse(getTestExample("invalidOperator1.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperator1.tex"));
   }
 
   public void testInvalidOperator2()
   {
-    try {
-      parse(getTestExample("invalidOperator2.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperator2.tex"));
   }
 
   public void testInvalidOperator3()
   {
-    try {
-      parse(getTestExample("invalidOperator3.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperator3.tex"));
   }
 
   public void testInvalidOperator4()
   {
-    try {
-      parse(getTestExample("invalidOperator4.tex"), manager_);
-      fail("Should throw parse exception!");
-    }
-    catch (ParseException e) {
-      // ok
-    }
-    catch (IOException e) {
-      fail("Should not throw IOException!");
-    }
+    check(getTestExample("invalidOperator4.tex"));
   }
 }
