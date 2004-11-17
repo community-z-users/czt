a85519485cb34543be3c653856210bb5fa479763
Tim Miller <timmiller@users.sourceforge.net>
2004-11-17 11:59:59 +0000

    ADDED: new class UResult, which is used to return results of unification - failed, succeedded, or partially succeeded (still some variable types).

---
+++ typechecker/src/net/sourceforge/czt/typecheck/util/typingenv/UResult.java
@@ -0,0 +1,49 @@
+package net.sourceforge.czt.typecheck.util.typingenv;
+
+import net.sourceforge.czt.base.ast.*;
+import net.sourceforge.czt.z.ast.*;
+import net.sourceforge.czt.typecheck.util.impl.*;
+
+/**
+ * A class for holding the results of unification. A partial
+ * unificiation (PARTIAL), must be created with the types that are not
+ * unified.
+ */
+public class UResult
+{
+  /** The possible results of a unification. */
+  /** Succeed. */
+  public static final UResult SUCC = new UResult("SUCC");
+
+  /** Partial (there are still variable types). */
+  public static final UResult PARTIAL = new UResult("PARTIAL");
+
+  /** Failure. */
+  public static final UResult FAIL = new UResult("FAIL");
+
+  protected final String name_;
+
+  /**
+   * Only this class can create instances.
+   */
+  private UResult(String name)
+  {
+    name_ = name;
+  }
+
+  public boolean equals(Object obj)
+  {
+    if (obj instanceof UResult) {
+      UResult uResult = (UResult) obj;
+      if (name_.equals(uResult.name_)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String toString()
+  {
+    return name_;
+  }
+}
