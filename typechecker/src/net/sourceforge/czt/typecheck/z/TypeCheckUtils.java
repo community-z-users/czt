6c49ef056d2eadc10df0f77b721a90959fe2d1bf
Tim Miller <timmiller@users.sourceforge.net>
2004-11-29 21:13:00 +0000

    ADDED: Added a new class that provides a simple class interface to the typechecker.

---
+++ typechecker/src/net/sourceforge/czt/typecheck/z/TypeCheckUtils.java
@@ -0,0 +1,68 @@
+
+package net.sourceforge.czt.typecheck.z;
+
+import java.io.*;
+import java.util.List;
+
+import net.sourceforge.czt.util.*;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.session.SectionInfo;
+import net.sourceforge.czt.z.ast.ZFactory;
+import net.sourceforge.czt.z.impl.ZFactoryImpl;
+import net.sourceforge.czt.parser.z.*;
+import net.sourceforge.czt.parser.util.*;
+import net.sourceforge.czt.typecheck.util.typingenv.*;
+
+/**
+ * Utilities for typechecking Z specifications.
+ *
+ * @author Petra Malik, Tim Miller
+ */
+public final class TypeCheckUtils
+{
+  /**
+   * Do not generate instances of this class.
+   */
+  private TypeCheckUtils()
+  {
+  }
+
+  /**
+   * Typecheck and type annotate a file.
+   * @param term the term to typecheck.
+   * @return the list of ErrorAnns in the AST added by the typechecker.
+   */
+  public static List typecheck(Term term,
+                               SectionInfo sectInfo,
+                               ErrorFactory errorFactory,
+                               SectTypeEnv sectTypeEnv)
+  {
+    ZFactory zFactory = new ZFactoryImpl();
+    CheckerInfo checker =
+      new CheckerInfo(zFactory, sectTypeEnv, errorFactory, sectInfo);
+    checker.typecheck(term);
+    return checker.errors();
+  }
+
+  public static List typecheck(Term term,
+                               SectionInfo sectInfo,
+                               SectTypeEnv sectTypeEnv)
+  {
+    return typecheck(term, sectInfo,
+                     new DefaultErrorFactory(sectInfo), sectTypeEnv);
+  }
+
+  public static List typecheck(Term term,
+                               SectionInfo sectInfo,
+                               ErrorFactory errorFactory)
+  {
+    return typecheck(term, sectInfo, errorFactory, new SectTypeEnv());
+  }
+
+  public static List typecheck(Term term,
+                               SectionInfo sectInfo)
+  {
+    return typecheck(term, sectInfo,
+                     new DefaultErrorFactory(sectInfo), new SectTypeEnv());
+  }
+}
