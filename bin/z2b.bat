61d29333fdc38dfa969c02d76444fa59b6880cce
Mark Utting <marku@users.sourceforge.net>
2005-05-25 09:30:36 +0000

    MODIFY:  enable assertion checking while running z2b.     Also, added quotes around path expansion, in case paths contain spaces.

--- bin/z2b.bat
+++ bin/z2b.bat
@@ -1,3 +1,3 @@
 @echo off
 call "%~f0\..\settings.bat"
-java -classpath "%PARSER%;%JAVA_CUP%;%GAFFE%;%COREJAVA%;%JWSDP%;%Z2B%" net.sourceforge.czt.z2b.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -enableassertions -classpath "%PARSER%;%JAVA_CUP%;%GAFFE%;%COREJAVA%;%JWSDP%;%Z2B%" net.sourceforge.czt.z2b.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
