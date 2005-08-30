a9508c1e3dae7737dfa3dd538f25fd42356bd140
Petra Malik <petramalik@users.sourceforge.net>
2005-08-30 20:54:27 +0000

    INITIAL REVISION:   windows executables for the typechecker.

---
+++ bin/zedtypecheck.bat
@@ -0,0 +1,3 @@
+@echo off
+call "%~f0\..\settings.bat"
+java -classpath "%TYPECHECKER%;%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%" net.sourceforge.czt.typecheck.z.TypeCheckUtils %1 %2 %3 %4 %5 %6 %7 %8 %9
