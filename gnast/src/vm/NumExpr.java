54049edfff10316e643a95d6711e54ffefbe5f77
Petra Malik <petramalik@users.sourceforge.net>
2005-09-19 02:24:06 +0000

    ADD:   convinience method getValue().

--- gnast/src/vm/NumExpr.java
+++ gnast/src/vm/NumExpr.java
@@ -5,3 +5,10 @@
    * ZNumeral and throws an UnsupportedAstClassException otherwise.
    */
   ZNumeral getZNumeral();
+
+  /**
+   * This is a convenience method.
+   * It returns the value of the ZNumeral if Numeral is an instance of
+   * ZNumeral and throws an UnsupportedAstClassException otherwise.
+   */
+  java.math.BigInteger getValue();
--- gnast/src/vm/NumExprImpl.java
+++ gnast/src/vm/NumExprImpl.java
@@ -12,3 +12,17 @@
     }
     throw new net.sourceforge.czt.base.util.UnsupportedAstClassException();
   }
+
+  /**
+   * This is a convenience method.
+   * It returns the value of the ZNumeral if Numeral is an instance of
+   * ZNumeral and throws an UnsupportedAstClassException otherwise.
+   */
+  public java.math.BigInteger getValue()
+  {
+    Numeral numeral = getNumeral();
+    if (numeral instanceof ZNumeral) {
+      return new java.math.BigInteger(((ZNumeral) numeral).getValue().toString());
+    }
+    throw new net.sourceforge.czt.base.util.UnsupportedAstClassException();
+  }
