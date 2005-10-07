7ea87f363b21a55501c1ee58d74eababa4829ead
Petra Malik <petramalik@users.sourceforge.net>
2005-10-07 03:47:46 +0000

    ADD:   subproject rules.

--- bin/beanshell.bat
+++ bin/beanshell.bat
@@ -1,3 +1,3 @@
 @echo off
 call "%~f0\..\settings.bat"
-java -classpath "%~f0\..;%BSH%;%ZLIVE%;%PARSER%;%COREJAVA%;%TYPECHECKER%;%JWSDP%;%JAVA_CUP%" bsh.Console %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~f0\..;%BSH%;%ZLIVE%;%RULES%;%PARSER%;%COREJAVA%;%TYPECHECKER%;%JWSDP%;%JAVA_CUP%" bsh.Console %1 %2 %3 %4 %5 %6 %7 %8 %9
