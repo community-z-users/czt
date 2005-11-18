74c75afca00dab0356e487acca71e7d2a3b3dfd3
Leo Freitas <leouk@users.sourceforge.net>
2005-11-18 10:39:22 +0000

    ADD: runtime exception class including type checking error annotations (ErrorAnn)

---
+++ typechecker/src/net/sourceforge/czt/typecheck/z/util/TypeErrorException.java
@@ -0,0 +1,40 @@
+/*
+ * TypeErrorException.java
+ *
+ * Created on 18 November 2005, 10:11
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package net.sourceforge.czt.typecheck.z.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import net.sourceforge.czt.typecheck.z.ErrorAnn;
+
+/**
+ *
+ * @author leo
+ */
+public class TypeErrorException extends RuntimeException {    
+    
+    private final List<ErrorAnn> fErrors = new ArrayList<ErrorAnn>();
+    
+    /** Creates a new instance of TypeErrorException */
+    public TypeErrorException(String message, List<? extends ErrorAnn> errors) {
+        super(message);
+        fErrors.addAll(errors);
+    }
+    
+    public TypeErrorException(String message, Throwable cause, List<? extends ErrorAnn> errors) {
+        super(message,  cause);
+        fErrors.addAll(errors);
+    }
+    
+    public List<? extends ErrorAnn> errors() {
+        return Collections.unmodifiableList(fErrors);
+    }
+}
