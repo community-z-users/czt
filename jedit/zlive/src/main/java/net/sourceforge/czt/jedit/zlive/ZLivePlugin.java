5cbc4c69737aca3a4ea12f0389a94ebc6b691a0e
Petra Malik <petramalik@users.sourceforge.net>
2006-01-05 19:45:49 +0000

    INITIAL RELEASE:   a zlive jedit plugin.

---
+++ jedit/zlive/src/main/java/net/sourceforge/czt/jedit/zlive/ZLivePlugin.java
@@ -0,0 +1,29 @@
+/*
+ * ZLivePlugin.java
+ * Copyright (C) 2006 Petra Malik
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.sourceforge.czt.jedit.zlive;
+
+import org.gjt.sp.jedit.*;
+
+public class ZLivePlugin
+  extends EditPlugin
+{
+  public static final String NAME = "ZLive";
+  public static final String OPTION_PREFIX = "options.zlive.";
+}
