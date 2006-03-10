5cc438b6719e230dd38ffa6c97b3861560316a81
Petra Malik <petramalik@users.sourceforge.net>
2006-03-10 05:08:06 +0000

    ADD:   support for reading (unmarshalling) from an InputSource.

--- corejava/src/net/sourceforge/czt/base/jaxb/JaxbXmlReader.java
+++ corejava/src/net/sourceforge/czt/base/jaxb/JaxbXmlReader.java
@@ -1,29 +1,34 @@
 /*
-Copyright (C) 2003, 2004 Mark Utting
-This file is part of the czt project.
+  Copyright (C) 2003, 2004, 2006 Mark Utting
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.base.jaxb;
 
+import java.io.File;
+import java.io.InputStream;
+import org.xml.sax.InputSource;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-import net.sourceforge.czt.util.ReflectiveVisitor;
 import net.sourceforge.czt.base.ast.*;
+import net.sourceforge.czt.base.util.XmlReader;
+import net.sourceforge.czt.util.ReflectiveVisitor;
 
 /**
  * The unmarshaller responsible for deserializing XML data.
@@ -72,11 +77,32 @@ public class JaxbXmlReader
    * @param stream  the stream to be unmarshalled.
    * @return the root element of the unmarshalled file.
    */
-  public Term read(java.io.InputStream stream)
+  public Term read(InputStream stream)
   {
     Term term = null;
     try {
       term = (Term) visitor_.dispatch(createUnmarshaller().unmarshal(stream));
+    }
+    catch (UnsupportedOperationException e) {
+      throw e;
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    return term;
+  }
+
+  /**
+   * Unmarshalles XML data from the specified source and
+   * returns the root Term.
+   * @param input  the source to be unmarshalled.
+   * @return the root element of the unmarshalled file.
+   */
+  public Term read(InputSource input)
+  {
+    Term term = null;
+    try {
+      term = (Term) visitor_.dispatch(createUnmarshaller().unmarshal(input));
     }
     catch (UnsupportedOperationException e) {
       throw e;
@@ -94,7 +120,7 @@ public class JaxbXmlReader
    * @param file  the file to be unmarshalled.
    * @return the root element of the unmarshalled file.
    */
-  public Term read(java.io.File file)
+  public Term read(File file)
   {
     Term term = null;
     try {
--- corejava/src/net/sourceforge/czt/base/util/XmlReader.java
+++ corejava/src/net/sourceforge/czt/base/util/XmlReader.java
@@ -1,26 +1,28 @@
 /*
-Copyright 2003, 2004 Mark Utting
-This file is part of the czt project.
+  Copyright 2003, 2004, 2006 Mark Utting
+  This file is part of the czt project.
 
-The czt project contains free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-The czt project is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with czt; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package net.sourceforge.czt.base.util;
 
 import java.io.File;
 import java.io.InputStream;
+import org.xml.sax.InputSource;
+
 import net.sourceforge.czt.base.ast.Term;
 
 /**
@@ -43,6 +45,17 @@ public interface XmlReader
    *                              is <code>null</code>.
    */
   Term read(File file)
+    throws UnmarshalException;
+
+  /**
+   * Unmarshalles XML data from the specified source and
+   * returns the root Term.
+   * @param input  the source to be unmarshalled.
+   * @return the root element of the unmarshalled file.
+   * @throws NullPointerException if <code>file</code>
+   *                              is <code>null</code>.
+   */
+  public Term read(InputSource input)
     throws UnmarshalException;
 
   /**
