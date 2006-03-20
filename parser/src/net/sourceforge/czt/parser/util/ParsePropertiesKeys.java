b8145e330179640e9e27d616cb4614c661207694
Petra Malik <petramalik@users.sourceforge.net>
2006-03-20 04:48:24 +0000

    REMOVE:   property PROP_ADD_SPACE_BEFORE_PUNCTATION and references to it.   This is an implementation of a very first attempt to handle the   colon, semicolon problem (a_1 : \nat), which is better solved   via the EXTRACT_COMMA_OR_SEMI_FROM_DECORWORDS approach.

--- parser/src/net/sourceforge/czt/parser/util/ParsePropertiesKeys.java
+++ parser/src/net/sourceforge/czt/parser/util/ParsePropertiesKeys.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2005 Petra Malik
+  Copyright (C) 2005, 2006 Petra Malik
   This file is part of the czt project.
 
   The czt project contains free software; you can redistribute it and/or modify
@@ -20,13 +20,34 @@
 package net.sourceforge.czt.parser.util;
 
 /**
- * Contains constants for the keys used in
- * parse properties.
+ * <P>Contains String constants for the keys used in parse properties.</P>
+ *
+ * <P>The behaviour of the CZT parser utilities (scanner, parser, etc.)
+ * can be configured via Properties.  This interface provides the String
+ * constants that are currently supported by the parser utilities.</P>
+ *
+ * @author Petra Malik
  */
 public interface ParsePropertiesKeys
 {
+  /**
+   * When set to <code>true</code>, the parser tools will extract
+   * symbol characters COMMA, SEMICOLON, and FULL STOP from the
+   * beginning and end of a WORD token to become WORDs themselves.
+   * This is a planned change to the Z Standard; see also
+   * the Draft Technical Corrigendum 1: Corrections,
+   * including to use of Unicode, March 17th, 2006.
+   * As of now, this has yet to be submitted for official ballot.
+   */
   String PROP_EXTRACT_COMMA_OR_SEMI_FROM_DECORWORDS =
     "extract_comma_or_semi";
-  String PROP_ADD_SPACE_BEFORE_PUNCTATION = "add_space_before_punctation";
+
+  /**
+   * When set to <code>true</code>, the parser tools will ignore
+   * unknown latex commands (that is, give a warning and use the name
+   * of the command) instead of reporting an error.  Reporting an
+   * error is Standard conforming but ignoring those unknown commands
+   * is sometimes convenient.
+   */
   String PROP_IGNORE_UNKNOWN_LATEX_COMMANDS = "ignore_unknow_latex_commands";
 }
