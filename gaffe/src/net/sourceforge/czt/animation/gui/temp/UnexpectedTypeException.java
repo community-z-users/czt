9380fd903987bb6011826b89b0541282d1ddc593
Linan Zhang <lz173@users.sourceforge.net>
2006-05-23 12:06:55 +0000

    *** empty log message ***

---
+++ gaffe/src/net/sourceforge/czt/animation/gui/temp/UnexpectedTypeException.java
@@ -0,0 +1,33 @@
+
+package net.sourceforge.czt.animation.gui.temp;
+
+public final class UnexpectedTypeException extends Exception
+{
+  /**
+   * Unexpected type of expr with message
+   * @param message The message ready to show
+   */
+  public UnexpectedTypeException(String message)
+  {
+    super(message);
+  }
+  
+  /**
+   * Unexpected type of expr with message and cause
+   * @param message The message ready to show
+   * @param cause The cause
+   */
+  public UnexpectedTypeException(String message, Throwable cause)
+  {
+    super(message, cause);
+  }
+  
+  /**
+   * Unexpected type of expr with cause
+   * @param cause
+   */
+  public UnexpectedTypeException(Throwable cause)
+  {
+    super(cause);
+  }
+}
