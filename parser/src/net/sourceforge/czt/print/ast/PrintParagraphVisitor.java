8c442ea34d35047464859fb983fdb2929b201ff2
Petra Malik <petramalik@users.sourceforge.net>
2006-06-10 06:22:03 +0000

    REFACTORING:   make visitor generic.

--- parser/src/net/sourceforge/czt/print/ast/PrintParagraphVisitor.java
+++ parser/src/net/sourceforge/czt/print/ast/PrintParagraphVisitor.java
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2004 Petra Malik
+  Copyright (C) 2004, 2006 Petra Malik
   This file is part of the czt project.
 
   The czt project contains free software; you can redistribute it and/or modify
@@ -21,8 +21,8 @@ package net.sourceforge.czt.print.ast;
 
 import net.sourceforge.czt.util.Visitor;
 
-public interface PrintParagraphVisitor
-  extends Visitor
+public interface PrintParagraphVisitor<R>
+  extends Visitor<R>
 {
-  Object visitPrintParagraph(PrintParagraph printParagraph);
+  R visitPrintParagraph(PrintParagraph printParagraph);
 }
