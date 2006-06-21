2148a9c0249caec3f07b895b5855d06d59c2de16
Leo Freitas <leouk@users.sourceforge.net>
2006-06-21 15:14:58 +0000

    ADD: first version of circus printer

---
+++ parser/src/main/java/net/sourceforge/czt/print/circus/AstToPrintTreeVisitor.java
@@ -0,0 +1,45 @@
+/*
+  Copyright (C) 2006 Leo Freitas
+  This file is part of the czt project.
+
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.print.circus;
+
+import java.util.List;
+
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.session.SectionInfo;
+import net.sourceforge.czt.z.ast.*;
+import net.sourceforge.czt.z.visitor.*;
+import net.sourceforge.czt.circus.ast.*;
+import net.sourceforge.czt.circus.visitor.*;
+
+public class AstToPrintTreeVisitor
+  extends net.sourceforge.czt.print.z.AstToPrintTreeVisitor
+  /*implements BasicProcessVisitor<Term> NO NEED*/
+
+{
+  /**
+   * Creates a new ast to print tree visitor.
+   * The section information should be able to provide information of
+   * type <code>net.sourceforge.czt.parser.util.OpTable.class</code>.
+   */
+  public AstToPrintTreeVisitor(SectionInfo sectInfo)
+  {
+    super(sectInfo);
+  }
+}
