4f3b5c414aa4eec0544060f68399ff5a49cfbe8a
Leo Freitas <leouk@users.sourceforge.net>
2006-06-22 16:14:55 +0000

    UPDATE: imports BUGFIX: CircusPrintVisitor instantiation

--- parser/src/main/java/net/sourceforge/czt/print/circus/ZmlScanner.java
+++ parser/src/main/java/net/sourceforge/czt/print/circus/ZmlScanner.java
@@ -19,22 +19,8 @@
 
 package net.sourceforge.czt.print.circus;
 
-import java.util.List;
-import java.util.Vector;
-
-import net.sourceforge.czt.java_cup.runtime.Scanner;
-import net.sourceforge.czt.java_cup.runtime.Symbol;
-
-import net.sourceforge.czt.base.ast.*;
-import net.sourceforge.czt.base.visitor.*;
-import net.sourceforge.czt.base.util.*;
-
-import net.sourceforge.czt.z.ast.*;
-import net.sourceforge.czt.z.visitor.*;
-
-import net.sourceforge.czt.util.CztException;
-import net.sourceforge.czt.parser.util.*;
-import net.sourceforge.czt.print.z.*;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.print.z.PrecedenceParenAnnVisitor;
 
 /**
  * This Scanner uses the print visitor to tokenize a
@@ -54,7 +40,7 @@ public class ZmlScanner
       new PrecedenceParenAnnVisitor();
     term.accept(precVisitor);
     SymbolCollector collector = new SymbolCollector(Sym.class);
-    ZPrintVisitor visitor = new CircusPrintVisitor(collector);
+    CircusPrintVisitor visitor = new CircusPrintVisitor(collector);
     term.accept(visitor);
     symbols_ = collector.getSymbols();
   }
