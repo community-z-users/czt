0407e5a6e056dd7210383b1cb5b8cf7fe8b4ea49
Leo Freitas <leouk@users.sourceforge.net>
2006-06-23 10:26:51 +0000

    ADD: Circus and Circus Latex parsers into ZSideKick plugin (thanks for the hints Petra ;p)

---
+++ jedit/ZSideKick/src/main/java/zsidekick/CircusParser.java
@@ -0,0 +1,42 @@
+/*
+ * CircusParser.java
+ * Copyright (C) 2006 Leo Freitas
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package zsidekick;
+
+import net.sourceforge.czt.session.*;
+
+public class CircusParser
+  extends AbstractParser
+{
+  public ZParser()
+  {
+    super("circus");
+  }
+
+  public SectionManager getManager()
+  {
+    SectionManager manager = new SectionManager();
+    setParseProperties(manager);
+    return manager;
+  }
+
+  public Markup getMarkup()
+  {
+    return Markup.UNICODE;
+  }
+}
