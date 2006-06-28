d10acf4b0529407d47db3ac4d0ac61043f0a24c2
Petra Malik <petramalik@users.sourceforge.net>
2006-06-28 22:15:20 +0000

    ADD:   support for printing ZML. MODIFY:   improve the usage message.

--- ui/src/main/java/net/sourceforge/czt/ui/Main.java
+++ ui/src/main/java/net/sourceforge/czt/ui/Main.java
@@ -43,9 +43,13 @@ public class Main
     else {
       String extension = "z";
       String output = null;
+      boolean syntaxCheckOnly = false;
       for (int i = 0; i < args.length; i++) {
-        if ("-e".equals(args[i])) {
-          if (i < args.length) {
+        if ("-s".equals(args[i])) {
+          syntaxCheckOnly = true;
+        }
+        else if ("-e".equals(args[i])) {
+          if (i + 1 < args.length) {
             extension = args[++i];
           }
           else {
@@ -54,7 +58,7 @@ public class Main
           }
         }
         else if ("-o".equals(args[i])) {
-          if (i < args.length) {
+          if (i + 1 < args.length) {
             output = args[++i];
           }
           else {
@@ -65,7 +69,7 @@ public class Main
         else {
           SectionManager manager = new SectionManager(extension);
           FileSource source = new FileSource(args[i]);
-          if (parse(source, manager) && output != null) {
+          if (parse(source, manager, syntaxCheckOnly) && output != null) {
             if (output.endsWith("utf8")) {
               UnicodeString unicode = (UnicodeString)
                 manager.get(new Key(source.getName(), UnicodeString.class));
@@ -90,6 +94,14 @@ public class Main
               writer.write(latex.toString());
               writer.close();
             }
+            else if (output.endsWith("xml") || output.endsWith("zml")) {
+              XmlString xml = (XmlString)
+                manager.get(new Key(source.getName(), XmlString.class));
+              FileOutputStream stream = new FileOutputStream(output);
+              Writer writer = new OutputStreamWriter(stream);
+              writer.write(xml.toString());
+              writer.close();
+            }
             else {
               System.err.println("Unsupported output file " + output);
               return;
@@ -108,8 +120,17 @@ public class Main
   public static String usage(String prefix)
   {
     return "Usage:\n" +
-      prefix + "[-e <extension>] <filename>\n" +
-      prefix + "<command> [<commandArg1> .. <commandArgN>]";
+      "  " + prefix + "[-e <extension>] [-o <filename>] [-s] <filename>\n" +
+      "  " + prefix + "<command> [<commandArg1> .. <commandArgN>]\n" +
+      "Flags:\n  -e   specify the extension to be used\n" +
+      "  -o   specifiy output file (mark-up is determined by file ending)\n" +
+      "  -s   syntax check only\n" +
+      "Extensions:\n  z    Standard Z (default)\n  oz   Object Z\n" +
+      "File ending bingings:\n  tex, zed --> LaTeX Markup\n" +
+      "  xml, zml --> ZML\n" +
+      "  utf8     --> Unicode (encoding UTF-8)\n" +
+      "  utf16    --> Unicode (encoding UTF-16)\n" +
+      "Commands:\n" + printCommands();
   }
 
   /**
@@ -119,19 +140,12 @@ public class Main
   public static void command(String[] args)
     throws Throwable
   {
-    URL url = Main.class.getResource("command.properties");
-    Properties props = new Properties();
-    InputStream is = url.openStream();
-    if (is != null) {
-      props.loadFromXML(is);
+    Properties props = getCommands();
+    if (props != null) {
       final String name = props.getProperty(args[0]);
       if (name == null) {
         System.err.println("Cannot find tool " + args[0]);
-        System.err.println("Available tools are:");
-        for(Map.Entry entry : props.entrySet()) {
-          System.err.println("  " + entry.getKey() +
-                             " (bound to " + entry.getValue() + ")");
-        }
+        System.err.println("Available tools are:\n" + printProperties(props));
         return;
       }
       Class cmdClass = Class.forName(name);
@@ -150,19 +164,53 @@ public class Main
         throw e;
       }
     }
-    else {
-      throw new RuntimeException("Cannot read resource command.properties");
-    }
   }
 
-  public static boolean parse(Source source, SectionManager manager)
+  public static String printCommands()
+  {
+    return printProperties(getCommands());
+    
+  }
+
+  public static String printProperties(Properties props)
+  {
+    StringBuilder builder = new StringBuilder();
+    for(Map.Entry entry : props.entrySet()) {
+      builder.append("  " + entry.getKey() +
+                     " (bound to " + entry.getValue() + ")\n");
+    }
+    return builder.toString();
+  }
+
+  public static Properties getCommands()
+  {
+    String errorMessage = "Cannot read resource command.properties";
+    try {
+      URL url = Main.class.getResource("command.properties");
+      Properties result = new Properties();
+      InputStream is = url.openStream();
+      if (is != null) {
+        result.loadFromXML(is);
+        return result;
+      }
+    }
+    catch (IOException e) {
+      errorMessage += "\n" + e.getMessage();
+    }
+    System.err.println(errorMessage);
+    return null;
+  }
+
+  public static boolean parse(Source source,
+                              SectionManager manager,
+                              boolean syntaxCheckOnly)
     throws CommandException
   {
     try {
       String name = source.getName();
       manager.put(new Key(name, Source.class), source);
       Spec spec = (Spec) manager.get(new Key(name, Spec.class));
-      if (spec.getSect().size() > 0) {
+      if (! syntaxCheckOnly && spec.getSect().size() > 0) {
         for (Sect sect : spec.getSect()) {
           if (sect instanceof ZSect) {
             manager.get(new Key(((ZSect) sect).getName(),
