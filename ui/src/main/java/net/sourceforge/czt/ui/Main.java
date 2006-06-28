64506162846656a485e28d0eff69ef3fd3cc72b7
Petra Malik <petramalik@users.sourceforge.net>
2006-06-28 06:33:55 +0000

    ADD:   lots of code to provide simple parsing and typechecking support.

--- ui/src/main/java/net/sourceforge/czt/ui/Main.java
+++ ui/src/main/java/net/sourceforge/czt/ui/Main.java
@@ -24,17 +24,55 @@ import java.lang.reflect.*;
 import java.net.URL;
 import java.util.*;
 
+import net.sourceforge.czt.parser.util.*;
 import net.sourceforge.czt.session.*;
+import net.sourceforge.czt.z.ast.*;
 
 public class Main
 {
+  public static String PREFIX = "java -jar czt.jar ";
+
   public static void main(String[] args)
     throws Throwable
   {
-    if (args[0].contains(".")) {
-      System.err.println("Not yet implemented");
-      return;
+    if (args.length == 0) System.out.println(usage());
+    else if ("-e".equals(args[0])) {
+      if (args.length < 3) {
+        System.out.println(usage());
+      }
+      else {
+        SectionManager manager = new SectionManager(args[1]);
+        parse(args[2], manager);
+      }
     }
+    else if (args[0].contains(".")) {
+      SectionManager manager = new SectionManager();
+      parse(args[0], manager);
+    }
+    else {
+      command(args);
+    }
+  }
+
+  public static String usage()
+  {
+    return usage(PREFIX);
+  }
+
+  public static String usage(String prefix)
+  {
+    return "Usage:\n" +
+      prefix + "[-e <extension>] <filename>\n" +
+      prefix + "<command> [<commandArg1> .. <commandArgN>]";
+  }
+
+  /**
+   * The first string contains the command to be invoked,
+   * the following are arguments to the command.
+   */
+  public static void command(String[] args)
+    throws Throwable
+  {
     URL url = Main.class.getResource("command.properties");
     Properties props = new Properties();
     InputStream is = url.openStream();
@@ -68,6 +106,52 @@ public class Main
     }
     else {
       throw new RuntimeException("Cannot read resource command.properties");
+    }
+  }
+
+  public static void parse(String filename, SectionManager manager)
+    throws CommandException
+  {
+    try {
+      FileSource source = new FileSource(filename);
+      String name = source.getName();
+      manager.put(new Key(name, Source.class), source);
+      Spec spec = (Spec) manager.get(new Key(name, Spec.class));
+      if (spec.getSect().size() > 0) {
+        for (Sect sect : spec.getSect()) {
+          if (sect instanceof ZSect) {
+            manager.get(new Key(((ZSect) sect).getName(),
+                                SectTypeEnvAnn.class));
+          }
+        }
+      }
+      try {
+        ParseException parseException = (ParseException)
+          manager.get(new Key(source.getName(), ParseException.class));
+        if (parseException != null) {
+          printErrors(parseException.getErrors());
+        }
+      }
+      catch (CommandException e) {
+        // TODO Is ignoring OK?
+      }
+    }
+    catch (CommandException exception) {
+      Throwable cause = exception.getCause();
+      if (cause instanceof CztErrorList) {
+        List<? extends CztError> errors = ((CztErrorList) cause).getErrors();
+        printErrors(errors);
+      }
+      else {
+        throw exception;
+      }
+    }
+  }
+
+  private static void printErrors(List<? extends CztError> errors)
+  {
+    for (CztError error : errors) {
+      System.out.println(error);
     }
   }
 }
