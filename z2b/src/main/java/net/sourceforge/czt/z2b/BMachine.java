8beb53b632125dde1adf2e22c186cd83449eb41d
Petra Malik <petramalik@users.sourceforge.net>
2006-12-07 01:30:23 +0000

    REFACTORING:   use Java 1.5 features, especially generics.

--- z2b/src/main/java/net/sourceforge/czt/z2b/BMachine.java
+++ z2b/src/main/java/net/sourceforge/czt/z2b/BMachine.java
@@ -1,5 +1,5 @@
 /**
-Copyright 2003 Mark Utting
+Copyright 2003, 2006 Mark Utting
 This file is part of the CZT project.
 
 The CZT project contains free software; you can redistribute it and/or modify
@@ -45,43 +45,35 @@ import net.sourceforge.czt.z2b.*;
 public class BMachine
 {
   /** The name of the machine */
-  protected String name;
-
-  /** The source location, where the machine came from*/
-  protected String source;
-
-  /** Parameter types */
-  List/*<String>*/  params = new ArrayList();
+  protected String name_;
 
   /** Sets of constants */
-  Map/*<String,List<String>>*/ sets = new HashMap();
+  private Map<String,List<String>> sets_ = new HashMap<String,List<String>>();
 
   /** The constants and their properties */
-  List/*<String>*/  constants = new ArrayList();
-  List/*<Pred>*/  properties = new ArrayList();
+  private List<String> constants_ = new ArrayList<String>();
+  private List<Pred>  properties_ = new ArrayList<Pred>();
 
   /** Definitions */
-  Map/*<String,Expr>*/ defns = new HashMap();
+  Map<String,Expr> defns_ = new HashMap<String,Expr>();
 
   /** State variables and their invariants */
-  List/*<String>*/  variables = new ArrayList();
-  List/*<Pred>*/  invariant = new ArrayList();
+  private List<String>  variables_ = new ArrayList<String>();
+  List<Pred>  invariant_ = new ArrayList<Pred>();
 
   /** Initialisation variables and their invariants */
-  List/*<Pred>*/  initialisation = new ArrayList();
+  List<Pred>  initialisation_ = new ArrayList<Pred>();
 
-  List/*<BOperation>*/ operations = new ArrayList();
+  List<BOperation> operations_ = new ArrayList();
 
 
   /**
    * Constructor for BMachine
    *
    * @param name   The name of the machine.
-   * @param source The name of the file/URL where this machine came from.
    */
-  public BMachine(String name, String source) {
-    this.name = name;
-    this.source = source;
+  public BMachine(String name) {
+    name_ = name;
   }
 
 
@@ -91,28 +83,28 @@ public class BMachine
    *  The name of each set is mapped to its list of members (DeclNames),
    *  or to null if the set has unknown contents.
    */
-  public Map/*<String,List<String>>*/ getSets() { return sets; }
+  public Map<String,List<String>> getSets() { return sets_; }
 
   /** Returns the constant 'Name's of this machine. */
-  public List/*<String>*/ getConstants() { return constants; }
+  public List<String> getConstants() { return constants_; }
 
   /** Returns the properties of the constants */
-  public List/*<Pred>*/ getProperties() { return properties; }
+  public List<Pred> getProperties() { return properties_; }
 
   /** Stores all Name == Expr definitions. */
-  public Map/*<String,Expr>*/ getDefns() { return defns; }
+  public Map<String,Expr> getDefns() { return defns_; }
 
   /** Returns the state variables of this machine.  They have type Name. */
-  public List/*<String>*/ getVariables() { return variables; }
+  public List/*<String>*/ getVariables() { return variables_; }
 
   /** Returns the invariant predicates */
-  public List/*<Pred>*/ getInvariant() { return invariant; }
+  public List<Pred> getInvariant() { return invariant_; }
 
   /** Returns the initialisation predicates of this machine. */
-  public List/*<Pred>*/ getInitialisation() { return initialisation; }
+  public List<Pred> getInitialisation() { return initialisation_; }
 
   /** Returns the operations of this machine.  They have type BOperation. */
-  public List/*<BOperation>*/ getOperations() { return operations; }
+  public List<BOperation> getOperations() { return operations_; }
 
 
 
@@ -121,19 +113,19 @@ public class BMachine
    */
   public void print(BWriter dest) {
     dest.startSection("MACHINE");
-    dest.printName(name);
-    if (sets.size() > 0) {
+    dest.printName(name_);
+    if (sets_.size() > 0) {
       dest.continueSection("MACHINE","SETS");
-      Iterator i = sets.keySet().iterator();
+      Iterator<String> i = sets_.keySet().iterator();
       while (i.hasNext()) {
-	String n = (String)i.next();
+	String n = i.next();
 	dest.printName(n);
-	List members = (List)sets.get(n);
+	List<String> members = sets_.get(n);
 	if (members != null) {
 	  dest.print(" = {");
-	  Iterator mem = members.iterator();
+	  Iterator<String> mem = members.iterator();
 	  while (mem.hasNext()) {
-	    dest.printName((String)mem.next());
+	    dest.printName(mem.next());
 	    if (mem.hasNext())
 	      dest.print(",");
 	  }
@@ -143,56 +135,56 @@ public class BMachine
 	  dest.printSeparator(";");
       }
     }
-    if (constants.size() > 0) {
+    if (constants_.size() > 0) {
       dest.continueSection("MACHINE","CONSTANTS");
-      printNames(dest,constants);
-      assert properties.size() > 0;
+      printNames(dest,constants_);
+      assert properties_.size() > 0;
       dest.continueSection("MACHINE","PROPERTIES");
-      dest.printPreds(properties);
+      dest.printPreds(properties_);
     }
-    if (defns.size() > 0) {
+    if (defns_.size() > 0) {
       dest.continueSection("MACHINE","DEFINITIONS");
-      for (Iterator i = defns.entrySet().iterator(); i.hasNext(); ) {
-        Map.Entry entry = (Map.Entry)i.next();
+      for (Iterator<Map.Entry<String,Expr>> i = defns_.entrySet().iterator();
+           i.hasNext(); ) {
+        Map.Entry<String,Expr> entry = i.next();
         dest.beginPrec(BTermWriter.DEFN_PREC);
-        dest.printName((String)entry.getKey());
+        dest.printName(entry.getKey());
         dest.print(" == ");
-        dest.printExpr((Expr)entry.getValue());
+        dest.printExpr(entry.getValue());
         dest.endPrec(BTermWriter.DEFN_PREC);
         if (i.hasNext())
           dest.printSeparator(";");
       }
     }
-    if (variables.size() > 0) {
+    if (variables_.size() > 0) {
       dest.continueSection("MACHINE","VARIABLES");
-      printNames(dest,variables);
-      assert invariant.size() > 0;
+      printNames(dest,variables_);
+      assert invariant_.size() > 0;
       dest.continueSection("MACHINE","INVARIANT");
-      dest.printPreds(invariant);
+      dest.printPreds(invariant_);
     }
     dest.continueSection("MACHINE","INITIALISATION");
-    if (initialisation.size() == 0)
+    if (initialisation_.size() == 0)
       throw new BException("no initialisation predicates");
     // We use:  ANY state' WHERE init' THEN state := state' END
     // Rename outputs to outputs' in the postconditions.
     Map initRename = new HashMap();
-    for (Iterator i = variables.iterator(); i.hasNext(); ) {
-      String name = (String)i.next();
+    for (String name : variables_) {
       initRename.put(name, Create.prime(name));
     }
     RenameVisitor initPrimer = new RenameVisitor(initRename);
     List initPreds = new ArrayList();
-    for (Iterator i = initialisation.iterator(); i.hasNext(); ) {
-      initPreds.add(initPrimer.rename((Pred)i.next()));
+    for (Pred pred : initialisation_) {
+      initPreds.add(initPrimer.rename(pred));
     }
     dest.printAnyAssign(initRename, initPreds);
     
     // Now the operations
-    if (operations.size() > 0) {
+    if (operations_.size() > 0) {
       dest.continueSection("MACHINE","OPERATIONS");
-      Iterator i = operations.iterator();
+      Iterator<BOperation> i = operations_.iterator();
       while (i.hasNext()) {
-	BOperation op = (BOperation)i.next();
+	BOperation op = i.next();
 	op.print(dest);
         if (i.hasNext())
           dest.printSeparator(";");
