29aef4be9fde86fa13a69ebba27da9c9a538f902
Petra Malik <petramalik@users.sourceforge.net>
2007-02-17 01:03:50 +0000

    REMOVE:   since this is not maintained any more.

--- bin/z2zeves.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "%~f0\..\settings.bat"
-set ZEVES="%CZT_HOME%\lib\zeves.jar"
-java -enableassertions -classpath "%TYPECHECKER%;%PARSER%;%COREJAVA%;%JWSDP%;%JAVA_CUP%";"%ZEVES%" net.sourceforge.czt.zeves.CZT2ZEves %1 %2 %3 %4 %5 %6 %7 %8 %9
