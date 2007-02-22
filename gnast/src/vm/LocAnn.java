f5e6383994611c08d0067981012767c21d1adea1
Petra Malik <petramalik@users.sourceforge.net>
2007-02-22 03:08:09 +0000

    MODIFY:   use provided methods instead of convenience methods to make   it easier to maintain the GnAST generated classes.

--- gnast/src/vm/LocAnn.java
+++ gnast/src/vm/LocAnn.java
@@ -1,5 +1,1 @@
   java.math.BigInteger getEnd();
-  void setLine(int line);
-  void setCol(int col);
-  void setStart(int start);
-  void setLength(int length);
--- gnast/src/vm/LocAnnImpl.java
+++ gnast/src/vm/LocAnnImpl.java
@@ -6,23 +6,3 @@
     }
     return null;
   }
-
-  public void setLine(int line)
-  {
-    setLine(java.math.BigInteger.valueOf(line));
-  }
-
-  public void setCol(int col)
-  {
-    setCol(java.math.BigInteger.valueOf(col));
-  }
-
-  public void setStart(int start)
-  {
-    setStart(java.math.BigInteger.valueOf(start));
-  }
-
-  public void setLength(int length)
-  {
-    setLength(java.math.BigInteger.valueOf(length));
-  }
