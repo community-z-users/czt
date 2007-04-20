df5e47a122b0a2fa64cff1081290e5be8fcc626a
Leo Freitas <leouk@users.sourceforge.net>
2007-04-20 15:53:28 +0000

    ADD: CircusPattern Corejava base files ADD: CircusPattern.xsd (no interactivity with GnAST yet) ADD: Jaxb Files for CircusPattern in CoreJava

---
+++ corejava/src/main/java/net/sourceforge/czt/circuspatt/util/CircusPattUtils.java
@@ -0,0 +1,26 @@
+/*
+ * CircusUtils.java
+ *
+ * Created on 15 June 2006, 09:04
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.sourceforge.czt.circuspatt.util;
+
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.base.util.UnsupportedAstClassException;
+
+/**
+ *
+ * @author leo
+ */
+public final class CircusPattUtils {
+  
+  /**
+   * Do not create instances of this class.
+   */
+  private CircusPattUtils() {
+  }     
+}
