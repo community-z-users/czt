3b945c6cb5bbe5124e8a3bba3de7700408ffaa8d
Leo Freitas <leouk@users.sourceforge.net>
2007-04-23 09:52:38 +0000

    ADD: ParserUtilsTest.java

---
+++ parser-circus/src/test/java/net/sourceforge/czt/parser/circus/ParserUtilsTest.java
@@ -0,0 +1,31 @@
+/*
+ * ParserUtilsTest.java
+ * JUnit based test
+ *
+ * Created on 15 February 2007, 10:11
+ */
+
+package net.sourceforge.czt.parser.circus;
+
+import junit.framework.*;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.session.FileSource;
+import net.sourceforge.czt.session.SectionManager;
+
+/**
+ *
+ * @author leo
+ */
+public class ParserUtilsTest extends TestCase {
+  
+  //private static SectionManager sm_ = new SectionManager("circus");
+  
+  public ParserUtilsTest(String testName) {
+    super(testName);
+  }
+  
+  public void testParsingZ() {
+    //String filename = "C:\\research\\tools\\java\\sourceforge\\czt\\0.5.2\\trunk\\parser\\target\\test-classes\\tests\\circus\\debug";
+   // Term term = net.sourceforge.czt.parser.z.ParseUtils.parse(new FileSource(filename), sm_);
+  }
+}
