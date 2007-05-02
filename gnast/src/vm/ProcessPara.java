7302e84b3d738068c73c4e1ccbac6ace64fc1608
Leo Freitas <leouk@users.sourceforge.net>
2007-05-02 14:44:40 +0000

    ADD: getZName to ProcessPara

--- gnast/src/vm/ProcessPara.java
+++ gnast/src/vm/ProcessPara.java
@@ -4,3 +4,10 @@
    * ZNameList or throws an UnsupportedAstClassException otherwise.
    */
   net.sourceforge.czt.z.ast.ZNameList getZGenFormals();
+
+  /**
+   * This is a convenience method.
+   * It returns the ZName if Name is an instance of
+	   * ZName and throws an UnsupportedAstClassException otherwise.
+   */
+  net.sourceforge.czt.z.ast.ZName getZName();
