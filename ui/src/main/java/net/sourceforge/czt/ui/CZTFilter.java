1528be050ff9c343ffe4b8322a7c2c20de7ce6c5
Yi-Na (Ignatius) Tsai <yt49@users.sourceforge.net>
2007-10-30 02:14:45 +0000

    MODIFY:  added some more file extensions

--- ui/src/main/java/net/sourceforge/czt/ui/CZTFilter.java
+++ ui/src/main/java/net/sourceforge/czt/ui/CZTFilter.java
@@ -15,7 +15,20 @@ public class CZTFilter extends FileFilter {
 
         String extension = Utils.getExtension(f);
         if (extension != null) {
-            if (extension.equals(Utils.utf8) ||
+            if (
+                extension.equals(Utils.zed) ||
+                extension.equals(Utils.zed8) ||
+                extension.equals(Utils.zed16) ||
+                extension.equals(Utils.oz) ||
+                extension.equals(Utils.oz8) ||
+                extension.equals(Utils.oz16) ||
+                extension.equals(Utils.circus) ||
+                extension.equals(Utils.circus8) ||
+                extension.equals(Utils.circus16) ||
+                extension.equals(Utils.zedpatt) ||
+                extension.equals(Utils.zedpatt8) ||
+                extension.equals(Utils.zedpatt16) ||
+                extension.equals(Utils.utf8) ||
                 extension.equals(Utils.utf16) ||
                 extension.equals(Utils.tex) ||
                 extension.equals(Utils.xml) ||
@@ -31,7 +44,7 @@ public class CZTFilter extends FileFilter {
 
     //The description of this filter
     public String getDescription() {
-        return "Common CZT files *.utf8,*.utf16,*.tex,*.xml,*.zml";
+        return "Common CZT files *.zed*,*.oz*,*.circus*,*.zedpatt*,*.utf8,*.utf16,*.tex,*.xml,*.zml";
     }
 }
 
