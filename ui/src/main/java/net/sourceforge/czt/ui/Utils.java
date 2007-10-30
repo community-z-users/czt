77c029efc5333c3b3cbdc8d58f36b38ca6c75ce8
Yi-Na (Ignatius) Tsai <yt49@users.sourceforge.net>
2007-10-30 02:15:02 +0000

    MODIFY:  added some more file extensions

--- ui/src/main/java/net/sourceforge/czt/ui/Utils.java
+++ ui/src/main/java/net/sourceforge/czt/ui/Utils.java
@@ -4,12 +4,24 @@ import java.io.File;
 
 public class Utils {
 
+    public final static String zed = "zed";
+    public final static String zed8 = "zed8";
+    public final static String zed16 = "zed16";
+    public final static String oz = "oz";
+    public final static String oz8 = "oz8";
+    public final static String oz16 = "oz16";
+    public final static String circus = "circus";
+    public final static String circus8 = "circus8";
+    public final static String circus16 = "circus16";
+    public final static String zedpatt = "zedpatt";
+    public final static String zedpatt8 = "zedpatt8";
+    public final static String zedpatt16 = "zedpatt16";
+    public final static String tex = "tex";
+    public final static String zml = "zml";
+    public final static String xml = "xml";
     public final static String utf8 = "utf8";
     public final static String utf16 = "utf16";
-    public final static String tex = "tex";
-    public final static String xml = "xml";
-    public final static String zml = "zml";
-
+    
     /*
      * Get the extension of a file.
      */  
