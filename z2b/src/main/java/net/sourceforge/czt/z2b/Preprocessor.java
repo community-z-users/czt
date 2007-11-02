772a9fbe1072787f96d69ebf16db714b3131d08d
Petra Malik <petramalik@users.sourceforge.net>
2007-11-02 06:18:30 +0000

    MODIFY:   use the new rewrite engine for the preprocessing.

--- z2b/src/main/java/net/sourceforge/czt/z2b/Preprocessor.java
+++ z2b/src/main/java/net/sourceforge/czt/z2b/Preprocessor.java
@@ -19,43 +19,24 @@
 
 package net.sourceforge.czt.z2b;
 
-import java.io.*;
-import java.util.logging.*;
-import java.util.*;
-import java.net.URL;
-
-import net.sourceforge.czt.base.ast.*;
-import net.sourceforge.czt.base.util.*;
-import net.sourceforge.czt.print.util.LatexString;
-import net.sourceforge.czt.rules.CopyVisitor;
-import net.sourceforge.czt.rules.ast.ProverFactory;
+import net.sourceforge.czt.base.ast.Term;
 import net.sourceforge.czt.rules.RuleTable;
-import net.sourceforge.czt.rules.RuleUtils;
-import net.sourceforge.czt.rules.oldrewriter.Rewrite;
+import net.sourceforge.czt.rules.UnboundJokerException;
+import net.sourceforge.czt.rules.rewriter.Strategies;
 import net.sourceforge.czt.session.CommandException;
-import net.sourceforge.czt.session.FileSource;
 import net.sourceforge.czt.session.Key;
 import net.sourceforge.czt.session.SectionManager;
-import net.sourceforge.czt.session.Source;
-import net.sourceforge.czt.session.UrlSource;
-import net.sourceforge.czt.z.ast.*;
-import net.sourceforge.czt.z.jaxb.JaxbXmlReader;
-import net.sourceforge.czt.zpatt.util.Factory;
 
 class Preprocessor
 {
-  private CopyVisitor copyVisitor_;
   private RuleTable rules_;
 
   Preprocessor()
   {
     try {
-      Factory factory = new Factory(new ProverFactory());
-      copyVisitor_ = new CopyVisitor(factory);
       SectionManager manager = new SectionManager("zpatt");
-      Source unfoldSource = new UrlSource(RuleUtils.getUnfoldRules());
-      manager.put(new Key("unfold", Source.class), unfoldSource);
-      rules_ = (RuleTable) manager.get(new Key("unfold", RuleTable.class));
+      rules_ = (RuleTable)
+        manager.get(new Key("expansion_rules", RuleTable.class));
     }
     catch(CommandException e) {
       throw new RuntimeException(e);
@@ -63,9 +44,12 @@ class Preprocessor
   }
 
   Term unfold(Term term, String section, SectionManager manager)
-    throws CommandException
   {
-    term = term.accept(copyVisitor_);
-    return Rewrite.rewrite(manager, section, term, rules_);
+    try {
+      return Strategies.innermost(term, rules_, manager, section);
+    }
+    catch (UnboundJokerException e) {
+      throw new RuntimeException("Unfolding failed!", e);
+    }
   }
 }
