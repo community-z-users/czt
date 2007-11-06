99b7e8cf82e030a2e70f2869c685222c101f07a9
Mark Utting <marku@users.sourceforge.net>
2007-11-06 00:23:20 +0000

    MODIFY: improve evalState and eval(Simple only). They now work with depth = 1 and maybe more.

--- modeljunit/src/main/java/net/sourceforge/czt/modeljunit/PessimisticTester.java
+++ modeljunit/src/main/java/net/sourceforge/czt/modeljunit/PessimisticTester.java
@@ -33,6 +33,7 @@ import net.sourceforge.czt.jdsl.graph.ref.IncidenceListGraph;
 import net.sourceforge.czt.modeljunit.coverage.CoverageMetric;
 import net.sourceforge.czt.modeljunit.coverage.TransitionCoverage;
 import net.sourceforge.czt.modeljunit.coverage.ActionCoverage;
+import net.sourceforge.czt.modeljunit.examples.FSM;
 
 /** An attempt at implementing the Pessimistic Player algorithm
  *
@@ -41,12 +42,30 @@ import net.sourceforge.czt.modeljunit.coverage.ActionCoverage;
 public class PessimisticTester extends Tester
 {
   protected GraphListener graph_;
+
   protected CoverageMetric transitions_;
+
   protected CoverageMetric actions_;
 
   private int depth_;
 
   private boolean complex_;
+
+  /**
+   * @return the depth of the recursive lookahead
+   */
+  public int getDepth()
+  {
+    return depth_;
+  }
+
+  /**
+   * @param depth the depth of the recursive lookahead
+   */
+  public void setDepth(int depth_)
+  {
+    this.depth_ = depth_;
+  }
 
   /**
    *  Creates a test generator that can generate random walks.
@@ -58,9 +77,9 @@ public class PessimisticTester extends Tester
     super(model);
     model.addListener("graph");
     transitions_ = (CoverageMetric) model.addListener(new TransitionCoverage());
-		actions_ = (CoverageMetric) model.addListener(new ActionCoverage());
+    actions_ = (CoverageMetric) model.addListener(new ActionCoverage());
     graph_ = (GraphListener) model.getListener("graph");
-    depth_ = 5;
+    depth_ = 1;
     complex_ = false;
   }
 
@@ -73,52 +92,78 @@ public class PessimisticTester extends Tester
     this(new Model(fsm));
   }
 
+  /**
+   * Evaluate the desirability of reaching {@code state}.
+   * However, at the top level of recursion (when {@code depth} equals
+   * getDepth()), it returns the number of the best action to take.
+   * @param state
+   * @param depth The depth of lookahead
+   * @return A desirability integer, or an action number.
+   */
   public int evalState(Object state, int depth)
   {
     if (depth == 0)
       return 0;
     int[] worth = new int[model_.getNumActions()];
-    
-    int result = 0;
-    
-		InspectableGraph fsmGraph = graph_.getGraph();
-		EdgeIterator edges = fsmGraph.edges();
-		int max = Integer.MIN_VALUE;;
-		
+    for (int i=0; i < worth.length; i++) {
+      worth[i] = Integer.MIN_VALUE;
+    }
+
+    InspectableGraph fsmGraph = graph_.getGraph();
+    EdgeIterator edges = fsmGraph.edges();
+
     while (edges.hasNext()) {
       Edge e = edges.nextEdge();
       Object origin = fsmGraph.origin(e).element();
       Object dest = fsmGraph.destination(e).element();
-      String action = (String) e.element();
-      if(origin.equals(state) && graph_.isDone(state, Integer.parseInt(action)))
-			{
-				int tempBest = eval(state, Integer.parseInt(action)) + evalState(dest, depth - 1);
-				if (tempBest > max)
-				{
-					worth[Integer.parseInt(action)] = tempBest;
-					result += tempBest;
-					max = tempBest;
-				}
-			}
+      String actionName = (String) e.element();
+      int action = model_.getActionNumber(actionName);
+      if (origin.equals(state)
+          && graph_.isDone(state, action)) {
+        int tempBest = eval(state, action)
+            + evalState(dest, depth - 1);
+        if (tempBest > worth[action]) {
+          worth[action] = tempBest;
+        }
+      }
     }
-		
-		fsmGraph = graph_.getGraph();
-		edges = fsmGraph.edges();
-		
-    while (edges.hasNext()) {
-      Edge e = edges.nextEdge();
-      Object origin = fsmGraph.origin(e).element();
-      Object dest = fsmGraph.destination(e).element();
-      String action = (String) e.element();
-      if(origin.equals(state) && graph_.isTodo(state, Integer.parseInt(action)))
-			{
-				worth[Integer.parseInt(action)] = 50;
-				result += 50;
-			}
+
+    for (int i=0; i<model_.getNumActions(); i++) {
+      if (graph_.isTodo(state, i)) {
+        worth[i] = 50;
+      }
     }
-    return result;
+
+    if (depth == depth_) {
+      // now find the best entry in worth
+      assert state == model_.getCurrentState();
+      int bestAction = -1;
+      int bestWorth = Integer.MIN_VALUE;
+      for (int i=0; i < model_.getNumActions(); i++) {
+        if (model_.isEnabled(i) && worth[i] > bestWorth) {
+          bestAction = i;
+          bestWorth = worth[i];
+        }
+      }
+      return bestAction;
+    }
+    else {
+      // return sum of all the action worths.
+      int result = 0;
+      for (int i=0; i < model_.getNumActions(); i++) {
+        result += worth[i];
+      }
+      return result;
+    }
   }
 
+  /**
+   * Evaluate the desirability of taking the action number
+   * {@code action} out of the given state.
+   * @param state
+   * @param action
+   * @return
+   */
   public int eval(Object state, int action)
   {
     if (complex_ == true)
@@ -127,30 +172,52 @@ public class PessimisticTester extends Tester
       return evalSimple(state, action);
   }
 
-  public int evalSimple(Object state, int actionNo)
+  public int evalSimple(Object state, int action)
   {
-		int result = 0;
-		
-		HashMap<Object, Integer> done = new HashMap<Object, Integer>();
-		
-		InspectableGraph fsmGraph = graph_.getGraph();
-		EdgeIterator edges = fsmGraph.edges();
-		
+    int result = 0;
+    String actionName = model_.getActionName(action);
+    if (graph_.isTodo(state, action)) {
+      result += 10;
+    }
+
+    InspectableGraph fsmGraph = graph_.getGraph();
+    EdgeIterator edges = fsmGraph.edges();
+
+    // TODO: see if we can iterate just through the (outgoing) edges of state
     while (edges.hasNext()) {
       Edge e = edges.nextEdge();
       Object origin = fsmGraph.origin(e).element();
       Object dest = fsmGraph.destination(e).element();
-      String action = (String) e.element();
-      if(origin.equals(state) && graph_.isTodo(state, Integer.parseInt(action)))
-      {
-        done.put(state, Integer.parseInt(action));
+      if (origin.equals(state) && e.element().equals(actionName)) {
+        Transition tr = new Transition(origin, (String)e.element(), dest);
+        Integer done = transitions_.getDetails().get(tr);
+        if (done != null) {
+          result -= done.intValue();
+        }
       }
     }
-		
-    if ((done.size() == 0) && (graph_.isTodo(state, actionNo)))
+
+    /*
+    HashMap<Object, Integer> done = new HashMap<Object, Integer>();
+
+    // TODO: see if we can iterate just through the (outgoing) edges of state
+    while (edges.hasNext()) {
+      Edge e = edges.nextEdge();
+      Object origin = fsmGraph.origin(e).element();
+      Object dest = fsmGraph.destination(e).element();
+      String actionName = (String) e.element();
+      int actionNum = model_.getActionNumber(actionName);
+      if (origin.equals(state)
+          && graph_.isTodo(state, actionNum)) {
+        done.put(state, actionNum);
+      }
+    }
+
+    if ((done.size() == 0) && (graph_.isTodo(state, action)))
       result += 10;
     else
-			result -= done.size();
+      result -= done.size();
+    */
     return result;
   }
 
@@ -159,7 +226,8 @@ public class PessimisticTester extends Tester
     int result = evalSimple(state, action);
     // If state has not been visited and action is in toDo set it gets
     // an even higher bias
-    if ((actions_.getDetails().get(state) == 0) && (graph_.isTodo(state, action)))
+    if ((actions_.getDetails().get(state) == 0)
+        && (graph_.isTodo(state, action)))
       result += 250;
     return result;
   }
@@ -167,7 +235,22 @@ public class PessimisticTester extends Tester
   @Override
   public int generate()
   {
-    // TODO Auto-generated method stub
-    return 0;
+    assert depth_ > 0;
+    int action = evalState(model_.getCurrentState(), depth_);
+    if (action < 0) {
+      model_.doReset();
+    }
+    else {
+      if ( ! model_.doAction(action))
+        throw new RuntimeException(this.getClass().getName()
+            +": could not take action "+action);
+    }
+    return action;
+  }
+
+  public static void main(String[] args) {
+    PessimisticTester tester = new PessimisticTester(new FSM());
+    tester.addListener(new VerboseListener());
+    tester.generate(10);
   }
 }
