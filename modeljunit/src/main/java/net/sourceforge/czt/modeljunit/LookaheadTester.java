1876a6f007683459449e25485f6a8eb5be596a3f
Mark Utting <marku@users.sourceforge.net>
2007-12-14 04:58:07 +0000

    ADD: unit tests for LookaheadTester. MODIFY: rewrote LookaheadTester to take the max of each path

--- modeljunit/src/main/java/net/sourceforge/czt/modeljunit/LookaheadTester.java
+++ modeljunit/src/main/java/net/sourceforge/czt/modeljunit/LookaheadTester.java
@@ -19,59 +19,118 @@
 
 package net.sourceforge.czt.modeljunit;
 
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.Random;
-import java.util.HashMap;
-
 import net.sourceforge.czt.jdsl.graph.api.Edge;
 import net.sourceforge.czt.jdsl.graph.api.EdgeIterator;
-import net.sourceforge.czt.jdsl.graph.api.Graph;
 import net.sourceforge.czt.jdsl.graph.api.InspectableGraph;
-import net.sourceforge.czt.jdsl.graph.api.Vertex;
-import net.sourceforge.czt.jdsl.graph.ref.IncidenceListGraph;
+import net.sourceforge.czt.modeljunit.coverage.ActionCoverage;
 import net.sourceforge.czt.modeljunit.coverage.CoverageMetric;
 import net.sourceforge.czt.modeljunit.coverage.TransitionCoverage;
-import net.sourceforge.czt.modeljunit.coverage.ActionCoverage;
 import net.sourceforge.czt.modeljunit.examples.FSM;
 
-/** An attempt at implementing the Pessimistic Player algorithm
+/** A test generator that looks N-levels ahead in the graph.
  *
- *  @author Pele Douangsavanh
+ *  It chooses the highest-valued transition (action) that is enabled
+ *  in the current state.
+ *  The value of transitions/actions and states is determined by three
+ *  parameters, NEWTRANS, NEWACTION and DEPTH.
+ *
+ *  In a given state, the value of an action that has never been taken
+ *  before in that state (so its destination state is unknown)
+ *  is NEWACTION if its action has never been taken anywhere in the graph,
+ *  or NEWTRANS if its action has been taken elsewhere.
+ *
+ *  The value of an action that has been taken before is the
+ *  value of the state that it leads to (*), minus the number of times
+ *  that the transition has been taken.  So previously-taken paths
+ *  gradually become less and less attractive.
+ *  (*) if the action is non-deterministic, then there are several
+ *  transitions labelled with that action, and the value of the action
+ *  is the maximum of the value of those transitions.
+ *
+ *  The value of a state is zero if it is the current state,
+ *  or if it is at least DEPTH steps away from the current state,
+ *  otherwise it is the maximum value of its outgoing transitions.
+ *  This means that the lookahead is limited to paths of length DEPTH.
+ *
+ *  TODO: add some way of doing random/occasional resets.
+ *
+ *  @author Pele Douangsavanh and Mark Utting
  */
 public class LookaheadTester extends Tester
 {
+  /** How worthwhile is it to explore a new transition? */
+  protected int NEW_TRANS = 10;
+
+  /** How worthwhile is it to use a completely new action? */
+  protected int NEW_ACTION = 25;
+
+  /** How far should we look ahead? */
+  protected int DEPTH = 3;
+
   protected GraphListener graph_;
 
   protected CoverageMetric transitions_;
 
   protected CoverageMetric actions_;
 
-  private int depth_;
-
-  private boolean complex_;
-
   /**
    * @return the depth of the recursive lookahead
    */
   public int getDepth()
   {
-    return depth_;
+    return DEPTH;
   }
 
   /**
    * Set the lookahead depth.
-   * Zero means no lookahead, so the choice will be random.
+   * depth=N means explore all known paths ahead upto length N.
+   * depth=1 means look only at the outgoing transitions of the current state,
+   * so this gives similar behaviour to GreedyTester (but not
+   * exactly the same, because this tester uses alphabetical ordering of
+   * the transition names to decide between equally good transitions, whereas
+   * GreedyTester makes a random choice between them).
    *
-   * @param depth the depth of the recursive lookahead (0..)
+   * @param depth the depth of the recursive lookahead (1..n)
+   *              This should be reasonably small, otherwise the lookahead
+   *              may be exponentially slow.
    */
-  public void setDepth(int depth_)
+  public void setDepth(int depth)
   {
-    this.depth_ = depth_;
+    assert depth > 0;
+    this.DEPTH = depth;
+  }
+
+  public int getNewActionValue()
+  {
+    return NEW_ACTION;
+  }
+
+  /** Set the NEW_ACTION parameter.
+   *  Higher values make the lookahead more eager to try new actions.
+   * @param value
+   */
+  public void setNewActionValue(int value)
+  {
+    this.NEW_ACTION = value;
+  }
+
+  public int getNewTransValue()
+  {
+    return NEW_TRANS;
+  }
+
+  /** Set the NEW_TRANS parameter.
+   *  Higher values make the lookahead more eager to try new transitions
+   *  (actions that are in the TODO list of a given state).
+   * @param value
+   */
+  public void setNewTransValue(int value)
+  {
+    this.NEW_TRANS = value;
   }
 
   /**
-   *  Creates a test generator that can generate random walks.
+   *  Creates a test generator that can does smart look ahead.
    *
    * @param model  Must be non-null;
    */
@@ -82,8 +141,6 @@ public class LookaheadTester extends Tester
     transitions_ = (CoverageMetric) model.addListener(new TransitionCoverage());
     actions_ = (CoverageMetric) model.addListener(new ActionCoverage());
     graph_ = (GraphListener) model.getListener("graph");
-    depth_ = 3;
-    complex_ = false;
   }
 
   /**
@@ -105,153 +162,95 @@ public class LookaheadTester extends Tester
    */
   public int evalState(Object state, int depth)
   {
-    if (depth == 0)
+    if (depth == 0 || depth < DEPTH && state == model_.getCurrentState())
       return 0;
     int[] worth = new int[model_.getNumActions()];
-    for (int i=0; i < worth.length; i++) {
+    for (int i = 0; i < worth.length; i++) {
       worth[i] = Integer.MIN_VALUE;
     }
 
     InspectableGraph fsmGraph = graph_.getGraph();
     EdgeIterator edges = fsmGraph.edges();
 
-    while (edges.hasNext()) {
-      Edge e = edges.nextEdge();
-      Object origin = fsmGraph.origin(e).element();
-      Object dest = fsmGraph.destination(e).element();
-      String actionName = (String) e.element();
-      int action = model_.getActionNumber(actionName);
-      if (origin.equals(state)
-          && graph_.isDone(state, action)) {
-        int tempBest = eval(state, action)
-            + evalState(dest, depth - 1);
-        if (tempBest > worth[action]) {
-          worth[action] = tempBest;
-        }
-      }
-    }
-
-    for (int i=0; i<model_.getNumActions(); i++) {
-      if (graph_.isTodo(state, i)) {
-        worth[i] = 50;
-      }
-    }
-
-    if (depth == depth_) {
-      // now find the best entry in worth
-      assert state == model_.getCurrentState();
-      int bestAction = -1;
-      int bestWorth = Integer.MIN_VALUE;
-      for (int i=0; i < model_.getNumActions(); i++) {
-        if (model_.isEnabled(i) && worth[i] > bestWorth) {
-          bestAction = i;
-          bestWorth = worth[i];
-        }
-      }
-      return bestAction;
-    }
-    else {
-      // return sum of all the action worths.
-      int result = 0;
-      for (int i=0; i < model_.getNumActions(); i++) {
-        result += worth[i];
-      }
-      return result;
-    }
-  }
-
-  /**
-   * Evaluate the desirability of taking the action number
-   * {@code action} out of the given state.
-   * @param state
-   * @param action
-   * @return
-   */
-  public int eval(Object state, int action)
-  {
-    if (complex_ == true)
-      return evalComplex(state, action);
-    else
-      return evalSimple(state, action);
-  }
-
-  public int evalSimple(Object state, int action)
-  {
-    int result = 0;
-    String actionName = model_.getActionName(action);
-    if (graph_.isTodo(state, action)) {
-      result += 10;
-    }
-
-    InspectableGraph fsmGraph = graph_.getGraph();
-    EdgeIterator edges = fsmGraph.edges();
-
-    // TODO: see if we can iterate just through the (outgoing) edges of state
-    while (edges.hasNext()) {
-      Edge e = edges.nextEdge();
-      Object origin = fsmGraph.origin(e).element();
-      Object dest = fsmGraph.destination(e).element();
-      if (origin.equals(state) && e.element().equals(actionName)) {
-        Transition tr = new Transition(origin, (String)e.element(), dest);
-        Integer done = transitions_.getDetails().get(tr);
-        if (done != null) {
-          result -= done.intValue();
-        }
-      }
-    }
-
-    /*
-    HashMap<Object, Integer> done = new HashMap<Object, Integer>();
-
-    // TODO: see if we can iterate just through the (outgoing) edges of state
+    // look ahead into the paths that we know about
+    // TODO: iterate only over the nodes out of this state
     while (edges.hasNext()) {
       Edge e = edges.nextEdge();
       Object origin = fsmGraph.origin(e).element();
       Object dest = fsmGraph.destination(e).element();
       String actionName = (String) e.element();
       int actionNum = model_.getActionNumber(actionName);
-      if (origin.equals(state)
-          && graph_.isTodo(state, actionNum)) {
-        done.put(state, actionNum);
+      if (origin.equals(state)) {
+        assert graph_.isDone(state, actionNum);
+        Transition tr = new Transition(origin, (String) e.element(), dest);
+        Integer takenBefore = transitions_.getDetails().get(tr);
+        assert takenBefore != null;
+        int destWorth = evalState(dest, depth - 1); // recursive lookahead
+        int tempBest = destWorth - takenBefore.intValue();
+        //System.out.println("  evalState(" + state + "," + depth
+        //    + ") sees action " + actionNum + "=" + takenBefore + " and dest "
+        //    + dest + "=" + destWorth);
+        if (tempBest > worth[actionNum]) {
+          //System.out.println("  worth[" + actionNum + "] = " + tempBest);
+          worth[actionNum] = tempBest;
+        }
       }
     }
 
-    if ((done.size() == 0) && (graph_.isTodo(state, action)))
-      result += 10;
-    else
-      result -= done.size();
-    */
-    return result;
-  }
+    // assign values to the unknown (unexplored) paths
+    for (int i = 0; i < model_.getNumActions(); i++) {
+      if (graph_.isTodo(state, i)) {
+        String actionName = model_.getActionName(i);
+        if (actions_.getDetails().containsKey(actionName)) {
+          //System.out.println("  worth[" + i + "] (TODO) = " + NEW_TRANS);
+          worth[i] = NEW_TRANS;
+        }
+        else {
+          //System.out.println("  worth[" + i + "] (TODO) = " + NEW_ACTION);
+          worth[i] = NEW_ACTION;
+        }
+      }
+    }
 
-  public int evalComplex(Object state, int action)
-  {
-    int result = evalSimple(state, action);
-    // If state has not been visited and action is in toDo set it gets
-    // an even higher bias
-    if ((actions_.getDetails().get(state) == 0)
-        && (graph_.isTodo(state, action)))
-      result += 250;
-    return result;
+    // find the best action
+    // (we only consider enabled actions if depth==DEPTH)
+    int bestAction = -1;
+    int bestWorth = Integer.MIN_VALUE;
+    for (int i = 0; i < model_.getNumActions(); i++) {
+      if (worth[i] > bestWorth && (depth != DEPTH || model_.isEnabled(i))) {
+        bestAction = i;
+        bestWorth = worth[i];
+      }
+    }
+    if (depth == DEPTH) {
+      assert state == model_.getCurrentState();
+      //System.out.println("RETURN bestAction="+bestAction+" worth "+bestWorth);
+      return bestAction;
+    }
+    else {
+      //System.out.println("evalState("+state+","+depth+") returns sum="+bestWorth);
+      return bestWorth;
+    }
   }
 
   @Override
   public int generate()
   {
-    assert depth_ > 0;
-    int action = evalState(model_.getCurrentState(), depth_);
+    assert DEPTH > 0;
+    int action = evalState(model_.getCurrentState(), DEPTH);
     if (action < 0) {
       model_.doReset();
     }
     else {
-      if ( ! model_.doAction(action))
+      if (!model_.doAction(action))
         throw new RuntimeException(this.getClass().getName()
-            +": could not take action "+action);
+            + ": could not take action " + action);
     }
     return action;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
     LookaheadTester tester = new LookaheadTester(new FSM());
     tester.addListener(new VerboseListener());
     tester.generate(10);
--- modeljunit/src/test/java/net/sourceforge/czt/modeljunit/LookaheadTesterTest.java
+++ modeljunit/src/test/java/net/sourceforge/czt/modeljunit/LookaheadTesterTest.java
@@ -19,7 +19,13 @@
 
 package net.sourceforge.czt.modeljunit;
 
-import net.sourceforge.czt.modeljunit.examples.FSM;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.czt.modeljunit.coverage.CoverageMetric;
+import net.sourceforge.czt.modeljunit.coverage.TransitionCoverage;
+import net.sourceforge.czt.modeljunit.examples.SimpleSet;
 import junit.framework.TestCase;
 
 /**
@@ -29,11 +35,27 @@ import junit.framework.TestCase;
  */
 public class LookaheadTesterTest extends TestCase
 {
-  protected LookaheadTester tester = new LookaheadTester(new FSM());
-  
-  public void testGenerate()
+  protected LookaheadTester tester;
+  protected List<Transition> path;
+  protected CoverageMetric transitions;
+
+  public void setUp()
   {
-    //fail("Not yet implemented");
+    tester = new LookaheadTester(new SimpleSet());
+    transitions = tester.addCoverageMetric(new TransitionCoverage());
+    path = new ArrayList<Transition>();
+    tester.addListener(new AbstractListener() {
+      public String getName()
+      {
+        return "path listener";
+      }
+      public void doneTransition(int action, Transition tr)
+      {
+        path.add(tr);
+        //System.out.println(path.size());
+      }
+    }
+    );
   }
 
   public void testGetDepth()
@@ -43,15 +65,76 @@ public class LookaheadTesterTest extends TestCase
 
   public void testSetDepth()
   {
-    tester.setDepth(0);
-    assertEquals(0, tester.getDepth());
+    tester.setDepth(10);
+    assertEquals(10, tester.getDepth());
     tester.setDepth(1);
     assertEquals(1, tester.getDepth());
   }
 
-  public void testGenerateInt()
+  public void testGraph() throws FileNotFoundException
   {
-    //fail("Not yet implemented");
+    Tester test = new GreedyTester(new SimpleSet());
+    GraphListener graph = (GraphListener) test.addListener("graph");
+    test.buildGraph();
+    graph.printGraphDot("quidonc.dot");
   }
 
+  public void testGenerate1()
+  {
+    tester.setDepth(1);
+    tester.setNewActionValue(tester.getNewTransValue());
+    tester.generate(17);
+    // impressive: this covers all 16 transitions with a walk of just 17.
+    assertEquals(17, path.size());
+    assertEquals("(FF, addS1, TF)", path.get(0).toString());
+    assertEquals("(TF, addS1, TF)", path.get(1).toString());
+    assertEquals("(TF, addS2, TT)", path.get(2).toString());
+    assertEquals("(TT, addS1, TT)", path.get(3).toString());
+    assertEquals("(TT, addS2, TT)", path.get(4).toString());
+    assertEquals("(TT, delS1, FT)", path.get(5).toString());
+    assertEquals("(FT, addS1, TT)", path.get(6).toString());
+    assertEquals("(TT, delS2, TF)", path.get(7).toString());
+    assertEquals("(TF, delS1, FF)", path.get(8).toString());
+    assertEquals("(FF, addS2, FT)", path.get(9).toString());
+    assertEquals("(FT, addS2, FT)", path.get(10).toString());
+    assertEquals("(FT, delS1, FT)", path.get(11).toString());
+    assertEquals("(FT, delS2, FF)", path.get(12).toString());
+    assertEquals("(FF, delS1, FF)", path.get(13).toString());
+    assertEquals("(FF, delS2, FF)", path.get(14).toString());
+    assertEquals("(FF, addS1, TF)", path.get(15).toString());
+    assertEquals("(TF, delS2, TF)", path.get(16).toString());
+    assertEquals(16, transitions.getCoverage());
+  }
+
+
+  public void testGenerate2()
+  {
+    tester.setDepth(3);
+    tester.generate(19);
+    // 
+    assertEquals(19, path.size());
+    assertEquals("(FF, addS1, TF)", path.get(0).toString());
+    assertEquals("(TF, addS2, TT)", path.get(1).toString());
+    assertEquals("(TT, delS1, FT)", path.get(2).toString());
+    assertEquals("(FT, delS2, FF)", path.get(3).toString());
+    assertEquals("(FF, addS2, FT)", path.get(4).toString());
+    assertEquals("(FT, addS1, TT)", path.get(5).toString());
+    assertEquals("(TT, addS1, TT)", path.get(6).toString());
+    assertEquals("(TT, addS2, TT)", path.get(7).toString());
+    assertEquals("(TT, delS2, TF)", path.get(8).toString());
+    
+    assertEquals("(TF, addS1, TF)", path.get(9).toString());
+    assertEquals("(TF, delS1, FF)", path.get(10).toString());
+    assertEquals("(FF, delS1, FF)", path.get(11).toString());
+    assertEquals("(FF, delS2, FF)", path.get(12).toString());
+    
+    assertEquals("(FF, addS1, TF)", path.get(13).toString());
+    assertEquals("(TF, delS2, TF)", path.get(14).toString());
+    
+    assertEquals("(TF, addS2, TT)", path.get(15).toString());
+    assertEquals("(TT, delS1, FT)", path.get(16).toString());
+    assertEquals("(FT, addS2, FT)", path.get(17).toString());
+    assertEquals("(FT, delS1, FT)", path.get(18).toString());
+    assertEquals(16, transitions.getCoverage());
+  }
 }
