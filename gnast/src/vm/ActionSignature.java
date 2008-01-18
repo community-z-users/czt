fa3fe0c5081ed9e8678daf2ca9018e4e3c9665a6
Leo Freitas <leouk@users.sourceforge.net>
2008-01-18 16:24:31 +0000

    ADD: extra accessor methods for action name and its signature entries

--- gnast/src/vm/ActionSignature.java
+++ gnast/src/vm/ActionSignature.java
@@ -1,10 +1,19 @@
+  /**
+   * This is a convenience method for getName.
+   */
+  net.sourceforge.czt.z.ast.Name getActionName();
 
   /**
    * This is a convenience method.
    * It returns the ZName if getName is an instance of
    * ZName and throws an UnsupportedAstClassException otherwise.
    */
-  net.sourceforge.czt.z.ast.ZName getActionName();
+  net.sourceforge.czt.z.ast.ZName getActionZName();
+
+  /**
+   * This is a convenience method for setName.
+   */  
+  void setActionName(net.sourceforge.czt.z.ast.Name name);
 
   /**
    * This is a convenience method.
@@ -13,14 +22,21 @@
    */
   net.sourceforge.czt.circus.ast.ZSignatureList getZSignatureList();
 
-
   /**
    * This is a convenience method. It represents the non-null (possibly empty)
    * signature of arguments for this action signature (i.e. its declared formal parameters).
    * It returns the Signature from getZSignatureList().get(0). It may throw a
-   * a UnsupportedAstClassException from getZSignatureList().
+   * a UnsupportedAstClassException from getZSignatureList().     
    */
   net.sourceforge.czt.z.ast.Signature getFormalParams();
+  
+  /**
+   * This is a convenience method. It sets the given non-null (possibly empty)  
+   * signature of the formal parameters signature of this action.
+   * It is the same as getZSignatureList().set(0, sig). It may throw a
+   * a UnsupportedAstClassException from getMainSignatures().
+   */  
+  void setFormalParams(net.sourceforge.czt.z.ast.Signature sig);
 
   /**
    * This is a convenience method. It represents the non-null (possibly empty)
@@ -28,7 +44,15 @@
    * It returns the Signature from getZSignatureList().get(1). It may throw a
    * a UnsupportedAstClassException from getZSignatureList().
    */
-  net.sourceforge.czt.z.ast.Signature getLocalVars();
+  net.sourceforge.czt.z.ast.Signature getLocalVars();  
+  
+  /**
+   * This is a convenience method. It sets the given non-null (possibly empty)  
+   * signature of the local variables signature of this action.
+   * It is the same as getZSignatureList().set(1, sig). It may throw a
+   * a UnsupportedAstClassException from getMainSignatures().
+   */    
+  void setLocalVars(net.sourceforge.czt.z.ast.Signature sig);
 
   /**
    * This is a convenience method. It represents the non-null (possibly empty)
@@ -37,3 +61,17 @@
    * a UnsupportedAstClassException from getZSignatureList().
    */
   net.sourceforge.czt.z.ast.Signature getUsedChannels();      
+  
+  /**
+   * This is a convenience method. It sets the given non-null (possibly empty)  
+   * signature of the used channels signature of this action.
+   * It is the same as getZSignatureList().set(2, sig). It may throw a
+   * a UnsupportedAstClassException from getMainSignatures().
+   */  
+  void setUsedChannels(net.sourceforge.czt.z.ast.Signature sig);
+ 
+  /**
+   * Returns whether or not this is a signature for Action or ActionPara
+   * (i.e. it has a name or not associated with it). TODO: CHECK: relevance... 
+   */   
+  boolean isActionPara();  
--- gnast/src/vm/ActionSignatureImpl.java
+++ gnast/src/vm/ActionSignatureImpl.java
@@ -1,7 +1,17 @@
 
-  public net.sourceforge.czt.z.ast.ZName getActionName()
+  public net.sourceforge.czt.z.ast.Name getActionName()
+  {
+    return getName();
+  }
+  
+  public net.sourceforge.czt.z.ast.ZName getActionZName()
   {
     return getZName();
+  }
+  
+  public void setActionName(net.sourceforge.czt.z.ast.Name name)
+  {
+    setName(name);
   }
   
   public net.sourceforge.czt.circus.ast.ZSignatureList getZSignatureList()
@@ -19,7 +29,16 @@
       return getZSignatureList().get(0);
     }
     throw new net.sourceforge.czt.base.util.UnsupportedAstClassException();
-  }
+  } 
+  
+  public void setFormalParams(net.sourceforge.czt.z.ast.Signature sig)
+  {
+    if (getZSignatureList().size() > 0) {
+      assert sig != null;
+      getZSignatureList().set(0, sig);
+    }
+    throw new net.sourceforge.czt.base.util.UnsupportedAstClassException(); 
+  }  
  
   public net.sourceforge.czt.z.ast.Signature getLocalVars()
   {
@@ -29,10 +48,33 @@
     throw new net.sourceforge.czt.base.util.UnsupportedAstClassException();
   }
   
+  public void setLocalVars(net.sourceforge.czt.z.ast.Signature sig)
+  {
+    if (getZSignatureList().size() > 1) {
+      assert sig != null;
+      getZSignatureList().set(1, sig);
+    }
+    throw new net.sourceforge.czt.base.util.UnsupportedAstClassException(); 
+  }  
+  
   public net.sourceforge.czt.z.ast.Signature getUsedChannels()      
   {
     if (getZSignatureList().size() > 2) {
       return getZSignatureList().get(2);
     }
     throw new net.sourceforge.czt.base.util.UnsupportedAstClassException();
+  }
+  
+  public void setUsedChannels(net.sourceforge.czt.z.ast.Signature sig)
+  {
+    if (getZSignatureList().size() > 2) {
+      assert sig != null;
+      getZSignatureList().set(2, sig);
+    }
+    throw new net.sourceforge.czt.base.util.UnsupportedAstClassException(); 
+  }
+  
+  public boolean isActionPara()
+  {
+    return (getName() != null);
   }
