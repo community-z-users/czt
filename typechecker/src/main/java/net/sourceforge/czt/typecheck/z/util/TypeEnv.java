f8702ca0a6990eafb75325549a4acaf2e3cd25e1
Leo Freitas <leouk@users.sourceforge.net>
2008-01-21 10:37:25 +0000

    ADD: documentation

--- typechecker/src/main/java/net/sourceforge/czt/typecheck/z/util/TypeEnv.java
+++ typechecker/src/main/java/net/sourceforge/czt/typecheck/z/util/TypeEnv.java
@@ -51,6 +51,11 @@ public class TypeEnv
    */
   protected Stack<List<ZName>> parameters_;
 
+  /**
+   * Creates a type environment with the given factory. 
+   * It is used within the environment (and its descendents)
+   * to create terms, usually lists and Type terms.
+   */
   public TypeEnv(Factory factory)
   {
     factory_ = factory;
@@ -74,6 +79,10 @@ public class TypeEnv
     return factory_;
   }
 
+  /**
+   * Enters a typing scope, which involves pushing a type information map
+   * and a list of generic parameters in the corresponding environment stacks.
+   */
   public void enterScope()
   {
     Map<ZName, NameTypePair> info = map();
@@ -82,12 +91,18 @@ public class TypeEnv
     parameters_.push(parameters);
   }
 
+  /**
+   * Exits a typing scope by popping the environment stacks.
+   */
   public void exitScope()
   {
     typeInfo_.pop();
     parameters_.pop();
   }
 
+  /**
+   * Add the list of the name to the top of the stack of generic parameters.
+   */
   public void addParameters(List<Name> paramNames)
   {
     for (Name paramName : paramNames) {
@@ -96,6 +111,11 @@ public class TypeEnv
     }
   }
 
+  /**
+   * Gets the list of names from the the top of the stack of generic parameters.
+   * This creates a new list of names (i.e., changes to the result do not affect
+   * the underlying stack).
+   */
   public List<ZName> getParameters()
   {
     List<ZName> result = getFactory().list();
@@ -103,12 +123,32 @@ public class TypeEnv
     return result;
   }
 
+  /**
+   * Adds a name and type pair to the type information map at the current scope.
+   */
   public void add(ZName zName, Type type)
   {
     NameTypePair pair = getFactory().createNameTypePair(zName, type);
     add(pair);
   }
-
+  
+  /**
+   * Adds a name and type pair to the type information map at the current scope.
+   */
+  public void add(NameTypePair pair)
+  {
+    typeInfo_.peek().put(pair.getZName(), pair);
+  }
+  
+  /**
+   * Adds a list of name and type pairs to the type information map at the current scope.
+   */
+  public void add(List<NameTypePair> pairs)
+  {
+    for (NameTypePair pair : pairs) {
+      add(pair);
+    }
+  }
 
   /**
    * Add a name into the environment, overriding an existing name in
@@ -127,36 +167,45 @@ public class TypeEnv
     //otherwise, add it to the environment
     add(zName, type);
   }
-
-  public void add(NameTypePair pair)
+  
+  /**
+   * <p>
+   * Returns the type of the given name in the type environment stack, or UnknownType
+   * if it does not exist at any scope. Thus, the result is never null. 
+   * See getTypeFromStack for details.
+   * </p>
+   *
+   *@param name the name to lookup the type
+   *@result the type for the given name or UnknownType 
+   */
+  public Type getType(ZName zName)
   {
-    typeInfo_.peek().put(pair.getZName(), pair);
+    Type result = getTypeFromStack(zName, typeInfo_);
+    assert result != null : "Invalid type for name " + zName;
+    return result;
   }
 
   /**
-   * Add a list of NameTypePair objects to this environment.
+   * Gets the pair for the corresponding name in the type environment, or null 
+   * if none exists.
    */
-  public void add(List<NameTypePair> pairs)
-  {
-    for (NameTypePair pair : pairs) {
-      add(pair);
-    }
-  }
-
-  public Type getType(ZName zName)
-  {
-    return getTypeFromStack(zName, typeInfo_);
-  }
-
-  //gets the pair with the corresponding name
   protected NameTypePair getPair(ZName zName)
   {    
     return getPairFromStack(zName, typeInfo_);
   }
   
   /**
-   * General method that retrieves NameTypePair for the given name from the given type information stack
-   * for Z typechecking that means
+   * <p>
+   * General method that retrieves NameTypePair for the given name from the given type 
+   * information stack. For Z typechecking, that means retrieving type information for
+   * global scope. Other extensions may have different stacks to be looked at, depending
+   * on the context where the information is needed. 
+   * </p>
+   * <p>
+   * The method looksup from the bottom to the top of the stack (i.e., outermost to
+   * innermost scopes) for the given name, which is looked up within the the map elements
+   * of the stack. Thus, innermost pairs are always returned. 
+   * </p>
    */
   protected NameTypePair getPairFromStack(ZName zName, Stack<Map<ZName, NameTypePair>> ti)
   {
@@ -170,7 +219,25 @@ public class TypeEnv
     return result;
   }
   
-  public Type getTypeFromStack(ZName zName, Stack<Map<ZName, NameTypePair>> ti)
+   /**
+   * <p>
+   * Returns the type of the given name in the environment (stack) given, or UnknownType
+   * if it does not exist at any scope. Thus, the result is never null.
+   * </p>
+   * <p>
+   * That is, it performs a lookup over each map of the type environment stack, where
+   * the innermost associated type is returned. Also, the IDs within the names
+   * given are updated to make sure references are properly linked with their
+   * declaring name. Finally, if no result has been found yet, it tries to see
+   * if this is a Delta or Xi name where the base type exists, otherwise, UnknownType 
+   * is returned.
+   * </p>
+   *
+   *@param name the name to lookup the type
+   *@param ti the type environment stack to search
+   *@result the type for the given name or UnknownType 
+   */
+  protected Type getTypeFromStack(ZName zName, Stack<Map<ZName, NameTypePair>> ti)
   {
     Type result = getFactory().createUnknownType();
 
