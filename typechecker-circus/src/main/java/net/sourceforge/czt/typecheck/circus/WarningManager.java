6663b369a82d3982dca476326175ba06bd8f33fd
Leo Freitas <leouk@users.sourceforge.net>
2008-02-28 09:15:30 +0000

    ADD: formatting of arguments with PrintUtils ADD: extra warning message for double checking the complex communication pattern checking protocol

--- typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/WarningManager.java
+++ typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/WarningManager.java
@@ -8,7 +8,13 @@
  */
 package net.sourceforge.czt.typecheck.circus;
 
+import java.io.StringWriter;
 import java.util.List;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.print.circus.PrintUtils;
+import net.sourceforge.czt.session.Markup;
+import net.sourceforge.czt.session.SectionInfo;
+import net.sourceforge.czt.util.CztException;
 
 /**
  *
@@ -16,16 +22,81 @@ import java.util.List;
  */
 public class WarningManager extends net.sourceforge.czt.z.util.WarningManager
 {
-
+  private Markup markup_ = Markup.LATEX;
+  private final SectionInfo sectInfo_;
+  
+  
   public WarningManager()
   {
     super();
+    sectInfo_ = null;
   }
 
-  /** Creates a new instance of WarningManager */
+  /** Creates a new instance of WarningManager
+   * @param forLogger 
+   */
   public WarningManager(Class<?> forLogger)
   {
     super(forLogger);
+    sectInfo_ = null;
+  }
+  
+  public WarningManager(SectionInfo manager)
+  {
+    super();
+    sectInfo_ = manager;
+  }
+  
+  public WarningManager(Class<?> forLogger, SectionInfo manager)
+  {
+    super(forLogger);
+    sectInfo_ = manager;
+  }
+  
+  @Override
+  protected String format(String message, Object... arguments)
+  { 
+    if (sectInfo_ != null)
+    {
+      Object[] values = new Object[arguments.length];
+      for(int i = 0; i < arguments.length; i++)      
+      {        
+        if (arguments[i] instanceof Term)
+        {          
+          try 
+          {
+            StringWriter writer = new StringWriter();          
+            PrintUtils.print((Term)arguments[i], writer, sectInfo_, getCurrentSectName(), getMarkup());
+            values[i] = writer.toString();
+          } catch (CztException e)
+          {
+            values[i] = String.valueOf(arguments[i]) + "\n\t\t- could not use pretty printer because '" + e.toString() + "'";
+          }
+          
+        }
+        else
+        {
+          values[i] = String.valueOf(arguments[i]);
+        }
+      }
+      return super.format(message, values);
+    }
+    else
+    {
+      return super.format(message, arguments);
+    }    
+  }
+  
+  public Markup getMarkup()
+  {
+    assert markup_ != null;
+    return markup_;
+  }
+  
+  public void setMarkup(Markup markup)
+  {
+    assert markup != null : "invalid null markup";
+    markup_ = markup;
   }
 
   public void warn(WarningMessage wm, List<Object> params)
--- typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/WarningMessage.java
+++ typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/WarningMessage.java
@@ -15,13 +15,27 @@ public enum WarningMessage {
       "\n\tProcess...: {0}" +
       "\n\tAction....: {1}" +
       "\n\tExpression: {2}",
-    "Without the special brackets, schema expression actions are wrongly interpreted as action calls.\n\t " +
-    "For simple schema reference, the type checker can recover and continue. For references with generic\n\t " +
-    "actuals or variable substitution, it is not possible to recover and an error is raised.\n\t " +
+    "Without the special brackets, schema expression actions are wrongly interpreted as action calls.\n\t" +
+    "For simple schema reference, the type checker can recover and continue. For references with generic\n\t" +
+    "actuals or variable substitution, it is not possible to recover and an error is raised.\n\t" +
     "The warning is mostly for other tools, which will also need to care about such special case."),
-  INVALID_DECL_IN_VARDECLCOMMAND("Variable declaration command does accept {2}." +
+  INVALID_DECL_IN_VARDECLCOMMAND("Variable declaration command does accept ''{2}''." +
     "\n\tProcess: {0}" +
-    "\n\tAction: {1}");
+    "\n\tAction: {1}",
+    "Variable declaration commands only accept a list of (possibly qualified) variables.\n\t" +
+    "That is, schema inclusions, constant declarations, and other forms are not allowed."),
+  INVALID_COMMUNICATION_PATTERN_WRT_CHANNEL_TYPE("Invalid field list encountered during typechecking: wrong number of variables w.r.t. field count." +
+    "\n\tProcess...: {0}" +
+    "\n\tAction....: {1}" +
+    "\n\tChannel...: {2}" +    
+    "\n\tExpected..: {3} (''{4}*4'' from input + ''{5}'' from output)" + 
+    "\n\tFound.....: {6}",
+    "The number of expressions found (i.e., declared variable references or expressions themselves)\n\t" +
+    "does not match the expected count. That can only happen because of a type mismatch on some\n\t" +
+    "output prefix expression with respect to the remainder type dimensions of a particular\n\t" +
+    "communication pattern (e.g., c?x!y, where c has A x B x C type, but y has a type not unifiable with B x C).\n\t" +
+    "This generates a type error. If you see this warning without a type error, please report this as a bug.")
+  ;
   
   private final String message_;
   private final String explanation_;
@@ -59,7 +73,7 @@ public enum WarningMessage {
   String getFullMessage()
   {
     String result = getMessage();
-    if (!flagged_) result += getExplanation();
+    if (!flagged_) result += "\n\t" + getExplanation();
     return result;
   }
 }
