0b4384b5a9201e6da9dd379ac670548f6992ea05
Leo Freitas <leouk@users.sourceforge.net>
2008-03-04 18:25:02 +0000

    ADD: used communications scope

--- typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/util/TypeEnv.java
+++ typechecker-circus/src/main/java/net/sourceforge/czt/typecheck/circus/util/TypeEnv.java
@@ -13,6 +13,9 @@ package net.sourceforge.czt.typecheck.circus.util;
 import java.util.Map;
 import java.util.Stack;
 import java.util.List;
+import net.sourceforge.czt.circus.ast.Communication;
+import net.sourceforge.czt.circus.ast.CommunicationType;
+import net.sourceforge.czt.circus.ast.CircusCommunicationList;
 import net.sourceforge.czt.z.util.ZString;
 import net.sourceforge.czt.z.ast.NameTypePair;
 import net.sourceforge.czt.z.ast.ZName;
@@ -50,6 +53,8 @@ public class TypeEnv
    */
   protected Stack<Map<ZName, NameTypePair>> genericImplicitChans_;
  
+  
+  protected Stack<CircusCommunicationList> usedComm_;
   /**
    * Creates a new instance of TypeEnv
    */
@@ -58,7 +63,8 @@ public class TypeEnv
     super(circusFactory);
     usedChans_ = new Stack<Map<ZName, NameTypePair>>();
     genericImplicitChans_ = new Stack<Map<ZName, NameTypePair>>();
-  }  
+    usedComm_ = new Stack<CircusCommunicationList>();
+  }
  
   /**
    * 
@@ -76,7 +82,9 @@ public class TypeEnv
     Map<ZName, NameTypePair> usedChans = GlobalDefs.map();
     usedChans_.push(usedChans);
     Map<ZName, NameTypePair> genericImplicitChans = GlobalDefs.map();
-    genericImplicitChans_.push(genericImplicitChans);    
+    genericImplicitChans_.push(genericImplicitChans);        
+    CircusCommunicationList commList = getFactory().getCircusFactory().createCircusCommunicationList();
+    usedComm_.push(commList);
   }
 
   @Override
@@ -92,6 +100,7 @@ public class TypeEnv
    */
   protected List<NameTypePair> getUsedChans()
   {
+    assert !usedChans_.isEmpty() : "empty stack of used channels";
     List<NameTypePair> result = getFactory().list();
     result.addAll(usedChans_.peek().values());
     return result;
@@ -103,23 +112,41 @@ public class TypeEnv
    */
   protected List<NameTypePair> getImplicitUsedChans()
   {
+    assert !genericImplicitChans_.isEmpty() : "empty stack of implicitly used channels";
     List<NameTypePair> result = getFactory().list();
     result.addAll(genericImplicitChans_.peek().values());
     return result;
     
   }
   
+  protected CircusCommunicationList getUsedCommunicationList()
+  {
+    assert !usedComm_.isEmpty() : "empty stack of used communications";
+    CircusCommunicationList result = getFactory().getCircusFactory().createCircusCommunicationList();
+    result.addAll(usedComm_.peek());
+    return result;
+  }
+  
   /**
    * Add the channel as used within the current context only.
    */
   public void addUsedChannels(boolean implicit, NameTypePair... chans) {
+    assert (implicit ? !genericImplicitChans_.isEmpty() : 
+      !usedChans_.isEmpty()) : "empty stack of used channels";
     Map<ZName, NameTypePair> uc = implicit ? genericImplicitChans_.peek() : usedChans_.peek();
     for(NameTypePair pair : chans) {
       NameTypePair hasNTP = getX(pair.getZName(), uc);
       if (hasNTP == null)      
       {
-        uc.put(hasNTP.getZName(), hasNTP);
+        uc.put(pair.getZName(), pair);
       }      
     }
   }
+  
+   public void addUsedCommunication(Communication comm)
+  {
+    assert !usedComm_.isEmpty() : "empty stack of used communications";
+    CircusCommunicationList ccl = usedComm_.peek();
+    ccl.add(comm);        
+  } 
 }
