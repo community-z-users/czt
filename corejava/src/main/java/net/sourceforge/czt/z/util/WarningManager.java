80831c490ab3df5e97d26cad88eb7c2ab9c36ad0
Leo Freitas <leouk@users.sourceforge.net>
2008-03-05 17:21:09 +0000

    COSMETICS: formatting style ADD: method to allow changing the logger class

--- corejava/src/main/java/net/sourceforge/czt/z/util/WarningManager.java
+++ corejava/src/main/java/net/sourceforge/czt/z/util/WarningManager.java
@@ -1,21 +1,18 @@
 /*
-  Copyright (C) 2006 Leo Freitas
-  This file is part of the czt project.
-
-  The czt project contains free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  The czt project is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with czt; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+Copyright (C) 2006 Leo Freitas
+This file is part of the czt project.
+The czt project contains free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+The czt project is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with czt; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.sourceforge.czt.z.util;
 
 import java.text.MessageFormat;
@@ -30,69 +27,98 @@ import net.sourceforge.czt.util.CztLogger;
  *
  * @author Leo Freitas
  */
-public class WarningManager {
-    
-    private String currentSectName_;
-    private final Class<?> loggerClass_;
-    private final TreeMap<String, List<String>> sectWarnings_;
-    
-    public WarningManager() {
-        loggerClass_ = getClass();
-        currentSectName_ = Section.ANONYMOUS.getName();
-        sectWarnings_ = new TreeMap<String, List<String>>();
-    }
-    
-    /** Creates a new instance of WarningManager
-     * @param forLogger 
-     */
-    public WarningManager(Class<?> forLogger) {        
-        loggerClass_ = forLogger;
-        currentSectName_ = Section.ANONYMOUS.getName();
-        sectWarnings_ = new TreeMap<String, List<String>>();
-    }
-    
-    /**
-     * Derived classes can change the formatting to take into account, say,
-     * pretty printing.
-     * @param message
-     * @param arguments
-     * @return
-     */
-    protected String format(String message, Object... arguments)
+public class WarningManager
+{
+
+  private String currentSectName_;
+  private Class<?> loggerClass_;
+  private final TreeMap<String, List<String>> sectWarnings_;
+
+  public WarningManager()
+  {
+    loggerClass_ = getClass();
+    currentSectName_ = Section.ANONYMOUS.getName();
+    sectWarnings_ = new TreeMap<String, List<String>>();
+  }
+
+  /** Creates a new instance of WarningManager
+   * @param forLogger 
+   */
+  public WarningManager(Class<?> forLogger)
+  {
+    this();
+    setLoggerClass(forLogger);   
+  }
+
+  /**
+   * Derived classes can change the formatting to take into account, say,
+   * pretty printing.
+   * @param message
+   * @param arguments
+   * @return
+   */
+  protected String format(String message, Object... arguments)
+  {
+    return MessageFormat.format(message, arguments);
+  }
+
+  public Map<String, List<String>> getZSectWarnings()
+  {
+    return Collections.unmodifiableSortedMap(sectWarnings_);
+  }
+
+  public void clear()
+  {
+    sectWarnings_.clear();
+  }
+
+  public String getCurrentSectName()
+  {
+    String result = currentSectName_;
+    if (result == null || result.isEmpty())
     {
-      return MessageFormat.format(message, arguments);
+      result = Section.ANONYMOUS.getName();
     }
-    
-    public Map<String, List<String>> getZSectWarnings() {
-        return Collections.unmodifiableSortedMap(sectWarnings_);
+    return result;
+  }
+
+  public void setCurrentSectName(String sectName)
+  {
+    currentSectName_ = sectName;
+  }
+
+  public Class<?> getLoggerClass()
+  {
+    return loggerClass_;
+  }
+
+  public Class<?> setLoggerClass(Class<?> cls)
+  {
+    Class<?> old = loggerClass_;
+    loggerClass_ = cls;
+    return old;
+  }
+
+  protected String createWarning(String message, Object... arguments)
+  {
+    final String msg = format(message, arguments);
+    final String sect = getCurrentSectName();
+    if (!sectWarnings_.containsKey(sect))
+    {
+      sectWarnings_.put(sect, new ArrayList<String>());
     }
-    
-    public void clear() {
-        sectWarnings_.clear();
-    }
-    
-    public String getCurrentSectName() {
-        String result = currentSectName_;
-        if (result == null || result.isEmpty())
-            result = Section.ANONYMOUS.getName();
-        return result;
-    }
-    
-    public void setCurrentSectName(String sectName) {
-        currentSectName_ = sectName;
-    }
-    
-    public Class<?> getLoggerClass() {
-        return loggerClass_;
-    }
-    
-    public void warn(String message, Object... arguments) {
-      final String msg = format(message, arguments);
-      final String sect = getCurrentSectName();
-      if (!sectWarnings_.containsKey(sect)) {          
-          sectWarnings_.put(sect, new ArrayList<String>());        
-      }
-      sectWarnings_.get(sect).add(msg);
-      CztLogger.getLogger(loggerClass_).warning(msg);
-  }     
+    sectWarnings_.get(sect).add(msg);
+    return msg;
+  }
+
+  protected void doWarn(String msg)
+  {
+    CztLogger.getLogger(loggerClass_).warning(msg);
+  }
+
+  public void warn(String message, Object... arguments)
+  {
+    String str = createWarning(message, arguments);
+    doWarn(str);
+  }
 }
