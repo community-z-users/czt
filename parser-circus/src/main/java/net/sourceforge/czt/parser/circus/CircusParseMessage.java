383e0aaaef7d73f986c43bf2cf9e0d8a0b0a9273
Leo Freitas <leouk@users.sourceforge.net>
2008-03-10 11:45:29 +0000

    UPDATE: case of error decl in ParamAction to reflect change in ParamCommand/Action protocol

--- parser-circus/src/main/java/net/sourceforge/czt/parser/circus/CircusParseMessage.java
+++ parser-circus/src/main/java/net/sourceforge/czt/parser/circus/CircusParseMessage.java
@@ -59,7 +59,7 @@ public enum CircusParseMessage {
   MSG_COMMPATTERN_PREFIXCOLON_NOT_RECOGNISED("Possible missing parenthesis around predicate for input field ''{0}''"),
   MSG_CIRCNAME_DOESNOT_ALLOW_STROKES("Circus name ''{0}'' does not accept strokes."),
   
-  MSG_NOT_VARDECL_IN_PARAMACTION("Parameterised actions can only contain variable declaration but a ''{0}'' has been found."),
+  MSG_ERRORDECL_IN_PARAMACTION("Parameterised actions/commands can only contain variable/qualified declarations."),
   MSG_NESTED_PARAMACTION("Nested action parameters are not allowed."),
   MSG_WRONG_NUMBER_FIELD_STROKES("Wrong number of fields in communication pattern." +
       "\n\tChannel.: {0}" +
