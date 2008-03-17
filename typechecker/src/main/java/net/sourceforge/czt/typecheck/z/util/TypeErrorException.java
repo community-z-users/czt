3a55370561431806aff2a691f945d75a18ad6d5d
Leo Freitas <leouk@users.sourceforge.net>
2008-03-17 18:05:46 +0000

    COSMESTICS: copyright notice

--- typechecker/src/main/java/net/sourceforge/czt/typecheck/z/util/TypeErrorException.java
+++ typechecker/src/main/java/net/sourceforge/czt/typecheck/z/util/TypeErrorException.java
@@ -1,12 +1,21 @@
 /*
- * TypeErrorException.java
- *
- * Created on 18 November 2005, 10:11
- *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
- */
+  Copyright (C) 2005 Leo Freitas
+  This file is part of the czt project.
+
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 package net.sourceforge.czt.typecheck.z.util;
 
@@ -18,7 +27,7 @@ import net.sourceforge.czt.parser.util.CztErrorList;
 
 /**
  *
- * @author leo
+ * @author Leo Freitas
  */
 public class TypeErrorException extends RuntimeException
   implements CztErrorList {    
