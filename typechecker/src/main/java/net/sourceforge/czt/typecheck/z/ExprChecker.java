66ae74f1e4ffdeec15b494a24e7cacec93adb530
Petra Malik <petramalik@users.sourceforge.net>
2008-06-22 04:58:46 +0000

    COSMETICS:   remove unused imports.

--- typechecker/src/main/java/net/sourceforge/czt/typecheck/z/ExprChecker.java
+++ typechecker/src/main/java/net/sourceforge/czt/typecheck/z/ExprChecker.java
@@ -24,11 +24,8 @@ import static net.sourceforge.czt.typecheck.z.util.GlobalDefs.*;
 import static net.sourceforge.czt.z.util.ZUtils.*;
 
 import net.sourceforge.czt.z.util.ZString;
-import net.sourceforge.czt.base.ast.*;
 import net.sourceforge.czt.z.ast.*;
 import net.sourceforge.czt.z.visitor.*;
-import net.sourceforge.czt.base.util.*;
-import net.sourceforge.czt.base.visitor.*;
 import net.sourceforge.czt.typecheck.z.util.*;
 import net.sourceforge.czt.typecheck.z.impl.*;
 
