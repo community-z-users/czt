aba1d9f6db3d9bfe89a522601268e1cf1c1808ea
Jerramy Winchester <jwinchester@users.sourceforge.net>
2008-07-20 00:20:30 +0000

    A listener for the visualisation so that it can receive information about ModelJUnit graph explorations.

---
+++ modeljunit/src/main/java/net/sourceforge/czt/modeljunit/gui/visualisaton/VisualisationListener.java
@@ -0,0 +1,47 @@
+package net.sourceforge.czt.modeljunit.gui.visualisaton;
+
+import net.sourceforge.czt.modeljunit.AbstractListener;
+import net.sourceforge.czt.modeljunit.Transition;
+
+/** An implementation of ModelListener that prints
+ *  event messages to the Model's <code>getOutput()</code> stream.
+ */
+public class VisualisationListener extends AbstractListener
+{
+	private JUNGHelper jView_ = JUNGHelper.getJUNGViewInstance();
+
+	public VisualisationListener()
+	{
+	}
+
+	public String getName()
+	{
+		return "visualGraph";
+	}
+
+	public void doneReset(String reason, boolean testing)
+	{    
+		jView_.graphDoneReset("done " + reason + " reset("+testing+")\n");
+	}
+
+	public void doneGuard(Object state, int action, boolean enabled, int value)
+	{
+	}
+
+	public void startAction(Object state, int action, String name)
+	{
+	}
+
+	public void doneTransition(int action, Transition tr)
+	{    
+		jView_.visitedVertex(tr.getStartState());
+		jView_.visitedVertex(tr.getEndState());
+		jView_.visitedEdge(tr);
+	}
+
+	public void failure(Exception ex)
+	{
+		System.out.println("FAILURE: "+ex.getLocalizedMessage());
+	}
+}
+
