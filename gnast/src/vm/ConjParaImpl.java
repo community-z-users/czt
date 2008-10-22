4d71cea8b82418d49d942df9e86301eea218f1e0
Mark Utting <marku@users.sourceforge.net>
2008-10-22 04:45:26 +0000

    ADD: support for conjecture names in the Unicode markup and in the AST (as annotations).

---
+++ gnast/src/vm/ConjParaImpl.java
@@ -0,0 +1,20 @@
+
+  /**
+   * This is a convenience method for getting the name of a conjecture.
+   * Conjecture names are currently optional at the Unicode level, and 
+   * are stored as annotations rather than as normal entries in the
+   * CZT XML format.  This method returns a non-null String if the conjecture
+   * has a name annotation, or null otherwise.  If the Z standard is
+   * extended in the future so that conjecture names are compulsory
+   * in the Unicode markup (as they are in the LaTeX markup), then
+   * this method may be replaced by an equivalent method that
+   * is automatically generated from the CZT XML schema.
+   */
+  public String getName()
+  {
+    Name name = this.getAnn(Name.class);
+    if (name instanceof ZName) {
+      return ((ZName) name).getWord();
+    }
+    return null;
+  }
