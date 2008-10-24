f76aa4290f4a7178f6566e1b86c699be5bf43137
Leo Freitas <leouk@users.sourceforge.net>
2008-10-24 15:07:56 +0000

    ADD: documentation to the improved lookup algorithm ADD: debugging information points to make sure things are they way we expect. It just adds a log entry to the section manager logger ADD: czt.path treatment as a list of paths separated by ; ADD: two public static methods that retrieve CZT properties loading then into the section manager, and the one that processes the CZT paths with regular expression splitting.
    ADD: throwable cause to SourceLocatorException 
    
    OBS: The SourceLocator is the only place where 
         czt.path is being dealt with so far (i.e.,
         as in splitting it into various lookup paths).   
         Everywhere else, just tries to set czt.path.

--- session/src/main/java/net/sourceforge/czt/session/SourceLocator.java
+++ session/src/main/java/net/sourceforge/czt/session/SourceLocator.java
@@ -20,7 +20,14 @@
 package net.sourceforge.czt.session;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  * A command to compute the URL for a Z section.
@@ -37,31 +44,163 @@ public class SourceLocator
     ".circus", ".circus8", ".circus16", ".zedpatt", ".zedpatt8", ".zedpatt16",
     ".zml"};
 
+  /**
+   * Tries to locate the resource named for the given section manager.
+   * The lookup algorithm for name as follows:
+   * 
+   * <ol>
+   *   <li> checks if is a toolkit within czt.jar!/lib/name.tex
+   *   <li> checks under current directory as ./name.suffixes, 
+   *        where the sufixes include all possible values allowed
+   *        (e.g., .tex, .zed, .oz, .zed8, .zml, .circus, etc)
+   *   <li> checks for czt.path within <code>SectionManager</code> properties:
+   *        <ol>
+   *          <li> if availble, looks for czt.path/name.suffixes; 
+   *          <li> otherwise, looks for czt.path within user.dir/czt.properties
+   *        </ol>
+   *   <li> checks if user.dir/czt.properties has czt.path within it,
+   *        and if so, checks its value with name.suffixes.
+   * </ol>
+   * 
+   * The first successful check in this order wins. If none work,
+   * a <code>SourceLocatorException</code> is raised with the latest calculated
+   * path and the name as additional information. Note that <code>czt.path</code>
+   * can be a semicolon-separated list of paths looked in FIFO order. That means,
+   * if a file is found at an earlier directory, that is the one used.
+   * 
+   * @param name the resource we are trying to locate.
+   * @param manager the manager requesting the resource.
+   * @return true if resource found, raises an exception otherwise.
+   * @throws net.sourceforge.czt.session.CommandException if resource not found.
+   */
+  @Override
   public boolean compute(String name, SectionManager manager)
     throws CommandException
   {
-    URL url = getClass().getResource("/lib/" + name + ".tex");
+    debug("SL-locate " + name);
+    // see if "name" is a toolkit (i.e., czt.jar/lib/name.tex)
+    String filename = "/lib/" + name + ".tex";
+    URL url = getClass().getResource(filename);
     if (url != null) {
+      debug("SL-TK-found="+filename);
       manager.put(new Key<Source>(name, Source.class), new UrlSource(url));
       return true;
     }
+    debug("SL-non-toolkit ; try current directory.");
+    // see if "name" is within the current directory 
+    // with all possible sufixes (i.e., ./name.suffix)
     for (int i = 0; i < suffix_.length; i++) {
-      File file = new File(name + suffix_[i]);
-      if (file.exists() && ! file.isDirectory()) {
-        manager.put(new Key<Source>(name, Source.class), new FileSource(file));
-        return true;
-      }
-    }
-    String path = (String) manager.getProperty("czt.path");
-    for (int i = 0; i < suffix_.length; i++) {
-      String filename = path + "/" + name + suffix_[i];
+      filename = name + suffix_[i];
       File file = new File(filename);
-      if (file.exists()) {
+      if (file.exists() && ! file.isDirectory()) {
+        debug("SL-CURDIR-found="+filename);
         manager.put(new Key<Source>(name, Source.class), new FileSource(file));
         return true;
       }
     }
+    debug("SL-non-current-dir ; try czt.path");
+    // try to retrieve czt.path
+    List<String> cztpaths = new ArrayList<String>();
+    String path = manager.getProperty("czt.path");
+    debug("SL-SM-czt.path   = " + path);    
+    // if empty or null, try czt.properties
+    if (path == null || path.isEmpty())
+    {
+      Properties cztprops = retrieveCZTProperties();
+      if (cztprops != null)
+      {
+        // gets within czt.properties (if it exists) czt.path or "." by default
+        path = cztprops.getProperty("czt.path", ".");
+        debug("SL-CZTP-SM-setProperties");
+        // sets the manager with all properties within cztprops
+        // hence overriding any previous properties within it.
+        manager.setProperties(cztprops);                              
+        debug("SL-CZTP-czt.properties=" + cztprops);
+      }
+    }
+    if (path != null && !path.isEmpty())
+    {
+      // split the paths nicely
+      cztpaths = processCZTPaths(path);    
+      for (String cztpath : cztpaths)
+      {     
+        debug("SL-CZTP-cztpath[i]=" + cztpath);
+        // check czt.path/name with all possible sufixes as last resort
+        for (int i = 0; i < suffix_.length; i++) {
+          filename = cztpath + "/" + name + suffix_[i];
+          File file = new File(filename);
+          if (file.exists()) {
+            debug("SL-CZTP-found="+filename);
+            manager.put(new Key<Source>(name, Source.class), new FileSource(file));
+            return true;
+          }
+        }
+      }
+    }
+    debug("SL-CZTP-FAILED="+path);
+    // raise an error that name could not be found.
     throw new SourceLocatorException(name, path);
+  }
+  
+  
+  /**
+   * Retrieve the czt.properties file within user.dir or "."
+   * @return the properties file containing information from czt.properties, or null if not found.
+   * @throws CommandException if czt.properties cannot be processed
+   */
+  public static Properties retrieveCZTProperties() throws CommandException
+  {
+    debug("Trying czt.path as user.dir and look for czt.properties");
+    // if not set, look for the user working directory, or "." by default      
+    String path = System.getProperty("user.dir", ".");
+    debug("SL-SYS-user.dir  = " + path);
+    String filename = path + "/czt.properties";
+    File cztpropsfile = new File(filename);
+    debug("SL-CZTP-fileexists? = " + filename + " " + cztpropsfile.exists());
+    Properties cztprops = null;
+    if (cztpropsfile.exists())
+    {
+        // if czt.properties lives on the working directory (or ".")
+        // retrieve its values
+        cztprops = new Properties();
+        debug("SL-CZTP-retrival");
+        try 
+        {          
+          FileInputStream fis = new FileInputStream(cztpropsfile);
+          //cztprops.loadFromXML(fis);          
+          cztprops.load(fis);
+        } catch (IOException e)
+        {        
+          throw new SourceLocatorException("czt.properties", path, e);
+        }        
+    }
+    debug("SL-CZTP-czt.path = " + path);
+    return cztprops;
+  }
+  
+  /**
+   * Very simple path splitting through <code>path.split(";")</code>.
+   * Any more complicated behaviour can be defined differently elsewhere.
+   * 
+   * @param path the string to split, it expects the path to be non-null and non-empty
+   * @return a list of paths to search for files
+   */
+  public static List<String> processCZTPaths(String path)
+  {
+    assert path != null && !path.isEmpty() : "invalid czt.path to process";
+    return Arrays.asList(path.split(";"));
+  }
+  
+  private static Logger getLogger()
+  {
+    return Logger.getLogger(SectionManager.class.getName());
+  }
+  
+  private static void debug(String msg)
+  {    
+    // log the stuff done through searching
+    //System.out.println(msg);
+    getLogger().fine(msg);
   }
 
   /**
@@ -81,6 +220,14 @@ public class SourceLocator
       name_ = name;
       path_ = path;
     }
+    
+    public SourceLocatorException(String name, String path, Throwable cause)
+    {
+      super("Cannot find source for section " + name
+          + " with czt.path="+path, cause);
+      name_ = name;
+      path_ = path;      
+    }
 
     public String getName()
     {
@@ -91,5 +238,5 @@ public class SourceLocator
     {
       return path_;
     }
-  }
+  }  
 }
