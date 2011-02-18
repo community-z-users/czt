6565cb2a5906c299f64c9ea52c503489d9246162
Leo Freitas <leouk@users.sourceforge.net>
2011-02-18 11:39:54 +0000

    ADD: fuzz toolkit name

--- corejava/src/main/java/net/sourceforge/czt/z/util/Section.java
+++ corejava/src/main/java/net/sourceforge/czt/z/util/Section.java
@@ -37,9 +37,13 @@ public enum Section
   /** The name of the mathematical toolkit. */
   STANDARD_TOOLKIT("standard_toolkit"),
   NUMBER_TOOLKIT("number_toolkit"),
+  FUNCTION_TOOLKIT("function_toolkit"),
   RELATION_TOOLKIT("relation_toolkit"),
   SEQUENCE_TOOLKIT("sequence_toolkit"),
-  SET_TOOLKIT("set_toolkit");
+  SET_TOOLKIT("set_toolkit"),
+
+  /** Extra (Old-Z) toolkit name. */
+  FUZZ_TOOLKIT("fuzz_toolkit");
 
   private String name_;
 
