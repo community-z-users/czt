10bd40aa4ba1f4df8ca6b1df01d1eaf92188f53b
Leo Freitas <leouk@users.sourceforge.net>
2011-07-08 16:29:45 +0000

    ADD: trivial / placeholder PrintUtils. STILL TODO; just so that the typechecker works

---
+++ parser-zeves/src/main/java/net/sourceforge/czt/print/zeves/ZmlScanner.java
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2004 Petra Malik
+  This file is part of the czt project.
+
+  The czt project contains free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  The czt project is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with czt; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sourceforge.czt.print.zeves;
+
+import java.util.Iterator;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.parser.util.Token;
+import net.sourceforge.czt.print.z.PrecedenceParenAnnVisitor;
+import net.sourceforge.czt.print.z.ZmlScanner.SymbolCollector;
+import net.sourceforge.czt.z.util.WarningManager;
+
+/**
+ * This Scanner uses the print visitor to tokenize a
+ * given Z/Circus term.
+ *
+ * @author Petra Malik
+ */
+public class ZmlScanner
+  extends net.sourceforge.czt.print.z.ZmlScanner
+{
+  /**
+   * Creates a new ZML scanner.
+   */
+  public ZmlScanner(Term term, WarningManager manager)
+  {
+    PrecedenceParenAnnVisitor precVisitor =
+      new PrecedenceParenAnnVisitor();
+    term.accept(precVisitor);
+    SymbolCollector collector = new SymbolCollector(Sym.class);
+    ZEvesPrintVisitor visitor = new ZEvesPrintVisitor(collector, manager);
+    term.accept(visitor);
+    symbols_ = collector.getSymbols();
+
+  }
+
+  public ZmlScanner(Iterator<Token> iter)
+  {
+    super(iter);
+  }
+}
