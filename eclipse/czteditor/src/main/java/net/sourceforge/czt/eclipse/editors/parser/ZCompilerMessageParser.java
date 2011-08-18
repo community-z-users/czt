3dbe452eff2491269d72f8a8537918f253d1ab75
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-08-18 09:08:18 +0000

    UPDATE: Updated the CZT marker generation to be done in batch mode, which significantly speeds up error reporting for large specifications with many errors.

--- eclipse/czteditor/src/main/java/net/sourceforge/czt/eclipse/editors/parser/ZCompilerMessageParser.java
+++ eclipse/czteditor/src/main/java/net/sourceforge/czt/eclipse/editors/parser/ZCompilerMessageParser.java
@@ -1,12 +1,11 @@
-/**
- * 
- */
-
 package net.sourceforge.czt.eclipse.editors.parser;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import net.sourceforge.czt.eclipse.util.IZMarker;
 import net.sourceforge.czt.parser.util.CztError;
@@ -14,13 +13,16 @@ import net.sourceforge.czt.parser.util.ErrorType;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.ui.texteditor.MarkerUtilities;
 
 /**
  * @author Chengdong Xu
+ * @author Andrius Velykis
  */
 public class ZCompilerMessageParser
 {
@@ -29,23 +31,24 @@ public class ZCompilerMessageParser
 
   private IDocument fDocument = null;
 
-  /**
-   * 
-   */
   public void parseCompilerMessage(IDocument document, IResource resource,
       List<CztError> errors) throws CoreException
   {
     this.fResource = resource;
     this.fDocument = document;
+    
+    List<Entry<String, Map<String, Object>>> markers = new ArrayList<Entry<String, Map<String, Object>>>();
     for (int i = 0; i < errors.size(); i++) {
-      parseError(errors.get(i));
+      markers.add(parseError(errors.get(i)));
     }
+    
+    createMarkers(markers);
   }
 
   /**
    * @param error a Czt error
    */
-  protected void parseError(CztError error) throws CoreException
+  protected Entry<String, Map<String, Object>> parseError(CztError error)
   {
     ErrorType errorType = error.getErrorType();
     String message = error.getMessage();
@@ -70,7 +73,7 @@ public class ZCompilerMessageParser
     end = start + length;
 
     int severity = ErrorType.ERROR.equals(errorType) ? IMarker.SEVERITY_ERROR : IMarker.SEVERITY_WARNING;
-    setMarker(severity, message, line + 1, start, end);
+    return setMarker(severity, message, line + 1, start, end);
   }
 
   /**
@@ -80,8 +83,8 @@ public class ZCompilerMessageParser
    * @param charEnd
    * @throws CoreException
    */
-  protected void setMarker(int severity, String message, int lineNumber, int charStart,
-      int charEnd) throws CoreException
+  private Entry<String, Map<String, Object>> setMarker(int severity, String message,
+      int lineNumber, int charStart, int charEnd)
   {
     Map<String, Object> attributes = new HashMap<String, Object>();
 
@@ -92,6 +95,26 @@ public class ZCompilerMessageParser
     attributes.put(IMarker.CHAR_END, new Integer(charEnd));
     attributes.put(IMarker.MESSAGE, message);
 
-    MarkerUtilities.createMarker(this.fResource, attributes, IZMarker.PROBLEM);
+    // mark all markers and create later in batch mode to avoid too many refresh events
+//    MarkerUtilities.createMarker(this.fResource, attributes, IZMarker.PROBLEM);
+    return new SimpleEntry<String, Map<String, Object>>(IZMarker.PROBLEM, attributes);
+  }
+  
+  public void createMarkers(final List<Entry<String, Map<String, Object>>> markers)
+      throws CoreException
+  {
+
+    IWorkspaceRunnable r = new IWorkspaceRunnable()
+    {
+      public void run(IProgressMonitor monitor) throws CoreException
+      {
+        for (Entry<String, Map<String, Object>> markerEntry : markers) {
+          IMarker marker = fResource.createMarker(markerEntry.getKey());
+          marker.setAttributes(markerEntry.getValue());
+        }
+      }
+    };
+
+    fResource.getWorkspace().run(r, null, IWorkspace.AVOID_UPDATE, null);
   }
 }
