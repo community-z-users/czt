1ab640884a21770d202ac7831ec4398afc683e2b
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-08-23 12:53:38 +0000

    UPDATE: Moved ZEditorUtil and FilteredTree2 upstream into czteditor from zeves package. This allows reuse in VCG project

---
+++ eclipse/czteditor/src/main/java/net/sourceforge/czt/eclipse/editors/zeditor/ZEditorUtil.java
@@ -0,0 +1,104 @@
+package net.sourceforge.czt.eclipse.editors.zeditor;
+
+import net.sourceforge.czt.eclipse.editors.IZReconcilingListener;
+import net.sourceforge.czt.eclipse.editors.parser.ParsedData;
+import net.sourceforge.czt.parser.util.CztError;
+import net.sourceforge.czt.parser.util.ErrorType;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+
+public class ZEditorUtil {
+
+  public static int getCaretPosition(ZEditor editor)
+  {
+    return editor.getViewer().getTextWidget().getCaretOffset();
+  }
+
+  public static boolean setCaretPosition(ZEditor editor, int position)
+  {
+    try {
+      editor.getViewer().getTextWidget().setCaretOffset(position);
+      editor.getViewer().getTextWidget().setSelection(position);
+    }
+    catch (IllegalArgumentException ex) {
+      // invalid, but ignore?
+      return false;
+    }
+
+    return true;
+  }
+
+  public static IResource getEditorResource(IEditorPart editor)
+  {
+
+    if (editor == null) {
+      return null;
+    }
+
+    return (IResource) ((IAdaptable) editor.getEditorInput()).getAdapter(IResource.class);
+  }
+
+  public static IDocument getDocument(ITextEditor editor)
+  {
+
+    if (editor == null || editor.getDocumentProvider() == null) {
+      return null;
+    }
+
+    return editor.getDocumentProvider().getDocument(editor.getEditorInput());
+  }
+  
+  public static boolean hasErrors(ParsedData parsedData) {
+    
+    if (parsedData == null) {
+      return true;
+    }
+    
+    for (CztError err : parsedData.getErrors()) {
+      if (err.getErrorType() == ErrorType.ERROR) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+
+  public static void runOnReconcile(final ZEditor editor, final ReconcileRunnable callback)
+  {
+    callback.setEditor(editor);
+    editor.addReconcileListener(callback);
+  }
+
+  public static abstract class ReconcileRunnable implements IZReconcilingListener
+  {
+
+    private ZEditor editor;
+
+    public void setEditor(ZEditor editor)
+    {
+      this.editor = editor;
+    }
+
+    @Override
+    public void reconciled(ParsedData parsedData, boolean forced, IProgressMonitor progressMonitor)
+    {
+      // remove itself from listeners - this was one-shot event
+      editor.removeReconcileListener(this);
+      run(parsedData);
+    }
+
+    @Override
+    public void aboutToBeReconciled()
+    {
+    }
+
+    protected abstract void run(ParsedData parsedData);
+  }
+
+}
