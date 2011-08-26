9201067fe94bea2aea9b975c440bf5e5630c2485
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-08-26 12:30:17 +0000

    UPDATE: For now use Replace instead of rename. This is because Z/Eves CZT parser does not support mixed cases of rename/replace. Since we cannot escape replace, and renaming can be substituted by one, we use replace everywhere for successful parsing.

--- zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesRename.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesRename.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 
-import net.sourceforge.czt.z.util.ZString;
-
 /**
  * <!ELEMENT rename (name, name)>
  * 
@@ -19,9 +17,16 @@ public class ZEvesRename {
 
 	@Override
 	public String toString() {
-		return String.valueOf(names.get(0)) 
-				+ ZString.SPACE + ZString.SLASH + ZString.SPACE
-				+ String.valueOf(names.get(1));
+	        /*
+	         * For now use Replace instead of rename. This is because Z/Eves CZT parser
+	         * does not support mixed cases of rename/replace. Since we cannot escape
+	         * replace, and renaming can be substituted by one, we use replace everywhere
+	         * for successful parsing.
+	         */
+		return String.valueOf(names.get(0)) + " := " + String.valueOf(names.get(1));
+//		return String.valueOf(names.get(0)) 
+//				+ ZString.SPACE + ZString.SLASH + ZString.SPACE
+//				+ String.valueOf(names.get(1));
 	}
 	
 }
