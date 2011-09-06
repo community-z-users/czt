099f887924b8d35823840ebf3130da32913790ab
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-09-06 14:56:16 +0000

    UPDATE: Extracted common functionality into AbstractUndoCommand

--- eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesBackCommand.java
+++ eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesBackCommand.java
@@ -1,65 +1,19 @@
 package net.sourceforge.czt.eclipse.zeves.core;
 
-import java.util.Map;
 import net.sourceforge.czt.eclipse.editors.zeditor.ZEditor;
-import net.sourceforge.czt.eclipse.editors.zeditor.ZEditorUtil;
 import net.sourceforge.czt.eclipse.zeves.ZEvesPlugin;
-import net.sourceforge.czt.zeves.ZEvesApi;
-import net.sourceforge.czt.zeves.ZEvesException;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.text.IDocument;
-
-public class ZEvesBackCommand extends AbstractExecCommand {
-	
-	private final ZEditor editor;
+public class ZEvesBackCommand extends AbstractUndoCommand {
 	
 	public ZEvesBackCommand(ZEditor editor) {
-		this.editor = editor;
+		super(editor);
 	}
-
+	
 	@Override
-	public IStatus doExecute(IProgressMonitor monitor) {
-		
-		ZEves prover = ZEvesPlugin.getZEves();
-		if (!prover.isRunning()) {
-			return Status.OK_STATUS;
-		}
-
-        IResource resource = ZEditorUtil.getEditorResource(editor);
-        IDocument document = ZEditorUtil.getDocument(editor);
-        
-        if (resource == null || document == null) {
-        	return Status.OK_STATUS;
-        }
-        
-        ZEvesSnapshot snapshot = prover.getSnapshot();
-        String filePath = ResourceUtil.getPath(resource);
-        
-        int lastOffset = snapshot.getLastPositionOffset(filePath);
-        if (lastOffset < 0) {
-        	// nothing submitted in this editor - cannot go back
-        	// TODO undo in other editors?
-        	return Status.OK_STATUS;
-        }
-        
-        ZEvesApi zEvesApi = prover.getApi();
-        
+	protected int getUndoOffset(String filePath) {
         // undo through the last offset - this will undo the last result
-		try {
-			Map<String, Integer> fileUndoOffsets = 
-				snapshot.undoThrough(zEvesApi, filePath, lastOffset);
-
-			ResourceUtil.deleteMarkers(fileUndoOffsets);
-			
-		} catch (ZEvesException e) {
-			return ZEvesPlugin.newErrorStatus(e.getMessage(), e);
-		}
-        
-		return Status.OK_STATUS;
+		ZEvesSnapshot snapshot = ZEvesPlugin.getZEves().getSnapshot();
+		return snapshot.getLastPositionOffset(filePath);
 	}
 	
 }
--- eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesUndoCommand.java
+++ eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesUndoCommand.java
@@ -1,33 +1,20 @@
 package net.sourceforge.czt.eclipse.zeves.core;
 
-import java.util.Map;
-
-import net.sourceforge.czt.eclipse.editors.zeditor.ZEditorUtil;
-import net.sourceforge.czt.eclipse.zeves.ZEvesPlugin;
-import net.sourceforge.czt.zeves.ZEvesApi;
-import net.sourceforge.czt.zeves.ZEvesException;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.texteditor.ITextEditor;
 
-public class ZEvesUndoCommand implements IZEvesExecCommand {
+public class ZEvesUndoCommand extends AbstractUndoCommand {
 	
-	private final ITextEditor editor;
 	private final int offset;
 	
 	public ZEvesUndoCommand(ITextEditor editor, int offset) {
-		this.editor = editor;
+		super(editor);
 		this.offset = offset;
 	}
 	
 	@Override
 	public boolean canMerge(IZEvesExecCommand command) {
 		if (command instanceof ZEvesUndoCommand) {
-			return editor == ((ZEvesUndoCommand) command).editor;
+			return getEditor() == ((ZEvesUndoCommand) command).getEditor();
 		}
 		
 		return false;
@@ -43,40 +30,8 @@ public class ZEvesUndoCommand implements IZEvesExecCommand {
 	}
 
 	@Override
-	public IStatus execute(IProgressMonitor monitor) {
-		
-		ZEves prover = ZEvesPlugin.getZEves();
-		if (!prover.isRunning()) {
-			return Status.OK_STATUS;
-		}
-		
-        IResource resource = ZEditorUtil.getEditorResource(editor);
-        IDocument document = ZEditorUtil.getDocument(editor);
-        
-        if (resource == null || document == null) {
-        	return Status.OK_STATUS;
-        }
-        
-        ZEvesSnapshot snapshot = prover.getSnapshot();
-        String filePath = ResourceUtil.getPath(resource);
-        
-        if (!snapshot.needUndo(filePath, offset)) {
-        	// editing after last submission - no undo necessary
-        	return Status.OK_STATUS;
-        }
-		
-        ZEvesApi zEvesApi = prover.getApi();
-        
-		try {
-			Map<String, Integer> fileUndoOffsets = 
-				snapshot.undoThrough(zEvesApi, filePath, offset);
-
-			ResourceUtil.deleteMarkers(fileUndoOffsets);
-			
-		} catch (ZEvesException e) {
-			return ZEvesPlugin.newErrorStatus(e.getMessage(), e);
-		}
-		
-		return Status.OK_STATUS;
+	protected int getUndoOffset(String filePath) {
+		return offset;
 	}
+	
 }
