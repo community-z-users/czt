ef5ce502cc81ad5e32fa6f0e5c6e11686bbfaddc
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-09-19 21:09:54 +0000

    MINOR: Reformatted to CZT style

--- zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesName.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesName.java
@@ -1,3 +1,4 @@
+
 package net.sourceforge.czt.zeves.response.form;
 
 import java.util.List;
@@ -17,127 +18,119 @@ import net.sourceforge.czt.zeves.response.ZEvesResponseUtil;
  * @author Andrius Velykis
  */
 @XmlRootElement(name = "name")
-public class ZEvesName {
+public class ZEvesName
+{
 
-	/**
-	 * <!ATTLIST name scope (local | global) #IMPLIED>
-	 * 
-	 * @author Andrius Velykis
-	 */
-	public enum NameScope {
-		@XmlEnumValue("local")
-		LOCAL,
-		@XmlEnumValue("global")
-		GLOBAL
-	}
-	
-	/**
-	 * <!ATTLIST name style (roman | underlined | italic | bold | sans)
-	 * #IMPLIED>
-	 * 
-	 * @author Andrius Velykis
-	 */
-	public enum NameStyle {
-		@XmlEnumValue("roman")
-		ROMAN,
-		@XmlEnumValue("underlined")
-		UNDERLINED,
-		@XmlEnumValue("italic")
-		ITALIC,
-		@XmlEnumValue("bold")
-		BOLD,
-		@XmlEnumValue("sans")
-		SANS
-	}
-	
-	/**
-	 * <!ATTLIST name class (infun | ingen | inrel | pregen | prerel | postfun |
-	 * 						 word | opname | relimg | other) #IMPLIED>
-	 * 
-	 * @author Andrius Velykis
-	 */
-	public enum NameClass {
-		@XmlEnumValue("infun")
-		INFUN,
-		@XmlEnumValue("ingen")
-		INGEN,
-		@XmlEnumValue("inrel")
-		INREL,
-		@XmlEnumValue("pregen")
-		PREGEN,
-		@XmlEnumValue("prerel")
-		PREREL,
-		@XmlEnumValue("postfun")
-		POSTFUN,
-		@XmlEnumValue("word")
-		WORD,
-		@XmlEnumValue("opname")
-		OPNAME,
-		@XmlEnumValue("relimg")
-		RELIMG,
-		@XmlEnumValue("other")
-		OTHER
-	}
-	
-	/**
-	 * The value of the ident attribute is intended to include underbars and
-	 * decorations. <!ATTLIST name ident CDATA #REQUIRED>
-	 */
-	@XmlAttribute(required = true)
-	private String ident;
+  /**
+   * <!ATTLIST name scope (local | global) #IMPLIED>
+   * 
+   * @author Andrius Velykis
+   */
+  public enum NameScope {
+    @XmlEnumValue("local")
+    LOCAL, @XmlEnumValue("global")
+    GLOBAL
+  }
 
-	/**
-	 * The scope attribute is for scope specified by the user, not scope as
-	 * determined by Z/EVES.
-	 */
-	@XmlAttribute
-	private NameScope scope;
 
-	/**
-	 * The style attribute can be used to underline a relation name, or set a
-	 * name like pre or dom in roman font. (We might want to add more styles -
-	 * perhaps it should be mixed or CDATA?)
-	 */
-	@XmlAttribute
-	private NameStyle style;
+  /**
+   * <!ATTLIST name style (roman | underlined | italic | bold | sans)
+   * #IMPLIED>
+   * 
+   * @author Andrius Velykis
+   */
+  public enum NameStyle {
+    @XmlEnumValue("roman")
+    ROMAN, @XmlEnumValue("underlined")
+    UNDERLINED, @XmlEnumValue("italic")
+    ITALIC, @XmlEnumValue("bold")
+    BOLD, @XmlEnumValue("sans")
+    SANS
+  }
 
-	@XmlAttribute(name = "class")
-	private NameClass cclass;
 
-	@XmlAttribute
-	private ZEvesKind kind;
+  /**
+   * <!ATTLIST name class (infun | ingen | inrel | pregen | prerel | postfun |
+   * 						 word | opname | relimg | other) #IMPLIED>
+   * 
+   * @author Andrius Velykis
+   */
+  public enum NameClass {
+    @XmlEnumValue("infun")
+    INFUN, @XmlEnumValue("ingen")
+    INGEN, @XmlEnumValue("inrel")
+    INREL, @XmlEnumValue("pregen")
+    PREGEN, @XmlEnumValue("prerel")
+    PREREL, @XmlEnumValue("postfun")
+    POSTFUN, @XmlEnumValue("word")
+    WORD, @XmlEnumValue("opname")
+    OPNAME, @XmlEnumValue("relimg")
+    RELIMG, @XmlEnumValue("other")
+    OTHER
+  }
 
-	/**
-	 * <!ELEMENT genactuals (%form;)+>
-	 */
-	@XmlElement(name = "genactuals")
-	private XmlAnyElementList genActuals = new XmlAnyElementList();
+  /**
+   * The value of the ident attribute is intended to include underbars and
+   * decorations. <!ATTLIST name ident CDATA #REQUIRED>
+   */
+  @XmlAttribute(required = true)
+  private String ident;
 
-	@XmlElement
-	private ZEvesType type;
+  /**
+   * The scope attribute is for scope specified by the user, not scope as
+   * determined by Z/EVES.
+   */
+  @XmlAttribute
+  private NameScope scope;
 
-	public String getIdent() {
-		
-		// Z/Eves outputs a simple ' instead of Prime, and it is not separated as a decorator,
-		// so instead just check and replace
-		if (ident != null && ident.endsWith("'")) {
-			ident = ident.substring(0, ident.length() - 1) + ZString.PRIME;
-		}
-		
-		return ident;
-	}
+  /**
+   * The style attribute can be used to underline a relation name, or set a
+   * name like pre or dom in roman font. (We might want to add more styles -
+   * perhaps it should be mixed or CDATA?)
+   */
+  @XmlAttribute
+  private NameStyle style;
 
-	@Override
-	public String toString() {
-		return getIdent() + getGenActInfo(genActuals.getItems());
-	}
+  @XmlAttribute(name = "class")
+  private NameClass cclass;
 
-	public static String getGenActInfo(List<?> genActuals) {
-		if (genActuals.isEmpty()) {
-			return "";
-		}
+  @XmlAttribute
+  private ZEvesKind kind;
 
-		return "[" + ZEvesResponseUtil.concat(genActuals, ", ") + "]";
-	}
-	
+  /**
+   * <!ELEMENT genactuals (%form;)+>
+   */
+  @XmlElement(name = "genactuals")
+  private XmlAnyElementList genActuals = new XmlAnyElementList();
+
+  @XmlElement
+  private ZEvesType type;
+
+  public String getIdent()
+  {
+
+    // Z/Eves outputs a simple ' instead of Prime, and it is not separated as a decorator,
+    // so instead just check and replace
+    if (ident != null && ident.endsWith("'")) {
+      ident = ident.substring(0, ident.length() - 1) + ZString.PRIME;
+    }
+
+    return ident;
+  }
+
+  @Override
+  public String toString()
+  {
+    return getIdent() + getGenActInfo(genActuals.getItems());
+  }
+
+  public static String getGenActInfo(List<?> genActuals)
+  {
+    if (genActuals.isEmpty()) {
+      return "";
+    }
+
+    return "[" + ZEvesResponseUtil.concat(genActuals, ", ") + "]";
+  }
+
 }
