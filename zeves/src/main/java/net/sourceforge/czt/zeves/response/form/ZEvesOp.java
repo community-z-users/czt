4d2f28c214804c4e6ac5d42d300c80fd2aa7124f
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-09-19 21:49:30 +0000

    MINOR: Formatting to CZT style only

--- zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesOp.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/form/ZEvesOp.java
@@ -1,3 +1,4 @@
+
 package net.sourceforge.czt.zeves.response.form;
 
 import java.util.ArrayList;
@@ -16,76 +17,80 @@ import javax.xml.bind.annotation.XmlRootElement;
  *
  */
 @XmlRootElement(name = "op")
-public class ZEvesOp {
+public class ZEvesOp
+{
 
-	/**
-	 * <!ATTLIST op type (preop | inop | postop) #REQUIRED>
-	 * 
-	 * @author Andrius Velykis
-	 */
-	public enum OpType {
-		@XmlEnumValue("preop")
-		PREOP,
-		@XmlEnumValue("inop")
-		INOP,
-		@XmlEnumValue("postop")
-		POSTOP
-	}
-	
-	@XmlAttribute(required = true)
-	private OpType type;
-	
-	@XmlAttribute(required = true)
-	private ZEvesKind kind;
-	
-//	// name interferes with form here, use #getName()
-//	@XmlElement(required = true)
-//	private ZEvesName name;
-	
-	@XmlElement(name = "type")
-	private ZEvesType elType;
+  /**
+   * <!ATTLIST op type (preop | inop | postop) #REQUIRED>
+   * 
+   * @author Andrius Velykis
+   */
+  public enum OpType {
+    @XmlEnumValue("preop")
+    PREOP, 
+    @XmlEnumValue("inop")
+    INOP, 
+    @XmlEnumValue("postop")
+    POSTOP
+  }
 
-	@XmlAnyElement(lax = true)
-	private List<?> form = new ArrayList<Object>();
+  @XmlAttribute(required = true)
+  private OpType type;
 
-	public ZEvesName getName() {
-		// always take the first in form
-		return (ZEvesName) form.get(0);
-	}
+  @XmlAttribute(required = true)
+  private ZEvesKind kind;
 
-	public List<?> getForm() {
-		// since form interferes with name, it gets caught into the first
-		// element here
-		return form.subList(1, form.size());
-	}
+  // name interferes with form here, use #getName()
+//  @XmlElement(required = true)
+//  private ZEvesName name;
 
-	@Override
-	public String toString() {
+  @XmlElement(name = "type")
+  private ZEvesType elType;
 
-		String opName = String.valueOf(getName());
+  @XmlAnyElement(lax = true)
+  private List<?> form = new ArrayList<Object>();
 
-		List<?> form = getForm();
-		if (form.size() < 1) {
-			throw new IllegalStateException("No ZEves Op items: " + form);
-		}
+  public ZEvesName getName()
+  {
+    // always take the first in form
+    return (ZEvesName) form.get(0);
+  }
 
-		String first = String.valueOf(form.get(0));
+  public List<?> getForm()
+  {
+    // since form interferes with name, it gets caught into the first
+    // element here
+    return form.subList(1, form.size());
+  }
 
-		switch (type) {
-		case PREOP:
-			return opName + " " + first;
-		case POSTOP:
-			return first + " " + opName;
-		case INOP: {
-			if (form.size() != 2) {
-				throw new IllegalStateException("Invalid ZEves Op items: " + form);
-			}
+  @Override
+  public String toString()
+  {
 
-			return first + " " + opName + " " + String.valueOf(form.get(1));
-		}
-		}
+    String opName = String.valueOf(getName());
 
-		return "_" + opName + "_";
-	}
-	
+    List<?> form = getForm();
+    if (form.size() < 1) {
+      throw new IllegalStateException("No ZEves Op items: " + form);
+    }
+
+    String first = String.valueOf(form.get(0));
+
+    switch (type) {
+      case PREOP :
+        return opName + " " + first;
+      case POSTOP :
+        return first + " " + opName;
+      case INOP : {
+        if (form.size() != 2) {
+          throw new IllegalStateException("Invalid ZEves Op items: " + form);
+        }
+
+        return first + " " + opName + " " + String.valueOf(form.get(1));
+      }
+    }
+
+    return "_" + opName + "_";
+  }
+
 }
