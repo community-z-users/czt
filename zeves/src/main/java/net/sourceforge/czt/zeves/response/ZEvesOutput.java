e2ff16fce90de05d8b5d49fa96c6e42bfed78cf7
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-10-22 19:43:06 +0000

    ADD: Implemented analysis of Z/Eves proof output trace - the trace is parsed in ZEvesProofTrace class and can be queried, e.g. for lemmas used, etc.
    ADD: Added support to retrieve Z/Eves proof case number, as a list of integers, each representing a case number of an increasing depth.

--- zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesOutput.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesOutput.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import net.sourceforge.czt.zeves.response.form.ZEvesBlurb;
+import net.sourceforge.czt.zeves.response.form.ZEvesNumber;
 
 /**
  * <!ELEMENT zoutput (%para; | zsectionbegin | zsectionend | zsectionproofbegin
@@ -18,56 +19,108 @@ import net.sourceforge.czt.zeves.response.form.ZEvesBlurb;
  * @author Andrius Velykis
  */
 @XmlRootElement(name = "zoutput")
-public class ZEvesOutput {
+public class ZEvesOutput
+{
 
-	@XmlElement(name = "provercmd")
-	private ZEvesProverCmd command;
+  @XmlElement(name = "provercmd")
+  private ZEvesProverCmd command;
 
-	@XmlElements({
-			// TODO proper classes - not used in Z/Eves at the moment?
-			@XmlElement(name = "zsectionbegin"), @XmlElement(name = "zsectionend"),
-			@XmlElement(name = "zsectionproofbegin"), @XmlElement(name = "zsectionproofend") })
-	private Object sectionTag;
+  @XmlElements({
+      // TODO proper classes - not used in Z/Eves at the moment?
+      @XmlElement(name = "zsectionbegin"), @XmlElement(name = "zsectionend"),
+      @XmlElement(name = "zsectionproofbegin"), @XmlElement(name = "zsectionproofend")})
+  private Object sectionTag;
 
-	@XmlElement(name = "blurb")
-	private ZEvesBlurb info;
+  @XmlElement(name = "blurb")
+  private ZEvesBlurb info;
 
-	@XmlAnyElement(lax = true)
-	private List<?> para = new ArrayList<Object>();
+  @XmlAnyElement(lax = true)
+  private List<?> para = new ArrayList<Object>();
+  
+  private ZEvesProofTrace trace;
 
-	public List<?> getResults() {
-		return Collections.unmodifiableList(para);
-	}
+  public List<?> getResults()
+  {
+    return Collections.unmodifiableList(para);
+  }
 
-	public Object getFirstResult() {
-		if (para.isEmpty()) {
-			return null;
-		}
+  public Object getFirstResult()
+  {
+    if (para.isEmpty()) {
+      return null;
+    }
 
-		return para.get(0);
-	}
-	
-	public ZEvesProverCmd getCommand() {
-		return command;
-	}
-	
-	public ZEvesBlurb getInfo() {
-		return info;
-	}
+    return para.get(0);
+  }
 
-	public boolean isEmpty() {
-		return command == null && sectionTag == null && info == null && para.isEmpty();
-	}
+  public ZEvesProverCmd getCommand()
+  {
+    return command;
+  }
 
-	@Override
-	public String toString() {
+  public ZEvesBlurb getInfo()
+  {
+    return info;
+  }
+  
+  public ZEvesProofTrace getProofTrace() {
+    if (trace == null) {
+      trace = new ZEvesProofTrace(getInfo());
+    }
+    
+    return trace;
+  }
+  
+  /**
+   * Retrieves the proof case, if this information is available. Proof
+   * cases are available for ZEvesOutputs from get-goal-proof-step
+   * commands.
+   * 
+   * The proof case is represented as a list of case numbers, where
+   * a subsequent case number represents a deeper proof case. 
+   * 
+   * @return
+   */
+  public List<Integer> getProofCase() {
+    // proof case is a list of numbers, that go after
+    // the proof result (first element in the para)
+    // thus take all numbers starting from the second element
+    List<Integer> proofCaseNumbers = new ArrayList<Integer>();
+    for (int index = 1; index < para.size(); index++) {
+      
+      Object zEvesElem = para.get(index);
+      if (!(zEvesElem instanceof ZEvesNumber)) {
+        continue;
+      }
+      
+      String valStr = ((ZEvesNumber) zEvesElem).getValue();
 
-		String cmdStr = command != null ? "Command sent: " + String.valueOf(command) + "\n" : "";
-		String infoStr = info != null ? String.valueOf(info) + "\n" : "";
-		String sectionStr = sectionTag != null ? String.valueOf(sectionTag) + "\n" : "";
+      try {
+        proofCaseNumbers.add(Integer.valueOf(valStr));
+      }
+      catch (NumberFormatException e) {
+        // ignore?
+      }
+    }
+    
+    return proofCaseNumbers;
+  }
 
-		return cmdStr + infoStr + sectionStr + ZEvesResponseUtil.concat(para, "\n");
+  public boolean isEmpty()
+  {
+    return command == null && sectionTag == null && info == null && para.isEmpty();
+  }
 
-	}
+  @Override
+  public String toString()
+  {
+
+    String cmdStr = command != null ? "Command sent: " + String.valueOf(command) + "\n" : "";
+    String infoStr = info != null ? String.valueOf(info) + "\n" : "";
+    String sectionStr = sectionTag != null ? String.valueOf(sectionTag) + "\n" : "";
+
+    return cmdStr + infoStr + sectionStr + ZEvesResponseUtil.concat(para, "\n");
+
+  }
 
 }
