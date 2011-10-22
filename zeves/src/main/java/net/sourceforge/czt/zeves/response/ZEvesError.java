5f8934c629ac197fada3089cbb13b2b8a801d24a
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2011-10-22 19:48:25 +0000

    ADD: Implemented parsing of some ZEvesError message types, such as NoEffect, parsing, scanning and typechecking errors.

--- zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesError.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesError.java
@@ -1,20 +1,71 @@
 package net.sourceforge.czt.zeves.response;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "zerror")
-public class ZEvesError {
+public class ZEvesError
+{
+  
+  public enum ZEvesErrorType {
+    NO_EFFECT,
+    PARSE_ERR,
+    SCAN_ERR,
+    TYPECHECK_ERR,
+    UNKNOWN
+  }
 
-	@XmlElement(name = "errormessage")
-	private List<ZEvesErrorMessage> errors = new ArrayList<ZEvesErrorMessage>();
+  private static final String COMMAND_NO_EFFECT_MSG = "Command had no effect.";
+  private static final String PARSE_ERR_STR = "[Parser";
+  private static final String SCAN_ERR_STR = "[Scanner]";
+  private static final String TYPECHECK_ERR_STR = "[Type checker]";
 
-	@Override
-	public String toString() {
-		return ZEvesResponseUtil.concat(errors, "\n");
-	}
+  @XmlElement(name = "errormessage")
+  private List<ZEvesErrorMessage> errors = new ArrayList<ZEvesErrorMessage>();
+
+  @Override
+  public String toString()
+  {
+    return ZEvesResponseUtil.concat(errors, "\n");
+  }
+  
+  public EnumSet<ZEvesErrorType> getType() {
+    EnumSet<ZEvesErrorType> errTypes = EnumSet.noneOf(ZEvesErrorType.class);
+    
+    for (ZEvesErrorMessage err : errors) {
+      String message = err.getMessage();
+      errTypes.add(getErrorType(message));
+    }
+    
+    return errTypes;
+  }
+  
+  private ZEvesErrorType getErrorType(String message) {
+    if (message == null) {
+      return ZEvesErrorType.UNKNOWN;
+    }
+    
+    if (COMMAND_NO_EFFECT_MSG.equals(message)) {
+      return ZEvesErrorType.NO_EFFECT;
+    }
+    
+    if (message.contains(PARSE_ERR_STR)) {
+      return ZEvesErrorType.PARSE_ERR;
+    }
+    
+    if (message.contains(SCAN_ERR_STR)) {
+      return ZEvesErrorType.SCAN_ERR;
+    }
+    
+    if (message.contains(TYPECHECK_ERR_STR)) {
+      return ZEvesErrorType.TYPECHECK_ERR;
+    }
+    
+    return ZEvesErrorType.UNKNOWN;
+  }
 
 }
--- zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesErrorMessage.java
+++ zeves/src/main/java/net/sourceforge/czt/zeves/response/ZEvesErrorMessage.java
@@ -6,15 +6,21 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlMixed;
 
-public class ZEvesErrorMessage {
+public class ZEvesErrorMessage
+{
 
-	@XmlMixed
-	@XmlAnyElement(lax = true)
-	private List<?> content = new ArrayList<Object>();
+  @XmlMixed
+  @XmlAnyElement(lax = true)
+  private List<?> content = new ArrayList<Object>();
 
-	@Override
-	public String toString() {
-		return "ERROR: " + ZEvesResponseUtil.concat(content, " ");
-	}
+  @Override
+  public String toString()
+  {
+    return "ERROR: " + getMessage();
+  }
+  
+  public String getMessage() {
+    return ZEvesResponseUtil.concat(content, " ");
+  }
 
 }
