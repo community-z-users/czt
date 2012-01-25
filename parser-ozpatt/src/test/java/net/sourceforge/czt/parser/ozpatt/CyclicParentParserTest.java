c1ecbb0250ec43e408cd1a4d8b5d0f8856b47795
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-01-25 09:48:38 +0000

    ADD: Added cyclic parent parsing tests (which extend the Z cyclic parent parsing test) to all different parsers.

---
+++ parser-ozpatt/src/test/java/net/sourceforge/czt/parser/ozpatt/CyclicParentParserTest.java
@@ -0,0 +1,19 @@
+package net.sourceforge.czt.parser.ozpatt;
+
+import net.sourceforge.czt.session.SectionManager;
+import net.sourceforge.czt.session.UrlSource;
+
+public class CyclicParentParserTest extends net.sourceforge.czt.parser.z.CyclicParentParserTest
+{
+
+  public CyclicParentParserTest(UrlSource source)
+  {
+    super(source);
+  }
+
+  @Override
+  protected SectionManager createSectionManager()
+  {
+    return new SectionManager("ozpatt");
+  }
+}
