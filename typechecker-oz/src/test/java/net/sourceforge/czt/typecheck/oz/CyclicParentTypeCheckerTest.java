8a63929dc8f21d1bea193338f5430577f36cda7f
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-01-25 10:00:24 +0000

    ADD: Added typechecker tests for cyclic parent relationships to all typechecker modules.

---
+++ typechecker-oz/src/test/java/net/sourceforge/czt/typecheck/oz/CyclicParentTypeCheckerTest.java
@@ -0,0 +1,19 @@
+package net.sourceforge.czt.typecheck.oz;
+
+import net.sourceforge.czt.session.SectionManager;
+import net.sourceforge.czt.session.UrlSource;
+
+public class CyclicParentTypeCheckerTest extends net.sourceforge.czt.typecheck.z.CyclicParentTypeCheckerTest
+{
+
+  public CyclicParentTypeCheckerTest(UrlSource source)
+  {
+    super(source);
+  }
+
+  @Override
+  protected SectionManager createSectionManager()
+  {
+    return new SectionManager("oz");
+  }
+}
