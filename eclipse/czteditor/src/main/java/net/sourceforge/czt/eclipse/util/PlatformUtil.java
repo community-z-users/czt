29d31415dcdaf6c2e77e9d6f27269849dd92e213
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-01-27 18:57:44 +0000

    ADD: Added a utility method to get all open editors

--- eclipse/czteditor/src/main/java/net/sourceforge/czt/eclipse/util/PlatformUtil.java
+++ eclipse/czteditor/src/main/java/net/sourceforge/czt/eclipse/util/PlatformUtil.java
@@ -1,8 +1,15 @@
 package net.sourceforge.czt.eclipse.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sourceforge.czt.eclipse.CZTPlugin;
 
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 public class PlatformUtil
@@ -34,6 +41,24 @@ public class PlatformUtil
       CZTPlugin.log(e);
     }
     
+  }
+  
+  /**
+   * Retrieves all open editors in the workbench.
+   * @return
+   */
+  public static List<IEditorPart> getOpenEditors()
+  {
+    List<IEditorPart> editors = new ArrayList<IEditorPart>();
+    for (IWorkbenchWindow window : PlatformUI.getWorkbench().getWorkbenchWindows()) {
+        for (IWorkbenchPage page : window.getPages()) {
+            for (IEditorReference editor : page.getEditorReferences()) {
+                editors.add(editor.getEditor(false));
+            }
+        }
+    }
+    
+    return editors;
   }
 
 }
