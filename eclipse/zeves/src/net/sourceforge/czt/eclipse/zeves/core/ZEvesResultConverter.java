f76373246d3d9495fd2d15281154f82a5e0c4078
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-03-09 16:47:01 +0000

    Nicer message if parent section is not available (e.g. the editor has been updated and is erroneous)

--- eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesResultConverter.java
+++ eclipse/zeves/src/net/sourceforge/czt/eclipse/zeves/core/ZEvesResultConverter.java
@@ -8,6 +8,7 @@ import net.sourceforge.czt.eclipse.editors.zeditor.ZEditorUtil;
 import net.sourceforge.czt.eclipse.zeves.ZEvesPlugin;
 import net.sourceforge.czt.parser.zeves.ParseUtils;
 import net.sourceforge.czt.session.CommandException;
+import net.sourceforge.czt.session.Key;
 import net.sourceforge.czt.session.Markup;
 import net.sourceforge.czt.session.SectionManager;
 import net.sourceforge.czt.session.Source;
@@ -15,6 +16,7 @@ import net.sourceforge.czt.session.StringSource;
 import net.sourceforge.czt.z.ast.Expr;
 import net.sourceforge.czt.z.ast.Para;
 import net.sourceforge.czt.z.ast.Pred;
+import net.sourceforge.czt.z.ast.ZSect;
 
 
 public class ZEvesResultConverter {
@@ -22,6 +24,7 @@ public class ZEvesResultConverter {
 	public static Pred parseZEvesPred(SectionManager sectInfo, String sectName, String zEvesPredStr)
 			throws IOException, CommandException {
 
+		assertSectionAvailable(sectInfo, sectName);
 		Source source = createParseSource(zEvesPredStr);
 		return ParseUtils.parsePred(source, sectName, sectInfo);
 	}
@@ -29,6 +32,7 @@ public class ZEvesResultConverter {
 	public static Expr parseZEvesExpr(SectionManager sectInfo, String sectName, String zEvesExprStr)
 			throws IOException, CommandException {
 		
+		assertSectionAvailable(sectInfo, sectName);
 		Source source = createParseSource(zEvesExprStr);
 		return ParseUtils.parseExpr(source, sectName, sectInfo);
 	}
@@ -36,6 +40,7 @@ public class ZEvesResultConverter {
 	public static List<Para> parseZEvesParas(SectionManager sectInfo, String sectName, String zEvesExprStr)
 			throws IOException, CommandException {
 		
+		assertSectionAvailable(sectInfo, sectName);
 		Source source = createParseSource(zEvesExprStr);
 		return ParseUtils.parseParas(source, sectName, sectInfo);
 	}
@@ -61,6 +66,24 @@ public class ZEvesResultConverter {
 		}
 	}
 	
+	/**
+	 * Asserts that the converted section is actually available in the section
+	 * manager. Otherwise, the parsing will fail anyway - so we produce a
+	 * nicer/simpler exception to indicate that the section is not available.
+	 * 
+	 * @param sectInfo
+	 * @param sectName
+	 * @throws CommandException
+	 */
+	private static void assertSectionAvailable(SectionManager sectInfo, String sectName)
+			throws CommandException {
+		
+		Key<ZSect> sectKey = new Key<ZSect>(sectName, ZSect.class);
+		if (!sectInfo.isCached(sectKey)) {
+			throw new CommandException("Section " + sectName + " is not parsed.");
+		}
+	}
+	
 	private static Source createParseSource(String zEvesResultStr) {
 		Source source = new StringSource(zEvesResultStr, "zevesResult");
 		source.setMarkup(Markup.UNICODE);
@@ -72,6 +95,7 @@ public class ZEvesResultConverter {
 			Markup markup, int textWidth, boolean display) {
 		
 		try {
+			assertSectionAvailable(sectInfo, sectName);
 			return ZEditorUtil.print(term, sectInfo, sectName, markup, textWidth, display);
 		} catch (CommandException e) {
 			// problems printing
