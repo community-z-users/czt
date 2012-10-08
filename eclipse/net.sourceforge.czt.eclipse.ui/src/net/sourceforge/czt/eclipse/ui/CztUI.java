6618d1e068f1070ee588d4564a32de8e946a0024
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-10-08 19:57:35 +0100

    reformatted

--- eclipse/net.sourceforge.czt.eclipse.ui/src/net/sourceforge/czt/eclipse/ui/CztUI.java
+++ eclipse/net.sourceforge.czt.eclipse.ui/src/net/sourceforge/czt/eclipse/ui/CztUI.java
@@ -1,46 +1,38 @@
-/**
- * 
- */
 package net.sourceforge.czt.eclipse.ui;
 
 /**
- * Central access point for the CZT plug-in.
- * <p>
- * This class provides static methods and fields only; it is not intended to be
- * instantiated or subclassed by clients.
- * </p>
+ * CZT UI component references.
+ * 
+ * @author Andrius Velykis
  * @author Chengdong Xu
- *
  */
 public final class CztUI
 {
-    
-    private CztUI() {
-        // prevent instantiation of CztUI.
-    }
-        
-    /**
-     * The id of the Character Map view
-     * (value {@code net.sourceforge.czt.eclipse.ui.views.ZCharMap}).
-     */
-    public static final String ID_CHARMAP = "net.sourceforge.czt.eclipse.ui.views.ZCharMap";
-    
-    /**
-     * The id of the Z conversion page view
-     * (value {@code net.sourceforge.czt.eclipse.ui.views.ZConversion}).
-     */
-    public static final String ID_CONVERSIONVIEW = "net.sourceforge.czt.eclipse.ui.views.ZConversion";
-    
-    /**
-     * The id of the new CZT project wizard
-     * (value {@code net.sourceforge.czt.eclipse.ui.wizards.CztProject}).
-     */
-    public static final String ID_NEW_CZT_PROJECT_WIZARD = "net.sourceforge.czt.eclipse.ui.wizards.CztProject";
-    
-    /**
-     * The id of the new Z specification wizard
-     * (value {@code net.sourceforge.czt.eclipse.ui.wizards.ZSpecification}).
-     */
-    public static final String ID_NEW_CZT_SPECIFICATION_WIZARD = "net.sourceforge.czt.eclipse.ui.wizards.ZSpecification";
-    
+
+  private CztUI() {}
+
+  /**
+   * The id of the Character Map view
+   * (value {@code net.sourceforge.czt.eclipse.ui.views.ZCharMap}).
+   */
+  public static final String ID_CHARMAP = "net.sourceforge.czt.eclipse.ui.views.ZCharMap";
+
+  /**
+   * The id of the Z conversion page view
+   * (value {@code net.sourceforge.czt.eclipse.ui.views.ZConversion}).
+   */
+  public static final String ID_CONVERSIONVIEW = "net.sourceforge.czt.eclipse.ui.views.ZConversion";
+
+  /**
+   * The id of the new CZT project wizard
+   * (value {@code net.sourceforge.czt.eclipse.ui.wizards.CztProject}).
+   */
+  public static final String ID_NEW_CZT_PROJECT_WIZARD = "net.sourceforge.czt.eclipse.ui.wizards.CztProject";
+
+  /**
+   * The id of the new Z specification wizard
+   * (value {@code net.sourceforge.czt.eclipse.ui.wizards.ZSpecification}).
+   */
+  public static final String ID_NEW_CZT_SPECIFICATION_WIZARD = "net.sourceforge.czt.eclipse.ui.wizards.ZSpecification";
+
 }
