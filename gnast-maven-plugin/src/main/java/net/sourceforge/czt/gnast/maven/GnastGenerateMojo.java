9918946036cfc28dcf8078ed550830adf602fd4a
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-10-11 14:23:22 +0100

    GnAST maven plugin updated to provide correct XML schema URLs to GnAST execution;
    Added a parameter to explicitly indicate Schema locations in Maven.
    Using plexus-resources library to locate resources: either as classpath URL, URL or file path.
    This allows to indicate Schema locations in other JARs and they should be found.

--- gnast-maven-plugin/src/main/java/net/sourceforge/czt/gnast/maven/GnastGenerateMojo.java
+++ gnast-maven-plugin/src/main/java/net/sourceforge/czt/gnast/maven/GnastGenerateMojo.java
@@ -1,8 +1,13 @@
 package net.sourceforge.czt.gnast.maven;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 
 import net.sourceforge.czt.gnast.Gnast;
@@ -11,6 +16,10 @@ import net.sourceforge.czt.gnast.Gnast.GnastBuilder;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.resource.DefaultResourceManager;
+import org.codehaus.plexus.resource.PlexusResource;
+import org.codehaus.plexus.resource.ResourceManager;
+import org.codehaus.plexus.resource.loader.ResourceNotFoundException;
 import org.sonatype.plexus.build.incremental.BuildContext;
 
 /**
@@ -49,13 +58,30 @@ public class GnastGenerateMojo
   private boolean verbose;
   
   /**
-   * The directory where all ZML schema files are located.
-   * TODO better resolution? E.g. something similar to resources plugin? 
+   * The directory where all XML schema source files are located.
+   * <p>
+   * At least one of {@link #sourceDirectory} or {@link #sourceSchemas} must
+   * be set to find the schemas.
+   * </p>
    * 
    * @parameter
-   * @required
    */
   private File sourceDirectory;
+  
+  /**
+   * An explicit list of XML schema source files to use in generation.
+   * <p>
+   * Potential values are a filesystem path, a URL, or a classpath resource.
+   * This parameter is resolved as resource, URL, then file.
+   * </p>
+   * <p>
+   * At least one of {@link #sourceDirectory} or {@link #sourceSchemas} must
+   * be set to find the schemas.
+   * </p>
+   * 
+   * @parameter alias="schemaLocation"
+   */
+  private List<String> sourceSchemas = new ArrayList<String>();
   
   /**
    * @parameter
@@ -74,6 +100,13 @@ public class GnastGenerateMojo
    * @component
    */
   private BuildContext buildContext;
+  
+  /**
+   * Injected by Maven
+   * @component
+   */
+  private ResourceManager locator;
+  
 
   @Override
   public void execute() throws MojoExecutionException
@@ -83,10 +116,20 @@ public class GnastGenerateMojo
     if (project != null) {
       project.addCompileSourceRoot(outputDirectory.getPath());
     }
+    
+    Set<URL> allSchemas = new HashSet<URL>();
+    allSchemas.addAll(Gnast.schemaDirToURL(sourceDirectory));
+    allSchemas.addAll(locateResources(sourceSchemas));
+    
+    if (allSchemas.isEmpty()) {
+      throw new MojoExecutionException("No XML schema source files found. "
+          + "They must be indicated either as sourceDirectory (" + sourceDirectory + ") "
+          + "or as sourceSchemas (" + sourceSchemas + ")");
+    }
 
     GnastBuilder config = new GnastBuilder()
         .templates(templates)
-        .source(sourceDirectory)
+        .sourceSchemas(allSchemas)
         .namespace(targetNamespace);
 
     if (outputDirectory != null) {
@@ -121,4 +164,33 @@ public class GnastGenerateMojo
     }
 
   }
+  
+  private Set<URL> locateResources(Collection<String> locations) throws MojoExecutionException {
+    Set<URL> urls = new HashSet<URL>();
+    for (String location : locations) {
+      urls.add(locateResource(location));
+    }
+    
+    return urls;
+  }
+  
+  private URL locateResource(String resourceLocation) throws MojoExecutionException {
+    
+    if (locator == null) {
+      locator = new DefaultResourceManager();
+    }
+    
+    try {
+      PlexusResource resource = locator.getResource(resourceLocation);
+      return resource.getURL();
+    }
+    catch (ResourceNotFoundException e) {
+      throw new MojoExecutionException("Cannot find resource " + resourceLocation, e);
+    }
+    catch (IOException e) {
+      throw new MojoExecutionException("Cannot find resource URL " + resourceLocation, e);
+    }
+    
+  }
+  
 }
