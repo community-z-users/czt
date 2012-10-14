5213ce01a290a4c308b2655ec8d59cefec8dd37b
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-10-14 18:41:47 +0100

    completely rewrote the CUP Mojo to support incremental build and all CUP options

--- cup-maven-plugin/src/main/java/net/sourceforge/czt/cup/maven/CupGenerateMojo.java
+++ cup-maven-plugin/src/main/java/net/sourceforge/czt/cup/maven/CupGenerateMojo.java
@@ -1,28 +1,34 @@
 /*
-  Copyright 2006, 2012  Petra Malik, Andrius Velykis
-  
-  This file is part of the CZT project.
+  The MIT License
 
-  The CZT project is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  Copyright 2012  Andrius Velykis
 
-  The CZT project is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
 
-  You should have received a copy of the GNU General Public License
-  along with CZT.  If not, see <http://www.gnu.org/licenses/>.
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
 */
 
 package net.sourceforge.czt.cup.maven;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import java_cup.Main;
@@ -30,17 +36,24 @@ import java_cup.Main;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.Scanner;
+import org.sonatype.plexus.build.incremental.BuildContext;
+import org.sonatype.plexus.build.incremental.DefaultBuildContext;
 
 /**
  * @goal generate
  * @phase generate-sources
  * @description Java CUP Maven Plugin
+ * 
+ * @author Andrius Velykis
  */
-public class CupGenerateMojo
-  extends AbstractMojo
+public class CupGenerateMojo extends AbstractMojo
 {
+  
+  private static final String CUP_EXTENSION = ".cup";
+  
   /**
-   * @parameter expression="${basedir}/src/main/cup"
+   * @parameter expression="${project.basedir}/src/main/cup"
    * @required
    */
   private File sourceDirectory;
@@ -49,111 +62,404 @@ public class CupGenerateMojo
    * @parameter expression="${project.build.directory}/generated-sources/cup"
    * @required
    */
-  private String outputDirectory;
-
-  /**
-   * @parameter 
-   */
-  private boolean dumpGrammar;
+  private File outputDirectory;
   
   /**
-   * @parameter 
+   * Package name.
+   * <p>
+   * Specify that the parser and sym classes are to be placed in the named package. By default, no
+   * package specification is put in the generated code (hence the classes default to the special
+   * "unnamed" package).
+   * </p>
+   * 
+   * @parameter
    */
-  private boolean dumpStates;
-
-  /**
-   * @parameter 
-   */
-  private boolean dumpTables;
+  private String packageName;
   
   /**
+   * Generated parser class name.
+   * <p>
+   * Output parser and action code into a file (and class) with the given name. If not given, CUP
+   * file name will be used as the class name.
+   * </p>
+   * 
+   * @parameter
+   */
+  private String className;
+  
+  /**
+   * Specify type arguments for parser class.
+   * 
+   * @parameter
+   */
+  private String typeArgs;
+  
+  /**
+   * Generated symbols class name.
+   * <p>
+   * Output the symbol constant code into a class with the given name instead of the default of
+   * "Sym".
+   * </p>
+   * 
+   * @parameter default-value="Sym"
+   */
+  private String symbolsName;
+
+  /**
+   * Outputs the symbol constant code as an {@code interface} rather than as a {@code class}.
+   * 
+   * @parameter
+   */
+  private boolean symbolsInterface;
+  
+  /**
+   * Place constants for non-terminals into the symbol constant class. The parser does not need
+   * these symbol constants, so they are not normally output. However, it can be very helpful to
+   * refer to these constants when debugging a generated parser.
+   * 
+   * @parameter
+   */
+  private boolean nontermsToSymbols;
+  
+  /**
+   * During parser construction the system may detect that an ambiguous situation would occur at
+   * runtime. This is called a conflict. In general, the parser may be unable to decide whether to
+   * shift (read another symbol) or reduce (replace the recognized right hand side of a production
+   * with its left hand side). This is called a shift/reduce conflict. Similarly, the parser may not
+   * be able to decide between reduction with two different productions. This is called a
+   * reduce/reduce conflict. Normally, if one or more of these conflicts occur, parser generation is
+   * aborted. However, in certain carefully considered cases it may be advantageous to arbitrarily
+   * break such a conflict. In this case CUP uses YACC convention and resolves shift/reduce
+   * conflicts by shifting, and reduce/reduce conflicts using the "highest priority" production (the
+   * one declared first in the specification). In order to enable automatic breaking of conflicts
+   * the -expect option must be given indicating exactly how many conflicts are expected. Conflicts
+   * resolved by precedences and associativities are not reported.
+   * 
+   * @parameter
+   */
+  private int expectedConflicts;
+  
+  /**
+   * Including this option enables a table compaction optimization involving reductions. In
+   * particular, it allows the most common reduce entry in each row of the parse action table to be
+   * used as the default for that row. This typically saves considerable room in the tables, which
+   * can grow to be very large. This optimization has the effect of replacing all error entries in a
+   * row with the default reduce entry. While this may sound dangerous, if not down right incorrect,
+   * it turns out that this does not affect the correctness of the parser. In particular, some
+   * changes of this type are inherent in LALR parsers (when compared to canonical LR parsers), and
+   * the resulting parsers will still never read past the first token at which the error could be
+   * detected. The parser can, however, make extra erroneous reduces before detecting the error, so
+   * this can degrade the parser's ability to do error recovery. (Refer to reference [2] pp. 244-247
+   * or reference [3] pp. 190-194 for a complete explanation of this compaction technique.)
+   * <p>
+   * This option is typically used to work-around the java bytecode limitations on table
+   * initialization code sizes. However, CUP 0.10h introduced a string-encoding for the parser
+   * tables which is not subject to the standard method-size limitations. Consequently, use of this
+   * option should no longer be required for large grammars.
+   * </p>
+   * 
    * @parameter
    */
   private boolean compactRed;
 
   /**
+   * Suppress all warning messages (as opposed to error messages) produced by the system.
+   * 
+   * @parameter 
+   */
+  private boolean noWarn;
+  
+  /**
+   * Suppress printing a summary listing such things as the number of terminals, non-terminals,
+   * parse states, etc. at the end of the generation.
+   * 
+   * @parameter
+   */
+  private boolean noSummary;
+
+  /**
+   * Print short messages indicating progress through various parts of the parser generation
+   * process.
+   * 
+   * @parameter
+   */
+  private boolean showProgress;
+  
+  /**
+   * Output a human readable dump of the grammar. 
+   * 
+   * @parameter 
+   */
+  private boolean dumpGrammar;
+  
+  /**
+   * Output a human readable dump of the constructed parse states (often needed to resolve parse
+   * conflicts).
+   * 
+   * @parameter
+   */
+  private boolean dumpStates;
+
+  /**
+   * Output a human readable dump of the parse tables (rarely needed).
+   * 
+   * @parameter 
+   */
+  private boolean dumpTables;
+  
+  /**
+   * Add detailed timing statistics to the normal summary of results.
+   * 
+   * @parameter
+   */
+  private boolean timeSummary;
+  
+  /**
+   * Output internal debugging information about the system as it runs.
+   * 
+   * @parameter
+   */
+  private boolean debug;
+  
+  /**
+   * Do not generate code to propagate the left and right hand values of terminals to non-terminals,
+   * and then from non-terminals to other terminals. If the left and right values aren't going to be
+   * used by the parser, then it will save some runtime computation to not generate these position
+   * propagations. This option also keeps the left and right label variables from being generated,
+   * so any reference to these will cause an error.
+   * 
+   * @parameter
+   */
+  private boolean noPositions;
+  
+  /**
+   * Suppress {@link java_cup.runtime.Scanner} references.
+   * <p>
+   * CUP 0.10j introduced improved scanner integration and a new interface,
+   * {@link java_cup.runtime.Scanner}. By default, the generated parser refers to this interface,
+   * which means you cannot use these parsers with CUP runtimes older than 0.10j. If your parser
+   * does not use the new scanner integration features, then you may suppress the
+   * {@link java_cup.runtime.Scanner} references and allow compatibility with old runtimes. Not many
+   * people should have reason to do this.
+   * </p>
+   * 
+   * @parameter
+   */
+  private boolean noScanner;
+  
+  /**
    * @parameter expression="${project}"
    * @required
    */
   private MavenProject project;
+  
+  /** 
+   * Injected by Maven
+   * @component
+   */
+  private BuildContext buildContext;
+
 
   public void execute()
     throws MojoExecutionException
   {
-    final String fileSep = System.getProperty("file.separator");
     if (project != null)
     {
-      project.addCompileSourceRoot(outputDirectory);
+      project.addCompileSourceRoot(outputDirectory.getPath());
     }
-    List<File> grammars = getGrammarFiles();
-    getLog().info("CUP: Processing " + grammars.size() + " cup files");
-    for (File file : grammars) {
-      getLog().info("CUP: Processing " + file.getPath());
-      String className = file.getName().replaceAll(".cup", "");
+    
+    List<File> cupSourceFiles = getCupFiles();
+    getLog().info("CUP: Processing " + cupSourceFiles.size() + " cup files");
+    
+    for (File file : cupSourceFiles) {
+      generateCup(file);
+    }
+  }
+  
+  /**
+   * Collects CUP files in the source directory (changed files only for incremental builds).
+   * 
+   * @return
+   */
+  private List<File> getCupFiles() {
+    
+    if (buildContext == null) {
+      // non-incremental build context by default
+      buildContext = new DefaultBuildContext();
+    }
+    
+    // find source files (only changed ones for incremental)
+    Scanner fileScanner = buildContext.newScanner(sourceDirectory);
+    fileScanner.setIncludes(new String[] { "**/*" + CUP_EXTENSION });
+    fileScanner.scan();
+    
+    List<File> cupFiles = new ArrayList<File>();
+    for (String path : fileScanner.getIncludedFiles()) {
+      // convert paths to absolute
+      cupFiles.add(new File(sourceDirectory, path));
+    }
+    
+    return cupFiles;
+  }
+  
+  private void generateCup(File file) throws MojoExecutionException
+  {
+    getLog().debug("CUP: Processing " + file.getPath());
+
+    String packageName = this.packageName;
+    if (packageName == null) {
       try {
-        String packageName = getPackage(file);
-        String destdir = outputDirectory + fileSep +
-          packageName.replace(".", fileSep);
-        File destDir = new File(destdir);
-        File destFile = new File(destDir, className + ".java");
-        getLog().debug("CUP: Checking file dates:\n\t" + new Date(destFile.lastModified()) + 
-          "= " + destFile + "\n\t" + new Date(file.lastModified()) + "= " +
-          file);
-        if (destFile.exists() &&
-            destFile.lastModified() >= file.lastModified()) {
-          getLog().debug("CUP: Parser file is up-to-date.");
+        packageName = getPackageName(file);
+      }
+      catch (IOException e) {
+        // do not stop execution if package name resolution has problems
+        getLog().debug(e);
+      }
+    }
+
+    String className = this.className;
+    if (className == null) {
+      String fileName = file.getName();
+      if (fileName.endsWith(CUP_EXTENSION)) {
+        className = fileName.substring(0, fileName.length() - CUP_EXTENSION.length());
+      }
+      else {
+        className = fileName;
+      }
+    }
+
+    File destDir;
+    if (packageName != null) {
+      destDir = new File(outputDirectory, packageName.replace(".", "/"));
+    }
+    else {
+      destDir = outputDirectory;
+    }
+
+    // create parent dirs
+    if (!destDir.exists()) {
+      destDir.mkdirs();
+    }
+
+    File destFile = new File(destDir, className + ".java");
+    File symbolsFile = new File(destDir, symbolsName + ".java");
+
+    List<String> args = new ArrayList<String>();
+
+    if (packageName != null) {
+      args.add("-package");
+      args.add(packageName);
+    }
+    if (className != null) {
+      args.add("-parser");
+      args.add(className);
+    }
+    if (typeArgs != null) {
+      args.add("-typearg");
+      args.add(typeArgs);
+    }
+    if (symbolsName != null) {
+      args.add("-symbols");
+      args.add(symbolsName);
+    }
+    if (symbolsInterface) {
+      args.add("-interface");
+    }
+    if (nontermsToSymbols) {
+      args.add("-nonterms");
+    }
+    if (expectedConflicts > 0) {
+      args.add("-expect");
+      args.add(String.valueOf(expectedConflicts));
+    }
+    if (compactRed) {
+      args.add("-compact_red");
+    }
+    if (noWarn) {
+      args.add("-nowarn");
+    }
+    if (noSummary) {
+      args.add("-nosummary");
+    }
+    if (showProgress) {
+      args.add("-progress");
+    }
+    if (dumpGrammar) {
+      args.add("-dump_grammar");
+    }
+    if (dumpStates) {
+      args.add("-dump_states");
+    }
+    if (dumpTables) {
+      args.add("-dump_tables");
+    }
+    if (timeSummary) {
+      args.add("-time");
+    }
+    if (debug) {
+      args.add("-debug");
+    }
+    if (noPositions) {
+      args.add("-nopositions");
+    }
+    if (noScanner) {
+      args.add("-noscanner");
+    }
+
+    // target directory path
+    args.add("-destdir");
+    args.add(destDir.getAbsolutePath());
+
+    // source file
+    args.add(file.getAbsolutePath());
+
+    try {
+      // run generator
+      Main.main(args.toArray(new String[0]));
+    }
+    catch (Exception e) {
+      throw new MojoExecutionException("CUP generation failed", e);
+    }
+
+    // refresh generated parser and symbol files after generation
+    buildContext.refresh(destFile);
+    buildContext.refresh(symbolsFile);
+
+    getLog().info("CUP: generated " + destFile);
+    getLog().info("CUP: generated " + symbolsFile);
+
+  }
+
+  /**
+   * Retrieves the package name from file.
+   * 
+   * @param cupFile
+   * @return
+   * @throws IOException
+   */
+  private String getPackageName(File cupFile) throws IOException
+  {
+    BufferedReader reader = new BufferedReader(new FileReader(cupFile));
+
+    try {
+
+      String line = "";
+      while ((line = reader.readLine()) != null) {
+        int packageIndex = line.indexOf("package");
+        if (packageIndex >= 0) {
+          int packageNameIndex = packageIndex += 7;
+          int packageNameEnd = line.indexOf(';', packageNameIndex);
+          if (packageNameEnd >= packageNameIndex) {
+            return line.substring(packageNameIndex, packageNameEnd).trim();
+          }
         }
-        else {
-          if (! destDir.exists()){ destDir.mkdirs();}
-          List<String> args = new ArrayList<String>();
-          if (dumpGrammar) {args.add("-dump_grammar");}
-          if (dumpStates) {args.add("-dump_states");}
-          if (dumpTables) {args.add("-dump_tables");}
-          if (compactRed) {args.add("-compact_red");}
-          args.addAll(Arrays.asList("-destdir", destdir,
-                                   "-package", packageName,
-                                   "-parser", className,
-                                   "-symbols", "Sym",
-                                   file.getPath()));
-          Main.main(args.toArray(new String[0]));
-        }
       }
-      catch (Exception e) {
-        throw new MojoExecutionException("Cup generation failed", e);
-      }
-    }
-  }
 
-  private List<File> getGrammarFiles()
-  {
-    List<File> fileList = new ArrayList<File>();
-    if (sourceDirectory != null) {
-      collectGrammarFiles(sourceDirectory, fileList);
     }
-    return fileList;
-  }
+    finally {
+      reader.close();
+    }
 
-  private void collectGrammarFiles(File directory, List<File> list)
-  {
-    File[] content = directory.listFiles();
-    if (content == null) return;
-    for (File file : content) {
-      if (file.isDirectory()) collectGrammarFiles(file, list);
-      else list.add(file);
-    }
-  }
-
-  private String getPackage(File cupfile)
-    throws IOException
-  {
-    BufferedReader br = new BufferedReader(new FileReader(cupfile));
-    while (br.ready()){
-      String line = br.readLine();
-      if (line.startsWith("package") && line.indexOf(";") != -1)
-      {
-        return line.substring(8, line.indexOf(";")).trim();
-      }
-    }
-    return "";
+    return null;
   }
 }
