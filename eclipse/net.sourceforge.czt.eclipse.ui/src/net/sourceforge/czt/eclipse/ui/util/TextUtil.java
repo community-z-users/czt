e3ed5c64bd1abc3c0508c657494a6d2b90c1eedc
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2012-11-20 14:38:26 +0000

    CZT formatting

--- eclipse/net.sourceforge.czt.eclipse.ui/src/net/sourceforge/czt/eclipse/ui/util/TextUtil.java
+++ eclipse/net.sourceforge.czt.eclipse.ui/src/net/sourceforge/czt/eclipse/ui/util/TextUtil.java
@@ -5,14 +5,17 @@ import net.sourceforge.czt.text.Position;
 /**
  * @author Andrius Velykis
  */
-public class TextUtil {
+public class TextUtil
+{
 
-	public static Position cztPos(org.eclipse.jface.text.Position jfacePos) {
-		return new Position(jfacePos.getOffset(), jfacePos.getLength());
-	}
-	
-	public static org.eclipse.jface.text.Position jfacePos(Position cztPos) {
-		return new org.eclipse.jface.text.Position(cztPos.getOffset(), cztPos.getLength());
-	}
-	
+  public static Position cztPos(org.eclipse.jface.text.Position jfacePos)
+  {
+    return new Position(jfacePos.getOffset(), jfacePos.getLength());
+  }
+
+  public static org.eclipse.jface.text.Position jfacePos(Position cztPos)
+  {
+    return new org.eclipse.jface.text.Position(cztPos.getOffset(), cztPos.getLength());
+  }
+
 }
--- util/src/main/java/net/sourceforge/czt/text/Position.java
+++ util/src/main/java/net/sourceforge/czt/text/Position.java
@@ -8,132 +8,143 @@ package net.sourceforge.czt.text;
  * 
  * @author Andrius Velykis
  */
-public class Position {
+public class Position
+{
 
-	/** The offset of the position */
-	public final int offset;
+  /** The offset of the position */
+  public final int offset;
 
-	/** The length of the position */
-	public final int length;
+  /** The length of the position */
+  public final int length;
 
-	/**
-	 * Creates a new position with the given offset and length 0.
-	 * 
-	 * @param offset
-	 *            the position offset, must be >= 0
-	 */
-	public Position(int offset) {
-		this(offset, 0);
-	}
+  /**
+   * Creates a new position with the given offset and length 0.
+   * 
+   * @param offset
+   *          the position offset, must be >= 0
+   */
+  public Position(int offset)
+  {
+    this(offset, 0);
+  }
 
-	/**
-	 * Creates a new position with the given offset and length.
-	 * 
-	 * @param offset
-	 *            the position offset, must be >= 0
-	 * @param length
-	 *            the position length, must be >= 0
-	 */
-	public Position(int offset, int length) {
-		
-		if (offset < 0) {
-			throw new IllegalArgumentException("Negative offset");
-		}
-		
-		if (length < 0) {
-			throw new IllegalArgumentException("Negative length");
-		}
+  /**
+   * Creates a new position with the given offset and length.
+   * 
+   * @param offset
+   *          the position offset, must be >= 0
+   * @param length
+   *          the position length, must be >= 0
+   */
+  public Position(int offset, int length)
+  {
 
-		this.offset = offset;
-		this.length = length;
-	}
+    if (offset < 0) {
+      throw new IllegalArgumentException("Negative offset");
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + length;
-		result = prime * result + offset;
-		return result;
-	}
+    if (length < 0) {
+      throw new IllegalArgumentException("Negative length");
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Position other = (Position) obj;
-		if (length != other.length)
-			return false;
-		if (offset != other.offset)
-			return false;
-		return true;
-	}
+    this.offset = offset;
+    this.length = length;
+  }
 
-	/**
-	 * Returns the length of this position.
-	 * 
-	 * @return the length of this position
-	 */
-	public int getLength() {
-		return length;
-	}
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + length;
+    result = prime * result + offset;
+    return result;
+  }
 
-	/**
-	 * Returns the offset of this position.
-	 * 
-	 * @return the offset of this position
-	 */
-	public int getOffset() {
-		return offset;
-	}
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Position other = (Position) obj;
+    if (length != other.length)
+      return false;
+    if (offset != other.offset)
+      return false;
+    return true;
+  }
 
-	/**
-	 * Calculates the end offset of this position.
-	 * 
-	 * @return the end offset of this position (last point within the range).
-	 */
-	public int getEndOffset() {
-		return offset + length - 1;
-	}
-	
-	/**
-	 * Checks whether the intersection of the given text range and the text range represented by
-	 * this position is empty or not.
-	 * 
-	 * @param rangeOffset
-	 *            the offset of the range to check
-	 * @param rangeLength
-	 *            the length of the range to check
-	 * @return {@code true} if intersection is not empty
-	 */
-	public boolean overlapsWith(int rangeOffset, int rangeLength) {
+  /**
+   * Returns the length of this position.
+   * 
+   * @return the length of this position
+   */
+  public int getLength()
+  {
+    return length;
+  }
 
-		int end = rangeOffset + rangeLength - 1;
-		int thisEnd = getEndOffset();
+  /**
+   * Returns the offset of this position.
+   * 
+   * @return the offset of this position
+   */
+  public int getOffset()
+  {
+    return offset;
+  }
 
-		return (this.offset <= end) && (thisEnd >= rangeOffset);
-	}
+  /**
+   * Calculates the end offset of this position.
+   * 
+   * @return the end offset of this position (last point within the range).
+   */
+  public int getEndOffset()
+  {
+    return offset + length - 1;
+  }
 
-	@Override
-	public String toString() {
-		return "Position(" + offset + (length > 0 ? "+" + length : "") + ")";
-	}
+  /**
+   * Checks whether the intersection of the given text range and the text range represented by
+   * this position is empty or not.
+   * 
+   * @param rangeOffset
+   *          the offset of the range to check
+   * @param rangeLength
+   *          the length of the range to check
+   * @return {@code true} if intersection is not empty
+   */
+  public boolean overlapsWith(int rangeOffset, int rangeLength)
+  {
 
-	/**
-	 * Static factory method to create a position based on its start and end offsets.
-	 * 
-	 * @param start
-	 *            the start offset of the position
-	 * @param end
-	 *            the end offset of the position
-	 * @return new position with the given start and end offsets
-	 */
-	public static Position createStartEnd(int start, int end) {
-		return new Position(start, end - start);
-	}
-	
+    int end = rangeOffset + rangeLength - 1;
+    int thisEnd = getEndOffset();
+
+    return (this.offset <= end) && (thisEnd >= rangeOffset);
+  }
+
+  @Override
+  public String toString()
+  {
+    return "Position(" + offset + (length > 0 ? "+" + length : "") + ")";
+  }
+
+  /**
+   * Static factory method to create a position based on its start and end offsets.
+   * 
+   * @param start
+   *          the start offset of the position
+   * @param end
+   *          the end offset of the position
+   * @return new position with the given start and end offsets
+   */
+  public static Position createStartEnd(int start, int end)
+  {
+    return new Position(start, end - start);
+  }
+
 }
