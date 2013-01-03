47c1f7ad088e0a3ece00f8634990a0c755592edd
Andrius Velykis <andrius-velykis@users.sourceforge.net>
2013-01-03 19:02:52 +0000

    match printed version to the library one

--- dev/java-cup/src/main/java/java_cup/version.java
+++ dev/java-cup/src/main/java/java_cup/version.java
@@ -30,7 +30,7 @@ public class version {
   /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
   /** The update letter. */
-  public static final String update = "a beta 20060608";
+  public static final String update = "a czt01 beta";
 
   /*. . . . . . . . . . . . . . . . . . . . . . . . . . . . . .*/
 
