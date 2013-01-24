16ef308d319c44e9ffaf380f328c6ea61ab54aa6
Leo Freitas <leo.freitas@newcastle.ac.uk>
2013-01-24 16:16:44 +0000

    ADD: support for default dialect

--- parser/buildp.sh
+++ parser/buildp.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# A convenience build script that performs 2-step build of CZT.
-#
-# 2-step build is necessary to build CZT core modules needed for
-# Eclipse plug-ins. Only after the core is built, the plug-ins
-# can be located by CZT Eclipse dependency resolution.
-#
-# See http://wiki.eclipse.org/Tycho/How_Tos/Dependency_on_pom-first_artifacts
-
 # Set the heap space needed for core builds
 export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 
