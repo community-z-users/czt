356ec0634ec13c02054a3c83dde8e28ac3b1baed
Leo Freitas <leo.freitas@newcastle.ac.uk>
2013-06-25 16:26:12 +0100

    ADD: generic time expression checking for circus time

--- typechecker/typechecker-circustime/src/main/java/net/sourceforge/czt/typecheck/circustime/Checker.java
+++ typechecker/typechecker-circustime/src/main/java/net/sourceforge/czt/typecheck/circustime/Checker.java
@@ -15,16 +15,32 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package net.sourceforge.czt.typecheck.circustime;
 
-import net.sourceforge.czt.typecheck.circustime.TypeChecker;
+import net.sourceforge.czt.base.ast.Term;
+import net.sourceforge.czt.typecheck.circus.util.GlobalDefs;
+import net.sourceforge.czt.typecheck.z.util.UResult;
+import net.sourceforge.czt.z.ast.Expr;
+import net.sourceforge.czt.z.ast.PowerType;
+import net.sourceforge.czt.z.ast.Type2;
+import net.sourceforge.czt.z.util.ZString;
 
 public abstract class Checker<R>
   extends net.sourceforge.czt.typecheck.circus.Checker<R>
 {
+	
+  // Needed to check the time expression is of the right (maximal) type
+  private final Expr arithmos_;
+
   public Checker(TypeChecker typeChecker)
   {
     super(typeChecker);
     assert typeChecker != null;
     typeChecker_ = typeChecker;
+    
+    // if arithmos type is wrong somehow, this will catch it.
+    // DON'T CACHE arithmos type as it won't work from the beginning.
+    arithmos_ = factory().createRefExpr(factory().createZDeclName(ZString.ARITHMOS));
+  }
+  
   protected ErrorAnn errorAnn(Term term, ErrorMessage error, Object[] params)
   {
     ErrorAnn errorAnn = new ErrorAnn(error.toString(), params, sectInfo(),
@@ -37,6 +53,27 @@ public abstract class Checker<R>
     ErrorAnn errorAnn = errorAnn(term, errorMsg, params);
     error(term, errorAnn);
   }
+
+  protected void typeCheckTimeExpr(Term term, Expr expr)
+  {
+    // whatever the type, even if with generic, it must be at least ARITHMOS
+    // this include both \nat and \real for the time of TIME.
+    Type2 found = GlobalDefs.unwrapType(expr.accept(exprChecker()));
+    Type2 expected = arithmos_.accept(exprChecker());
+    if (expected instanceof PowerType)
+    {
+      expected = ((PowerType)expected).getType();
+    }
+    // if arithmos type is wrong somehow, this will catch it.
+    // DON'T CACHE arithmos type as it won't work from the beginning.
+    if (!unify(found, expected).equals(UResult.SUCC))
+    {
+      Object[] params = {
+        getCurrentProcessName(), getCurrentActionName(),
+        term.getClass().getSimpleName(), expr, expected, found
+      };
+      error(term, ErrorMessage.CIRCUS_TIME_EXPR_DONT_UNIFY, params);
+    }
   }
 }
 
