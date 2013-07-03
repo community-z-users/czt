f841f0886934ba19bf440a04a39cdd9ae48ef8cf
Leo Freitas <leo.freitas@newcastle.ac.uk>
2013-07-03 11:19:37 +0100

    Revert "MODIFY: The proper derive class is added to maintained the code standard."
    This reverts commit 9a78c3f5c7d9ca3896ec1c25f109115d7cb228ec.

--- typechecker/typechecker-circustime/src/main/java/net/sourceforge/czt/typecheck/circustime/SpecChecker.java
+++ typechecker/typechecker-circustime/src/main/java/net/sourceforge/czt/typecheck/circustime/SpecChecker.java
@@ -15,52 +15,39 @@
   You should have received a copy of the GNU General Public License
   along with czt; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+*/
 package net.sourceforge.czt.typecheck.circustime;
 
 import java.util.List;
 import net.sourceforge.czt.base.ast.*;
 import net.sourceforge.czt.circustime.util.CircusTimeUtils;
-import net.sourceforge.czt.typecheck.circus.Checker;
-import net.sourceforge.czt.typecheck.circus.TypeChecker;
 import net.sourceforge.czt.z.ast.NameSectTypeTriple;
 
-public class SpecChecker extends Checker<List<NameSectTypeTriple>> {
-	// a Circus spec checker
-	protected net.sourceforge.czt.typecheck.circus.SpecChecker circusSpecChecker_;
 
-	public SpecChecker(TypeChecker typeChecker) {
-		super(typeChecker);
-		circusSpecChecker_ = new net.sourceforge.czt.typecheck.circus.SpecChecker(
-				typeChecker);
+public class SpecChecker extends net.sourceforge.czt.typecheck.circus.SpecChecker
+{
+  //a Circus spec checker
+  protected net.sourceforge.czt.typecheck.circus.SpecChecker circusSpecChecker_;
 
-		NameSectTypeTriple synchTriple = factory()
-				.createNameSectTypeTriple(factory().createSynchName(),
-						CircusTimeUtils.CIRCUSTIME_PRELUDE,
-						factory().createSynchType());
-		NameSectTypeTriple circustimeIdTriple = factory()
-				.createNameSectTypeTriple(factory().createCircusIdName(),
-						CircusTimeUtils.CIRCUSTIME_PRELUDE,
-						factory().createCircusIdType());
-		NameSectTypeTriple transformerTriple = factory()
-				.createNameSectTypeTriple(factory().createTransformerName(),
-						CircusTimeUtils.CIRCUSTIME_PRELUDE,
-						factory().createTransformerType());
-		sectTypeEnv().add(
-				factory().list(synchTriple, circustimeIdTriple,
-						transformerTriple));
-	}
+  public SpecChecker(TypeChecker typeChecker)
+  {
+    super(typeChecker);
+    circusSpecChecker_ = new net.sourceforge.czt.typecheck.circus.SpecChecker(typeChecker);
+    
+    NameSectTypeTriple synchTriple = factory().createNameSectTypeTriple(
+    	      factory().createSynchName(), CircusTimeUtils.CIRCUSTIME_PRELUDE, factory().createSynchType());
+    	    NameSectTypeTriple circustimeIdTriple = factory().createNameSectTypeTriple(
+    	      factory().createCircusIdName(), CircusTimeUtils.CIRCUSTIME_PRELUDE, factory().createCircusIdType());
+    	    NameSectTypeTriple transformerTriple = factory().createNameSectTypeTriple(
+    	      factory().createTransformerName(), CircusTimeUtils.CIRCUSTIME_PRELUDE, 
+    	      factory().createTransformerType());    
+    	    sectTypeEnv().add(factory().list(synchTriple, circustimeIdTriple, transformerTriple));            
+    	  
+  }
 
-	/**
-	 * Visits all other terms that the Circus SpecChecker can deal with, or
-	 * reach the global Checker.visitTerm, which catches and flags it as an AST
-	 * error.
-	 */
-
-	@Override
-	public List<NameSectTypeTriple> visitTerm(Term term) {
-		// for all other terms, just use the circusSpecChecker.
-		return term.accept(circusSpecChecker_);
-	}
-
+  @Override
+  public List<NameSectTypeTriple> visitTerm(Term term)
+  {
+    return term.accept(circusSpecChecker_);
+  }
 }
