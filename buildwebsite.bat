32df98b788d408861ec3ba947876c075d924bfb1
Leo Freitas <leo.freitas@newcastle.ac.uk>
2015-07-29 15:47:10 +0100

    UPDATE: remove MAVEN_OPTS given Java 8 doesn't use them

--- buildwebsite.bat
+++ buildwebsite.bat
@@ -1,4 +1,4 @@
-call set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=512m
+:: call set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=512m
 
 call mvn site site:stage -P site -U
 if not "%ERRORLEVEL%" == "0" pause & exit /b
