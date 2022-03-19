4cd272a89570cb957ef2fa47c42811d9ef8ae705
SamuelValery <samuel.valery@hotmail.com>
2022-03-19 14:35:18 +1000

    Changed source/target from 1.8 to 11

--- buildall.sh
+++ buildall.sh
@@ -9,7 +9,7 @@
 # See http://wiki.eclipse.org/Tycho/How_Tos/Dependency_on_pom-first_artifacts
 
 # Set the heap space needed for core builds
-#export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
 
 # In case of Maven errors, terminate the full build as well
 # (This is to ensure step 2 does not run if step 1 fails)
