bf5ac59568bc6689e26f15d576bd1ef35bb8034b
SamuelValery <samuel.valery@hotmail.com>
2022-04-19 09:50:12 +1000

    Implement GitHub Actions CI workflow
    * Triggered when the developer creates a PR
    * Automates code compilation and returns result via a comment on the PR
    * Automates test execution and returns result via a comment on the PR

---
+++ ci_scripts/build/build_czt.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Set the heap space needed for core builds
+export MAVEN_OPTS="-Xmx1024m" 
+
+# In case of Maven errors, terminate the full build as well
+# set -e
+
+# Set the build report output file
+LOGFILE=build_report.txt
+
+# Build the CZT core
+# Output stdout and stderr to LOGFILE and to screen.
+# mvn clean install -U > >(tee ${LOGFILE}) 2>&1
+
+# Only compile and package source and test code (skip testing phase)
+mvn clean install -U -Dmaven.test.skip.exec=true > >(tee ${LOGFILE}) 2>&1
