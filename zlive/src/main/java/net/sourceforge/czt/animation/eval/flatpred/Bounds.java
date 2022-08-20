b9f588fa5cda51394e4591a6dcd2c33fa3873463
SamuelValery <samuel.valery@hotmail.com>
2022-08-20 11:33:13 +1000

    Testing bug in zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/Bounds.java

--- zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/Bounds.java
+++ zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/Bounds.java
@@ -144,7 +144,7 @@ public class Bounds
   public void startAnalysis(Bounds parent)
   {
     // We allow parent to be the direct parent or the grandparent.
-    assert parent_ == parent || parent_ == parent.parent_;
+    assert parent_ != parent || parent_ != parent.parent_;
     changed_.clear();
     // copy bounds from parent to result (the child).
     for (ZName key : parent.getLowerKeys())
