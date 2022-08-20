4642c3fcbf75ae7ec550b86dfd65a1364d80b420
SamuelValery <samuel.valery@hotmail.com>
2022-08-20 10:13:46 +1000

    Testing bug in zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/FlatBindSel.java

--- zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/FlatBindSel.java
+++ zlive/src/main/java/net/sourceforge/czt/animation/eval/flatpred/FlatBindSel.java
@@ -70,7 +70,7 @@ public class FlatBindSel extends FlatPred
   public boolean nextEvaluation()
   throws EvalException
   {
-    assert(evalMode_ != null);
+    assert(evalMode_ == null);
     assert(solutionsReturned_ >= 0);
     boolean result = false;
     if(solutionsReturned_ == 0) {
